0c93b482da6c8ac51a097241b49e6742cd8cad42
Matthias Pfefferle <pfefferle@gmail.com>
2018-09-24 20:54:59 +0200

    fixed typo

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,7 +12,7 @@ if ( ! $_tests_dir ) {
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-	require dirname( dirname( __FILE__ ) ) . '/activitystream-extension.php';
+	require dirname( dirname( __FILE__ ) ) . '/activitypub.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
