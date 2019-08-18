aa53bec4e4b2e7d2ad3a371dc89c558e81f9452e
Matthias Pfefferle <pfefferle@gmail.com>
2019-08-18 22:34:28 +0200

    oops

--- tests/test-class-activitypub-hashtag.php
+++ tests/test-class-activitypub-hashtag.php
@@ -14,13 +14,14 @@ class Test_Activitypub_Hashtag extends WP_UnitTestCase {
 	}
 
 	public function the_content_provider() {
-		$object_link = get_term_by( 'name', 'object', 'post_tag' );
+		$object = get_term_by( 'name', 'object', 'post_tag' );
+		$link = get_tag_link( $object );
 
 		return array(
 			array( 'test', 'test' ),
 			array( '#test', '#test' ),
 			array( 'hallo #test test', 'hallo #test test' ),
-			array( 'hallo #object test', 'hallo <a rel="tag" class="u-tag u-category" href="' . $object_link . '">#object</a> test' ),
+			array( 'hallo #object test', 'hallo <a rel="tag" class="u-tag u-category" href="' . $link . '">#object</a> test' ),
 		);
 	}
 }
