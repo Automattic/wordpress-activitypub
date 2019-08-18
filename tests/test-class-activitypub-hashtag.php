8e87800ee032a751b60d7013215c008026a41937
Matthias Pfefferle <pfefferle@gmail.com>
2019-08-18 22:08:58 +0200

    added test

---
+++ tests/test-class-activitypub-hashtag.php
@@ -0,0 +1,18 @@
+<?php
+class Test_Activitypub_Hashtag extends WP_UnitTestCase {
+	/**
+	 * @dataProvider the_content_provider
+	 */
+	public function test_the_content( $content, $content_with_hashtag ) {
+		$content = \Activitypub\Hashtag::the_content( $content );
+
+		$this->assertEquals( $content_with_hashtag, $content );
+	}
+
+	public function the_content_provider() {
+		return [
+			[ 'test', 'test' ],
+			[ '#test', '#test' ],
+		];
+	}
+}
