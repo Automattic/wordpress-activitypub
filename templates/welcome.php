f333053df1e40b61d29fe983690c0bcfadc22c32
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2022-12-05 17:48:08 +0100

    Update templates/welcome.php
    Co-authored-by: Alex Kirk <akirk@users.noreply.github.com>

--- templates/welcome.php
+++ templates/welcome.php
@@ -91,7 +91,7 @@
 		</div>
 		<h4 class="activitypub-settings-accordion-heading">
 			<button aria-expanded="false" class="activitypub-settings-accordion-trigger" aria-controls="activitypub-settings-accordion-block-activitypub-nodeinfo-plugin" type="button">
-				<span class="title"><?php \esc_html_e( 'NodeInfo Plugin', 'activitypub' ); ?></span>
+				<span class="title"><?php \esc_html_e( 'Provide Enhanced Information about Your Blog', 'activitypub' ); ?></span>
 				<span class="icon"></span>
 			</button>
 		</h4>
