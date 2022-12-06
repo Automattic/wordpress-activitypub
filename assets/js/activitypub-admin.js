62c99c87a3ed5f02ab2ba027123f9bef17bb2abb
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2022-12-06 09:21:18 +0100

    Update assets/js/activitypub-admin.js
    Co-authored-by: Alex Kirk <akirk@users.noreply.github.com>

--- assets/js/activitypub-admin.js
+++ assets/js/activitypub-admin.js
@@ -11,4 +11,11 @@ jQuery( function( $ ) {
 			$( '#' + $( this ).attr( 'aria-controls' ) ).attr( 'hidden', false );
 		}
 	} );
+	
+	$(document).on( 'wp-plugin-install-success', function( event, response ) {
+		setTimeout( function() {
+			debugger;
+			$( '.activate-now' ).removeClass( 'thickbox open-plugin-details-modal' );
+		}, 1200 );
+	} );	
 } );
