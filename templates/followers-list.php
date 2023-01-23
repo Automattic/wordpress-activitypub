88398f85cc208d676dc91676d1ea89a4197af79e
Greg <greg@toolstack.com>
2023-01-23 00:03:31 -0500

    Add profile info to Users -> ActivityPub.
    And use new DB based follower count.

--- templates/followers-list.php
+++ templates/followers-list.php
@@ -1,8 +1,12 @@
 <div class="wrap">
-	<h1><?php \esc_html_e( 'Followers (Fediverse)', 'activitypub' ); ?></h1>
+	<h1><?php \esc_html_e( 'ActivityPub', 'activitypub' ); ?></h1>
+
+	<?php $user = wp_get_current_user(); \Activitypub\get_identifier_settings( $user->ID ); ?>
+
+	<h2><?php \esc_html_e( 'Followers', 'activitypub' ); ?></h2>
 
 	<?php // translators: ?>
-	<p><?php \printf( \esc_html__( 'You currently have %s followers.', 'activitypub' ), \esc_attr( \Activitypub\Peer\Followers::count_followers( \get_current_user_id() ) ) ); ?></p>
+	<p><?php \printf( \esc_html__( 'You currently have %s followers.', 'activitypub' ), \esc_attr( \Activitypub\Peer\Followers::count_followers_extended( \get_current_user_id() ) ) ); ?></p>
 
 	<?php $token_table = new \Activitypub\Table\Followers_List(); ?>
 
