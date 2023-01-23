517287272b3d9d64f5fd98ce636ceceab167783e
Greg <greg@toolstack.com>
2023-01-23 13:45:46 -0500

    Move html for profile identifier to new user-activitypub
    And remove unused functions.

--- includes/functions.php
+++ includes/functions.php
@@ -229,43 +229,6 @@ function get_follower_inboxes( $user_id ) {
 	return $inboxes;
 }
 
-function get_identifier_settings( $user_id ) {
-	?>
-<table class="form-table">
-	<tbody>
-		<tr>
-			<th scope="row">
-				<label><?php \esc_html_e( 'Profile identifier', 'activitypub' ); ?></label>
-			</th>
-			<td>
-				<p><code><?php echo \esc_html( \Activitypub\get_webfinger_resource( $user_id ) ); ?></code> or <code><?php echo \esc_url( \get_author_posts_url( $user_id ) ); ?></code></p>
-				<?php // translators: the webfinger resource ?>
-				<p class="description"><?php \printf( \esc_html__( 'Try to follow "@%s" by searching for it on Mastodon, Friendica & Co., etc.', 'activitypub' ), \esc_html( \Activitypub\get_webfinger_resource( $user_id ) ) ); ?></p>
-			</td>
-		</tr>
-	</tbody>
-</table>
-	<?php
-}
-
-function get_followers_settings( $user_id ) {
-	?>
-<table class="form-table">
-	<tbody>
-		<tr>
-			<th scope="row">
-				<label><?php \esc_html_e( 'Followers', 'activitypub' ); ?></label>
-			</th>
-			<td>
-				<p><?php \printf( \esc_html__( 'You currently have %s%s%s followers.', 'activitypub' ), '<a href=" ' . get_admin_url() . 'users.php?page=activitypub-followers-list">', \esc_attr( \Activitypub\Peer\Followers::count_followers( \get_current_user_id() ) ), '</a>' ); ?></p>
-			</td>
-		</tr>
-	</tbody>
-</table>
-	<?php
-
-}
-
 function get_followers( $user_id ) {
 	$followers = \Activitypub\Peer\Followers::get_followers( $user_id );
 
--- templates/user-activitypub.php
+++ templates/user-activitypub.php
@@ -1,7 +1,12 @@
 <div class="wrap">
 	<h1><?php \esc_html_e( 'ActivityPub', 'activitypub' ); ?></h1>
 
-	<?php $user = wp_get_current_user(); \Activitypub\get_identifier_settings( $user->ID ); ?>
+	<h2><?php \esc_html_e( 'Profile identifier', 'activitypub' ); ?></h2>
+
+	<?php $user = wp_get_current_user(); ?>
+	<p><code><?php echo \esc_html( \Activitypub\get_webfinger_resource( $user->ID ) ); ?></code> or <code><?php echo \esc_url( \get_author_posts_url( $user->ID ) ); ?></code></p>
+	<?php // translators: the webfinger resource ?>
+	<p class="description"><?php \printf( \esc_html__( 'Try to follow "@%s" by searching for it on Mastodon, Friendica & Co., etc.', 'activitypub' ), \esc_html( \Activitypub\get_webfinger_resource( $user->ID ) ) ); ?></p>
 
 	<h2><?php \esc_html_e( 'Followers', 'activitypub' ); ?></h2>
 
