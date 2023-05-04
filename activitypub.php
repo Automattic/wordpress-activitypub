0fd11d25faf053807f3eab309e78b848ebfde02c
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-05-04 15:18:58 +0200

    will be auto-loaded

--- activitypub.php
+++ activitypub.php
@@ -81,7 +81,6 @@ require_once \dirname( __FILE__ ) . '/includes/functions.php';
 
 // load NodeInfo endpoints only if blog is public
 if ( true === (bool) \get_option( 'blog_public', 1 ) ) {
-	require_once \dirname( __FILE__ ) . '/includes/rest/class-nodeinfo.php';
 	Rest\NodeInfo::init();
 }
 
