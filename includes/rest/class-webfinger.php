5a91fdcf0ad73a1555d5928ac57a1db0399df123
Matt Wiebe <wiebe@automattic.com>
2023-05-12 15:43:04 -0500

    remove debug log

--- includes/rest/class-webfinger.php
+++ includes/rest/class-webfinger.php
@@ -24,7 +24,6 @@ class Webfinger {
 	 * Register routes
 	 */
 	public static function register_routes() {
-		\l( 'register webfinger' );
 		\register_rest_route(
 			ACTIVITYPUB_REST_NAMESPACE,
 			'/webfinger',
