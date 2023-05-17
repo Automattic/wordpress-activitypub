c34fb74b41b5bc84b38a33b0b9783fb8dd7c7d42
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-05-17 09:03:26 +0200

    coding style

--- includes/functions.php
+++ includes/functions.php
@@ -233,6 +233,7 @@ function is_tombstone( $wp_error ) {
  * Get the REST URL relative to this plugin's namespace.
  *
  * @param string $path Optional. REST route path. Otherwise this plugin's namespaced root.
+ *
  * @return string REST URL relative to this plugin's namespace.
  */
 function get_rest_url_by_path( $path = '' ) {
