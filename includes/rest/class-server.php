467a349b1618240e9b9efb6aa0a05ddeed351ff9
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-05-22 11:31:46 +0200

    some small improvements

--- includes/rest/class-server.php
+++ includes/rest/class-server.php
@@ -1,9 +1,12 @@
 <?php
 namespace Activitypub\Rest;
 
+use stdClass;
 use WP_REST_Response;
 use Activitypub\Signature;
 use Activitypub\Model\User;
+
+use function Activitypub\get_context;
 use function Activitypub\get_rest_url_by_path;
 
 
@@ -47,17 +50,17 @@ class Server {
 	 * @return WP_REST_Response The JSON profile of the Application Actor.
 	 */
 	public static function application_actor() {
-		$json = new \stdClass();
+		$json = new stdClass();
 
-		$json->{'@context'} = \Activitypub\get_context();
+		$json->{'@context'} = get_context();
 		$json->id = get_rest_url_by_path( 'application' );
 		$json->type = 'Application';
 		$json->preferredUsername = str_replace( array( '.' ), '-', wp_parse_url( get_site_url(), PHP_URL_HOST ) ); // phpcs:ignore WordPress.NamingConventions
 		$json->name = get_bloginfo( 'name' );
-		$json->summary = 'WordPress-ActivityPub application actor';
+		$json->summary = __( 'WordPress-ActivityPub application actor', 'activitypub' );
 		$json->manuallyApprovesFollowers = true; // phpcs:ignore WordPress.NamingConventions
 		$json->icon = array( get_site_icon_url() ); // phpcs:ignore WordPress.NamingConventions short array syntax
-		$json->publicKey = (object) array( // phpcs:ignore WordPress.NamingConventions
+		$json->publicKey = array( // phpcs:ignore WordPress.NamingConventions
 			'id' => get_rest_url_by_path( 'application#main-key' ),
 			'owner' => get_rest_url_by_path( 'application' ),
 			'publicKeyPem' => Signature::get_public_key( User::APPLICATION_USER_ID ), // phpcs:ignore WordPress.NamingConventions
@@ -73,7 +76,7 @@ class Server {
 	/**
 	 * Callback function to authorize each api requests
 	 *
-	 * @see \WP_REST_Request
+	 * @see WP_REST_Request
 	 *
 	 * @param WP_REST_Response|WP_HTTP_Response|WP_Error|mixed $response Result to send to the client.
 	 *                                                                   Usually a WP_REST_Response or WP_Error.
@@ -84,7 +87,12 @@ class Server {
 	 */
 	public static function authorize_activitypub_requests( $response, $handler, $request ) {
 		$route = $request->get_route();
+
 		if ( ! str_starts_with( $route, '/activitypub' ) ) {
+			return $response;
+		}
+
+		if ( get_rest_url_by_path( 'webfinger' ) !== $route ) {
 			return $response;
 		}
 
@@ -93,14 +101,11 @@ class Server {
 			if ( \is_wp_error( $verified_request ) ) {
 				return $verified_request;
 			}
-		} else {
-			if ( get_rest_url_by_path( 'webfinger' ) !== $route ) {
-				// SecureMode/Authorized fetch.
-				if ( ACTIVITYPUB_SECURE_MODE ) {
-					$verified_request = Signature::verify_http_signature( $request );
-					if ( \is_wp_error( $verified_request ) ) {
-						return $verified_request;
-					}
+		} elseif ( 'GET' === $request->get_method() ) {
+			if ( ACTIVITYPUB_SECURE_MODE ) {
+				$verified_request = Signature::verify_http_signature( $request );
+				if ( \is_wp_error( $verified_request ) ) {
+					return $verified_request;
 				}
 			}
 		}
