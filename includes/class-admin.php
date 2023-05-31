c04cf3fc7e72529f49fef3a0060bec82b284b71c
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-05-31 10:48:06 +0200

    move schedule to scheduler-class

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -15,7 +15,6 @@ class Admin {
 	public static function init() {
 		\add_action( 'admin_menu', array( self::class, 'admin_menu' ) );
 		\add_action( 'admin_init', array( self::class, 'register_settings' ) );
-		\add_action( 'admin_init', array( self::class, 'schedule_migration' ) );
 		\add_action( 'show_user_profile', array( self::class, 'add_profile' ) );
 		\add_action( 'personal_options_update', array( self::class, 'save_user_description' ) );
 		\add_action( 'admin_enqueue_scripts', array( self::class, 'enqueue_scripts' ) );
@@ -144,12 +143,6 @@ class Admin {
 				'default'      => array( 'post', 'pages' ),
 			)
 		);
-	}
-
-	public static function schedule_migration() {
-		if ( ! \wp_next_scheduled( 'activitypub_schedule_migration' ) ) {
-			\wp_schedule_single_event( \time(), 'activitypub_schedule_migration' );
-		}
 	}
 
 	public static function add_settings_help_tab() {
