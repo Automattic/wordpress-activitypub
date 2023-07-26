a3f348304ea8cf6d62a6202c4c03f0a82610d3c9
Matt Wiebe <wiebe@automattic.com>
2023-07-26 14:24:46 -0500

    fix divergent translators comments

--- templates/user-followers-list.php
+++ templates/user-followers-list.php
@@ -1,6 +1,6 @@
 <div class="wrap">
 	<h1><?php \esc_html_e( 'Followers', 'activitypub' ); ?></h1>
-	<?php // translators: ?>
+	<?php // translators: The follower count. ?>
 	<p><?php \printf( \esc_html__( 'You currently have %s followers.', 'activitypub' ), \esc_attr( \Activitypub\Collection\Followers::count_followers( \get_current_user_id() ) ) ); ?></p>
 
 	<?php $table = new \Activitypub\Table\Followers(); ?>
