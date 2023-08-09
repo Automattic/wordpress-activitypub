0ff9f70f5d2cd53737612c368fbd58c8d806cb0b
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-08-09 14:13:58 +0200

    fix @context

--- includes/rest/class-collection.php
+++ includes/rest/class-collection.php
@@ -80,9 +80,11 @@ class Collection {
 		}
 
 		$response = array(
-			'@context' => 'https://www.w3.org/ns/activitystreams',
-			array(
-				'Hashtah' => 'as:Hastag',
+			'@context' => array(
+				'https://www.w3.org/ns/activitystreams',
+				array(
+					'Hastag' => 'as:Hastag',
+				),
 			),
 			'id'         => get_rest_url_by_path( sprintf( 'users/%d/collections/tags', $user_id ) ),
 			'totalItems' => count( $tags ),
