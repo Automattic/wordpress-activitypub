07d805a1ce3ad5d70fb7592f914c70ce77a9964d
Matt Wiebe <wiebe@automattic.com>
2023-09-21 11:44:30 -0500

    Blocks: ensure that only a valid user can be selected
    Fixes #440

--- build/follow-me/index.asset.php
+++ build/follow-me/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9d4af3528585afd49437');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '5e11f2fcd4dc9ac6d7ee');
