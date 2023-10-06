aae2e23c023e673c1ac73320959c906b95fc8717
Matt Wiebe <wiebe@automattic.com>
2023-10-06 16:48:15 -0500

    npm run build

--- build/followers/view.asset.php
+++ build/followers/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '6384e801c2802d2fecee');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'f0e21057f7ec615290d6');
