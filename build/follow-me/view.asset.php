bade9a13485bc32c446f5bdf500ba1351efdece8
Matt Wiebe <wiebe@automattic.com>
2023-10-06 16:54:48 -0500

    Blocks: better frontend UX (#494)
    Before this: ugly, bad loading
    
    After this: happy, in my lane, moisturized, moving

--- build/follow-me/view.asset.php
+++ build/follow-me/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '06a922cf2e58c94b6431');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '50b21380c69f69ee1a33');
