4d7c0594cdadf294e56e739c22423b356192d61e
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-10-23 16:16:24 +0200

    remove featured tags endpoint

--- includes/model/class-application-user.php
+++ includes/model/class-application-user.php
@@ -58,10 +58,6 @@ class Application_User extends Blog_User {
 		return null;
 	}
 
-	public function get_featured_tags() {
-		return null;
-	}
-
 	public function get_featured() {
 		return null;
 	}
