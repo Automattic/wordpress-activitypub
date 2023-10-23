0ab8df539e6b300efd2956ab0bc8e6b28ec62b63
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-10-23 08:28:25 +0200

    simplify check

--- includes/rest/class-server.php
+++ includes/rest/class-server.php
@@ -90,12 +90,12 @@ class Server {
 		}
 
 		// POST-Requets are always signed
-		if ( 'get' !== \strtolower( $request->get_method() ) ) {
+		if ( 'GET' !== $request->get_method() ) {
 			$verified_request = Signature::verify_http_signature( $request );
 			if ( \is_wp_error( $verified_request ) ) {
 				return $verified_request;
 			}
-		} elseif ( 'get' === \strtolower( $request->get_method() ) ) { // GET-Requests are only signed in secure mode
+		} elseif ( 'GET' === $request->get_method() ) { // GET-Requests are only signed in secure mode
 			if ( ACTIVITYPUB_AUTHORIZED_FETCH ) {
 				$verified_request = Signature::verify_http_signature( $request );
 				if ( \is_wp_error( $verified_request ) ) {
