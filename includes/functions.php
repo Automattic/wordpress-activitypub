5b440b38199b4c2d9c93535a3f3c6848cd068c57
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-11-06 14:38:58 +0100

    remove reply_recipients

--- includes/functions.php
+++ includes/functions.php
@@ -310,48 +310,6 @@ function is_comment() {
 }
 
 /**
- * Get recipients to insert/tag in reply tag from received AP object meta
- * @param string $object_id a comment_id to search
- * @param boolean $post defaults to searching a comment_id
- * @return string space separated webfinger of tagged users
- */
-function reply_recipients( $object_id, $post = null ) {
-	$recipients = null;
-	if ( $post ) {
-		//post
-		$ap_object = \unserialize( \get_post_meta( $object_id, 'ap_object', true ) );
-	} else {
-		//comment
-		$ap_object = \unserialize( \get_comment_meta( $object_id, 'ap_object', true ) );
-	}
-
-	if ( ! empty( $ap_object ) ) {
-		// Replying to remote comments.
-		$recipients[] = \ActivityPub\url_to_webfinger( $ap_object['actor'] ); // Reply to Object actor!
-
-		if ( ! empty( $ap_object['object']['tag'] ) ) { // Reply to other tagged users.
-			if ( isset( $ap_object['user_id'] ) ) {
-				$author_post_url = \get_author_posts_url( $ap_object['user_id'] );// ignore self tag.
-				foreach ( $ap_object['object']['tag'] as $tag ) { // Other tagged users
-					if ( $author_post_url === $tag['href'] ) {
-						continue;
-					}
-					if ( in_array( 'Mention', $tag ) ) {
-						$recipients[] = $tag['name'];
-					}
-				}
-			}
-		}
-		return implode( ' ', $recipients );
-	} else {
-		// Replying to self with others.
-		$comment = \get_comment( $object_id );
-		preg_match_all( '/@' . ACTIVITYPUB_USERNAME_REGEXP . '/', $comment->comment_content, $recipients );
-		return implode( ' ', $recipients[0] );
-	}
-}
-
-/**
  * @param string $user_url
  * @return string $webfinger
  */
