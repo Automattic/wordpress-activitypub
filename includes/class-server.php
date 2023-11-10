7543884a17d4168ade43faaafa775980f397e320
Django Doucet <mediaformat.ux@gmail.com>
2023-11-10 08:25:14 -0700

    Add blog user followers to known inboxes

--- includes/class-server.php
+++ includes/class-server.php
@@ -2,6 +2,11 @@
 
 namespace Activitypub;
 
+use Activitypub\Collection\Followers;
+use Activitypub\Collection\Users;
+
+use function Activitypub\is_user_disabled;
+
 /**
  * ActivityPub Server Class
  *
@@ -18,6 +23,10 @@ class Server {
 		$follower_inboxes_all = [];
 		foreach ( $authors as $user ) {
 			$follower_inboxes = Followers::get_inboxes( $user->ID );
+			$follower_inboxes_all = array_merge( $follower_inboxes, $follower_inboxes_all );
+		}
+		if ( ! is_user_disabled( Users::BLOG_USER_ID ) ) {
+			$follower_inboxes = Followers::get_inboxes( Users::BLOG_USER_ID );
 			$follower_inboxes_all = array_merge( $follower_inboxes, $follower_inboxes_all );
 		}
 		return array_unique( array_filter( $follower_inboxes_all ) );
