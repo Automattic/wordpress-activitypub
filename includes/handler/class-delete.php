8e2ed70621e707429effb20f2db3e213a271a7ac
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-11-21 23:01:05 +0100

    improve delete handler

--- includes/handler/class-delete.php
+++ includes/handler/class-delete.php
@@ -1,6 +1,9 @@
 <?php
 namespace Activitypub\Handler;
 
+use WP_Error;
+use WP_REST_Request;
+use Activitypub\Http;
 use Activitypub\Collection\Followers;
 
 /**
@@ -12,7 +15,8 @@ class Delete {
 	 */
 	public static function init() {
 		\add_action( 'activitypub_inbox_delete', array( self::class, 'handle_delete' ), 10, 2 );
-		//
+		// defer signature verification for `Delete` requests.
+		\add_filter( 'activitypub_defer_signature_verification', array( self::class, 'defer_signature_verification' ), 10, 2 );
 	}
 
 	/**
@@ -21,11 +25,10 @@ class Delete {
 	 * @param array $activity The delete activity.
 	 * @param int   $user_id  The ID of the user performing the delete activity.
 	 */
-	public function handle_delete( $activity, $user_id ) {
+	public static function handle_delete( $activity, $user_id ) {
 		if (
 			! isset( $activity['object'] ) ||
-			! is_array( $activity['object'] ) ||
-			! isset( $activity['object']['id'] )
+			! isset( $activity['actor'] )
 		) {
 			return;
 		}
@@ -33,22 +36,22 @@ class Delete {
 		$object_type = isset( $activity['object']['type'] ) ? $activity['object']['type'] : '';
 
 		switch ( $object_type ) {
+			// Actor Types
+			// @see https://www.w3.org/TR/activitystreams-vocabulary/#actor-types
 			case 'Person':
 			case 'Group':
 			case 'Organization':
 			case 'Service':
 			case 'Application':
-				$follower = Followers::get_follower( $user_id, $activity['actor'] );
-				if ( $follower ) {
-					$follower->delete();
-				}
-
-				// delete all activities from this user.
-
+				self::maybe_delete_follower( $user_id, $activity );
 				break;
+			// Tombstone Type
+			// @see: https://www.w3.org/TR/activitystreams-vocabulary/#dfn-tombstone
 			case 'Tombstone':
 				// Handle tombstone.
 				break;
+			// Object and Link Types
+			// @see https://www.w3.org/TR/activitystreams-vocabulary/#object-types
 			case 'Note':
 			case 'Article':
 			case 'Image':
@@ -56,9 +59,62 @@ class Delete {
 			case 'Video':
 			case 'Event':
 			case 'Document':
+				self::maybe_delete_reaction( $user_id, $activity );
+				break;
+			// Minimal Activity
+			// @see https://www.w3.org/TR/activitystreams-core/#example-1
 			default:
-				// Handle delete activity for other object types.
+				// ignore non Minimal Activities.
+				if ( ! is_string( $activity['object'] ) ) {
+					return;
+				}
+
+				// check if Object is an Actor.
+				if ( $activity['actor'] === $activity['object'] ) {
+					self::maybe_delete_follower( $user_id, $activity );
+				} else { // assume a reaction otherwise.
+					self::maybe_delete_reaction( $user_id, $activity );
+				}
+				// maybe handle Delete Activity for other Object Types.
 				break;
 		}
+	}
+
+	/**
+	 * Delete a Follower if Actor-URL is a Tombstone.
+	 *
+	 * @param int   $user_id  The ID of the user receiving the delete activity.
+	 * @param array $activity The delete activity.
+	 */
+	public static function maybe_delete_follower( $user_id, $activity ) {
+		$follower = Followers::get_follower( $user_id, $activity['actor'] );
+
+		// if no matching follower, nothing to do.
+		if ( ! $follower ) {
+			return;
+		}
+
+		// verify id Actor is deleted.
+		if ( Http::is_tombstone( $activity['actor'] ) ) {
+			$follower->delete();
+		}
+	}
+
+	/**
+	 * Defer signature verification for `Delete` requests.
+	 *
+	 * @param bool            $defer   Whether to defer signature verification.
+	 * @param WP_REST_Request $request The request object.
+	 *
+	 * @return bool Whether to defer signature verification.
+	 */
+	public static function defer_signature_verification( $defer, $request ) {
+		$json = $request->get_json_params();
+
+		if ( 'Delete' === $json['type'] ) {
+			return true;
+		}
+
+		return false;
 	}
 }
