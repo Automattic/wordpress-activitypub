e8b819e2f1a4027678310310944fdeacea21d853
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-11-27 16:37:06 +0100

    update profile and update interaction

--- includes/handler/class-update.php
+++ includes/handler/class-update.php
@@ -4,6 +4,8 @@ namespace Activitypub\Handler;
 use WP_Error;
 use Activitypub\Collection\Interactions;
 
+use function Activitypub\get_remote_metadata_by_actor;
+
 /**
  * Handle Update requests.
  */
@@ -12,7 +14,7 @@ class Update {
 	 * Initialize the class, registering WordPress hooks
 	 */
 	public static function init() {
-		\add_action( 'activitypub_inbox_update', array( self::class, 'handle_update' ), 10, 3 );
+		\add_action( 'activitypub_inbox_update', array( self::class, 'handle_update' ), 10, 2 );
 	}
 
 	/**
@@ -22,21 +24,72 @@ class Update {
 	 * @param int                  $user_id The id of the local blog-user
 	 * @param Activitypub\Activity $object  The activity object
 	 */
-	public static function handle_update( $array, $user_id, $object = null ) {
-		if (
-			! isset( $array['object'] ) ||
-			! isset( $array['object']['id'] )
-		) {
-			return;
-		}
+	public static function handle_update( $activity, $user_id ) {
+		$object_type = isset( $activity['object']['type'] ) ? $activity['object']['type'] : '';
 
-		$state    = Interactions::update_comment( $array );
+		switch ( $object_type ) {
+			// Actor Types
+			// @see https://www.w3.org/TR/activitystreams-vocabulary/#actor-types
+			case 'Person':
+			case 'Group':
+			case 'Organization':
+			case 'Service':
+			case 'Application':
+				self::update_actor( $activity );
+				break;
+			// Object and Link Types
+			// @see https://www.w3.org/TR/activitystreams-vocabulary/#object-types
+			case 'Note':
+			case 'Article':
+			case 'Image':
+			case 'Audio':
+			case 'Video':
+			case 'Event':
+			case 'Document':
+				self::update_interaction( $activity, $user_id );
+				break;
+			// Minimal Activity
+			// @see https://www.w3.org/TR/activitystreams-core/#example-1
+			default:
+				break;
+		}
+	}
+
+	/**
+	 * Update an Interaction
+	 *
+	 * @param array $activity The activity-object
+	 * @param int   $user_id  The id of the local blog-user
+	 *
+	 * @return void
+	 */
+	public static function update_interaction( $activity, $user_id ) {
+		$state    = Interactions::update_comment( $activity );
 		$reaction = null;
 
 		if ( $state && ! \is_wp_error( $reaction ) ) {
 			$reaction = \get_comment( $state );
 		}
 
-		\do_action( 'activitypub_handled_update', $array, $user_id, $state, $reaction );
+		\do_action( 'activitypub_handled_update', $activity, $user_id, $state, $reaction );
+	}
+
+	/**
+	 * Update an Actor
+	 *
+	 * @param array $activity The activity-object
+	 *
+	 * @return void
+	 */
+	public static function update_actor( $activity ) {
+		if ( isset( $activity['actor'] ) ) {
+			$actor = $activity['actor'];
+
+			if ( is_array( $activity['actor'] ) ) {
+				$actor = $activity['actor']['id'];
+			}
+
+			get_remote_metadata_by_actor( $actor, false );
+		}
 	}
 }
