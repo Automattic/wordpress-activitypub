657d2b35c5da94c02d7c910f07829b2f73bd786e
Jan Boddez <jan.boddez@gmail.com>
2024-04-18 19:59:10 +0200

    Ugly, but works?

--- includes/transformer/class-base.php
+++ includes/transformer/class-base.php
@@ -1,6 +1,7 @@
 <?php
 namespace Activitypub\Transformer;
 
+use WP_Error;
 use WP_Post;
 use WP_Comment;
 
@@ -32,7 +33,54 @@ abstract class Base {
 	 *
 	 * @return Base_Object
 	 */
-	public static function transform( $object ) {
+	public static function transform( $object ) { // phpcs:ignore Universal.NamingConventions.NoReservedKeywordParameterNames.objectFound
+		if ( ! \is_object( $object ) ) {
+			return new WP_Error( 'invalid_object', __( 'Invalid object', 'activitypub' ) );
+		}
+
+		$class = \get_class( $object );
+
+		/**
+		 * Filter the transformer for a given object.
+		 *
+		 * Add your own transformer based on the object class or the object type.
+		 *
+		 * Example usage:
+		 *
+		 * // Filter be object class
+		 * add_filter( 'activitypub_transformer', function( $transformer, $object, $object_class ) {
+		 *     if ( $object_class === 'WP_Post' ) {
+		 *         return new My_Post_Transformer( $object );
+		 *     }
+		 *     return $transformer;
+		 * }, 10, 3 );
+		 *
+		 * // Filter be object type
+		 * add_filter( 'activitypub_transformer', function( $transformer, $object, $object_class ) {
+		 *     if ( $object->post_type === 'event' ) {
+		 *         return new My_Event_Transformer( $object );
+		 *     }
+		 *     return $transformer;
+		 * }, 10, 3 );
+		 *
+		 * @param Activitypub\Transformer\Base $transformer  The transformer to use.
+		 * @param mixed                        $object       The object to transform.
+		 * @param string                       $object_class The class of the object to transform.
+		 *
+		 * @return mixed The transformer to use.
+		 */
+		$transformer = \apply_filters( 'activitypub_transformer', null, $object, $class );
+		if ( $transformer ) {
+			if (
+				! \is_object( $transformer ) ||
+				! $transformer instanceof Base
+			) {
+				return new WP_Error( 'invalid_transformer', __( 'Invalid transformer', 'activitypub' ) );
+			}
+
+			return $transformer;
+		}
+
 		return new static( $object );
 	}
 
