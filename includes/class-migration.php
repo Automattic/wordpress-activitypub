63b0568737ac8f48be95eb73e73e4beaa2760508
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-07-08 19:02:17 +0200

    remove unused migration

--- includes/class-migration.php
+++ includes/class-migration.php
@@ -284,44 +284,4 @@ class Migration {
 			$user->add_cap( 'activitypub' );
 		}
 	}
-
-	public static function generate_extra_fields( $user_id ) {
-		$blog = array(
-			'post_type'      => 'ap_extrafield',
-			'post_title'     => \__( 'Blog', 'activitypub' ),
-			'post_status'    => 'publish',
-			'post_author'    => $user_id,
-			// format to block editor format
-			'post_content'   => '<!-- wp:paragraph --><p><a rel="me" title="' . \esc_attr( \home_url( '/' ) ) . '" target="_blank" href="' . \home_url( '/' ) . '">' . \wp_parse_url( \home_url( '/' ), \PHP_URL_HOST ) . '</a></p><!-- /wp:paragraph -->',
-			'comment_status' => 'closed',
-		);
-
-		wp_insert_post( $blog );
-
-		$profile = array(
-			'post_type'      => 'ap_extrafield',
-			'post_title'     => \__( 'Profile', 'activitypub' ),
-			'post_status'    => 'publish',
-			'post_author'    => $user_id,
-			// format to block editor format
-			'post_content'   => '<!-- wp:paragraph --><p><a rel="me" title="' . \esc_attr( \get_author_posts_url( $user_id ) ) . '" target="_blank" href="' . \get_author_posts_url( $user_id ) . '">' . \wp_parse_url( \get_author_posts_url( $user_id ), \PHP_URL_HOST ) . '</a></p><!-- /wp:paragraph -->',
-			'comment_status' => 'closed',
-		);
-
-		wp_insert_post( $profile );
-
-		if ( \get_the_author_meta( 'user_url', $user_id ) ) {
-			$homepage = array(
-				'post_type'      => 'ap_extrafield',
-				'post_title'     => \__( 'Homepage', 'activitypub' ),
-				'post_status'    => 'publish',
-				'post_author'    => $user_id,
-				// format to block editor format
-				'post_content'   => '<!-- wp:paragraph --><p><a rel="me" title="' . \esc_attr( \get_the_author_meta( 'user_url', $user_id ) ) . '" target="_blank" href="' . \get_the_author_meta( 'user_url', $user_id ) . '">' . \wp_parse_url( \get_the_author_meta( 'user_url', $user_id ), \PHP_URL_HOST ) . '</a></p><!-- /wp:paragraph -->',
-				'comment_status' => 'closed',
-			);
-
-			wp_insert_post( $homepage );
-		}
-	}
 }
