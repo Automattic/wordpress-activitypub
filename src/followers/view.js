4babfd5708ed491de34112c43c71842e98e1a9b4
Matt Wiebe <wiebe@automattic.com>
2024-07-11 22:09:05 -0500

    render -> createRoot

--- src/followers/view.js
+++ src/followers/view.js
@@ -1,12 +1,12 @@
 import './style.scss';
 import { Followers } from './followers';
-import { render } from '@wordpress/element';
+import { createRoot } from '@wordpress/element';
 import domReady from '@wordpress/dom-ready';
 
 domReady( () => {
 	// iterate over a nodelist
 	[].forEach.call( document.querySelectorAll( '.activitypub-follower-block' ), ( element ) => {
 		const attrs = JSON.parse( element.dataset.attrs );
-		render( <Followers { ...attrs } />, element );
+		createRoot( element ).render( <Followers { ...attrs } /> );
 	} );
 } );
