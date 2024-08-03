56eef721dfe61f85f30c45e8bc9ee76166e41071
Daniel Hüsken <mail@danielhuesken.de>
2024-08-03 10:25:01 +0200

    moved duplicated code to a function

--- includes/functions.php
+++ includes/functions.php
@@ -1077,3 +1077,69 @@ function get_reply_intent_uri() {
 		esc_url( \admin_url( 'post-new.php?in_reply_to=' ) )
 	);
 }
+
+/**
+ * Replace content with links by Regex callback and not affect protected tags
+ *
+ * @param $content string the content that should be changed
+ * @param $regex string the regex to use
+ * @param $regex_callback callable callback for regex
+ *
+ * @return string
+ */
+function content_replace_links_by_regex( $content, $regex, $regex_callback ) {
+	// small protection against execution timeouts: limit to 1 MB
+	if ( mb_strlen( $content ) > MB_IN_BYTES ) {
+		return $content;
+	}
+	$tag_stack = array();
+	$protected_tags = array(
+		'pre',
+		'code',
+		'textarea',
+		'style',
+		'a',
+	);
+	$content_with_links = '';
+	$in_protected_tag = false;
+	foreach ( wp_html_split( $content ) as $chunk ) {
+		if ( preg_match( '#^<!--[\s\S]*-->$#i', $chunk, $m ) ) {
+			$content_with_links .= $chunk;
+			continue;
+		}
+
+		if ( preg_match( '#^<(/)?([a-z-]+)\b[^>]*>$#i', $chunk, $m ) ) {
+			$tag = strtolower( $m[2] );
+			if ( '/' === $m[1] ) {
+				// Closing tag.
+				$i = array_search( $tag, $tag_stack, true );
+				// We can only remove the tag from the stack if it is in the stack.
+				if ( false !== $i ) {
+					$tag_stack = array_slice( $tag_stack, 0, $i );
+				}
+			} else {
+				// Opening tag, add it to the stack.
+				$tag_stack[] = $tag;
+			}
+
+			// If we're in a protected tag, the tag_stack contains at least one protected tag string.
+			// The protected tag state can only change when we encounter a start or end tag.
+			$in_protected_tag = array_intersect( $tag_stack, $protected_tags );
+
+			// Never inspect tags.
+			$content_with_links .= $chunk;
+			continue;
+		}
+
+		if ( $in_protected_tag ) {
+			// Don't inspect a chunk inside an inspected tag.
+			$content_with_links .= $chunk;
+			continue;
+		}
+
+		// Only reachable when there is no protected tag in the stack.
+		$content_with_links .= \preg_replace_callback( $regex, $regex_callback, $chunk );
+	}
+
+	return $content_with_links;
+}
