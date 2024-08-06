367d259c28f7df35748a6a2a579cf3817dcc6ec4
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-08-06 16:24:56 +0200

    add some PHPDoc

--- integration/load.php
+++ integration/load.php
@@ -6,28 +6,73 @@
 namespace Activitypub\Integration;
 
 function plugin_init() {
+	/**
+	 * Adds WebFinger (plugin) support.
+	 *
+	 * This class handles the compatibility with the WebFinger plugin
+	 * and coordinates the internal WebFinger implementation.
+	 *
+	 * @see https://wordpress.org/plugins/webfinger/
+	 */
 	require_once __DIR__ . '/class-webfinger.php';
 	Webfinger::init();
 
+	/**
+	 * Adds NodeInfo (plugin) support.
+	 *
+	 * This class handles the compatibility with the NodeInfo plugin
+	 * and coordinates the internal NodeInfo implementation.
+	 *
+	 * @see https://wordpress.org/plugins/nodeinfo/
+	 */
 	require_once __DIR__ . '/class-nodeinfo.php';
 	Nodeinfo::init();
 
+	/**
+	 * Adds Enable Mastodon Apps support.
+	 *
+	 * This class handles the compatibility with the Enable Mastodon Apps plugin.
+	 *
+	 * @see https://wordpress.org/plugins/enable-mastodon-apps/
+	 */
 	require_once __DIR__ . '/class-enable-mastodon-apps.php';
 	Enable_Mastodon_Apps::init();
 
+	/**
+	 * Adds OpenGraph support.
+	 *
+	 * This class handles the compatibility with the OpenGraph plugin.
+	 *
+	 * @see https://wordpress.org/plugins/opengraph/
+	 */
 	if ( '1' === \get_option( 'activitypub_use_opengraph', '1' ) ) {
 		require_once __DIR__ . '/class-opengraph.php';
 		Opengraph::init();
 	}
 
+	/**
+	 * Adds Jetpack support.
+	 *
+	 * This class handles the compatibility with Jetpack.
+	 *
+	 * @see https://jetpack.com/
+	 */
 	if ( \defined( 'JETPACK__VERSION' ) && ! \defined( 'IS_WPCOM' ) ) {
 		require_once __DIR__ . '/class-jetpack.php';
 		Jetpack::init();
 	}
 
+	/**
+	 * Adds Seriously Simple Podcasting support.
+	 *
+	 * This class handles the compatibility with Seriously Simple Podcasting.
+	 *
+	 * @see https://wordpress.org/plugins/seriously-simple-podcasting/
+	 */
 	if ( \defined( 'SSP_VERSION' ) ) {
 		add_filter(
 			'activitypub_transformer',
+			// phpcs:ignore Universal.NamingConventions.NoReservedKeywordParameterNames.objectFound
 			function( $transformer, $object, $object_class ) {
 				if (
 					'WP_Post' === $object_class &&
@@ -46,7 +91,11 @@ function plugin_init() {
 \add_action( 'plugins_loaded', __NAMESPACE__ . '\plugin_init' );
 
 /**
+ * Load the BuddyPress integration.
+ *
  * Only load code that needs BuddyPress to run once BP is loaded and initialized.
+ *
+ * @see https://buddypress.org/
  */
 add_action(
 	'bp_include',
