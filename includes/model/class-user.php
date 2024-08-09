2aa80eb7e1d380c541305c277899e9153759aecb
Daniel Hüsken <mail@danielhuesken.de>
2024-08-09 14:40:35 +0200

    improve link detection for attachments

--- includes/model/class-user.php
+++ includes/model/class-user.php
@@ -275,15 +275,9 @@ class User extends Actor {
 			// Add support for FEP-fb2a, for more information see FEDERATION.md
 			if ( \class_exists( '\WP_HTML_Tag_Processor' ) ) {
 				$tags = new \WP_HTML_Tag_Processor( $content );
-				$tags->next_tag();
-
-				if ( 'P' === $tags->get_tag() ) {
-					$tags->next_tag();
-				}
-
-				if ( 'A' === $tags->get_tag() && $tags->get_attribute( 'href' ) ) {
+				if ( $tags->next_tag( 'A' ) && $tags->get_attribute( 'href' ) ) {
 					$tags->set_bookmark( 'link' );
-					if ( ! $tags->next_tag() || 'SPAN' === $tags->get_tag() ) {
+					if ( ! $tags->next_tag( 'A' ) ) {
 						$tags->seek( 'link' );
 						$attachment = array(
 							'type' => 'Link',
