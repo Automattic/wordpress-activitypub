afb37b9d8622b0b7892138e08f047e6cbfb7f840
Daniel Hüsken <mail@danielhuesken.de>
2024-08-14 11:38:41 +0200

    commit current state

--- integration/class-the-events-calendar.php
+++ integration/class-the-events-calendar.php
@@ -1,6 +1,7 @@
 <?php
 namespace Activitypub\Integration;
 
+use Activitypub\Activity\Extended_Object\Event;
 use Activitypub\Transformer\Post;
 
 /**
@@ -12,28 +13,14 @@ use Activitypub\Transformer\Post;
  * @see https://wordpress.org/plugins/the-events-calendar/
  */
 class The_Events_Calendar extends Post {
-	/**
-	 * Gets the attachment for a podcast episode.
-	 *
-	 * This method is overridden to add the audio file as an attachment.
-	 *
-	 * @return array The attachments array.
-	 */
-	public function get_attachment() {
-		$post        = $this->wp_object;
-		$attachments = parent::get_attachment();
 
-		return $attachments;
-	}
 
-	/**
-	 * Gets the object type for a Event.
-	 *
-	 * Always returns 'Note' for the best possible compatibility with ActivityPub.
-	 *
-	 * @return string The object type.
-	 */
-	public function get_type() {
-		return 'Event';
+	public function to_activity( $type ) {
+		$event = $this->wp_object;
+		$object = $this->to_object();
+
+		$activity = new Event();
+
+		return $activity;
 	}
 }
