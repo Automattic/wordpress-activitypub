1d6c04ad59689925c192a2fea9b0c670da7b5b1d
Daniel Hüsken <mail@danielhuesken.de>
2024-08-14 18:02:10 +0200

    add event status

--- integration/class-the-events-calendar.php
+++ integration/class-the-events-calendar.php
@@ -2,6 +2,7 @@
 namespace Activitypub\Integration;
 
 use Activitypub\Activity\Extended_Object\Event;
+use Activitypub\Transformer\Base;
 use Activitypub\Transformer\Post;
 
 /**
@@ -14,13 +15,24 @@ use Activitypub\Transformer\Post;
  */
 class The_Events_Calendar extends Post {
 
+	/**
+	 * Base constructor.
+	 *
+	 * @param WP_Post|WP_Comment $wp_object The WordPress object
+	 */
+	public function __construct( $wp_object ) {
+		$this->wp_object = \tribe_get_event( $wp_object );
+	}
 
-	public function to_activity( $type ) {
+	public function to_object() {
 		$event = $this->wp_object;
-		$object = $this->to_object();
+		//print_r($this->wp_object);
+		$object = new Event();
 
-		$activity = new Event();
+		if ( 'canceled' === $event->event_status ) {
+			$object->set_status( 'CANCELLED' );
+		}
 
-		return $activity;
+		return $object;
 	}
 }
