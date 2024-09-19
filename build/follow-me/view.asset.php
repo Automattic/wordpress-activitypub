18da65ff2974b1a036c945f23637658c2c5d56fc
Matt Wiebe <wiebe@automattic.com>
2024-09-19 15:29:12 -0500

    Follow Me/Followers: add `inherit` mode for dynamic templating (#892)
    ---------
    
    Co-authored-by: Matthias Pfefferle <pfefferle@users.noreply.github.com>
    Co-authored-by: Alex Kirk <akirk@users.noreply.github.com>

--- build/follow-me/view.asset.php
+++ build/follow-me/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ec1f2f8a193d8d386abc');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ab8c0dad126bb0a61ed6');
