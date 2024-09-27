8c0c3fa886f28d04598563d8179a2633455105b4
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-09-27 14:56:54 +0200

    the handler should not handle outgoing stuff

--- includes/class-handler.php
+++ includes/class-handler.php
@@ -18,8 +18,6 @@ class Handler {
 	 */
 	public static function init() {
 		self::register_handlers();
-
-		\add_action( 'transition_post_status', array( self::class, 'schedule_post_activity' ), 33, 3 );
 	}
 
 	/**
@@ -38,54 +36,5 @@ class Handler {
 		}
 
 		do_action( 'activitypub_register_handlers' );
-	}
-
-	/**
-	 * Schedule Activities.
-	 *
-	 * @param string  $new_status New post status.
-	 * @param string  $old_status Old post status.
-	 * @param WP_Post $post       Post object.
-	 */
-	public static function schedule_post_activity( $new_status, $old_status, $post ) {
-		// Do not send activities if post is password protected.
-		if ( \post_password_required( $post ) ) {
-			return;
-		}
-
-		// Check if post-type supports ActivityPub.
-		$post_types = \get_post_types_by_support( 'activitypub' );
-		if ( ! \in_array( $post->post_type, $post_types, true ) ) {
-			return;
-		}
-
-		$type = false;
-
-		if ( 'publish' === $new_status && 'publish' !== $old_status ) {
-			$type = 'Create';
-		} elseif ( 'publish' === $new_status ) {
-			$type = 'Update';
-		} elseif ( 'trash' === $new_status ) {
-			$type = 'Delete';
-		}
-
-		if ( ! $type ) {
-			return;
-		}
-
-		\wp_schedule_single_event(
-			\time(),
-			'activitypub_send_activity',
-			array( $post, $type )
-		);
-
-		\wp_schedule_single_event(
-			\time(),
-			sprintf(
-				'activitypub_send_%s_activity',
-				\strtolower( $type )
-			),
-			array( $post )
-		);
 	}
 }
