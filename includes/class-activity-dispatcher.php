1384bdea79eadb1c5cceff0a2ef8112dfcc3e251
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-10-25 12:18:39 +0200

    Fallback to blog-user if user is disabled
    Fallback to the blog-user if enabled and the specific user is disabled.
    
    fix #948

--- includes/class-activity-dispatcher.php
+++ includes/class-activity-dispatcher.php
@@ -71,6 +71,14 @@ class Activity_Dispatcher {
 			return;
 		}
 
+		// check if user is disabled and blog user is enabled.
+		if (
+			is_user_disabled( $transformer->get_wp_user_id() ) &&
+			! is_user_disabled( Users::BLOG_USER_ID )
+		) {
+			$transformer->change_wp_user_id( Users::BLOG_USER_ID );
+		}
+
 		if ( null !== $user_id ) {
 			$transformer->change_wp_user_id( $user_id );
 		}
