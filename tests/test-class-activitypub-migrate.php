3514966be66eac02c91fc6dea425506d89278a7c
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-10-28 15:20:22 +0100

    unit tests

--- tests/test-class-activitypub-migrate.php
+++ tests/test-class-activitypub-migrate.php
@@ -48,4 +48,19 @@ class Test_Activitypub_Migrate extends ActivityPub_TestCase_Cache_HTTP {
 
 		$this->assertEquals( ACTIVITYPUB_ACTOR_MODE, \get_option( 'activitypub_actor_mode', ACTIVITYPUB_ACTOR_MODE ) );
 	}
+	public function test_migrate_to_4_1_0() {
+		$template = \get_option( 'activitypub_custom_post_content' );
+
+		$this->assertEquals( ACTIVITYPUB_CUSTOM_POST_CONTENT, $template );
+
+		\update_option( 'activitypub_post_content_type', 'title' );
+
+		\Activitypub\Migration::migrate_to_4_1_0();
+
+		$template = \get_option( 'activitypub_custom_post_content' );
+		$type     = \get_option( 'activitypub_post_content_type' );
+
+		$this->assertEquals( "[ap_title type=\"html\"]\n\n[ap_permalink type=\"html\"]", $template );
+		$this->assertEquals( null, $type );
+	}
 }
