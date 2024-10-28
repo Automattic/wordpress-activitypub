214692f4b1ba399b1f8b27c540af2d2db00f023b
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-10-28 16:32:48 +0100

    update unit-tests

--- tests/test-class-activitypub-migrate.php
+++ tests/test-class-activitypub-migrate.php
@@ -48,6 +48,7 @@ class Test_Activitypub_Migrate extends ActivityPub_TestCase_Cache_HTTP {
 
 		$this->assertEquals( ACTIVITYPUB_ACTOR_MODE, \get_option( 'activitypub_actor_mode', ACTIVITYPUB_ACTOR_MODE ) );
 	}
+
 	public function test_migrate_to_4_1_0() {
 		$template = \get_option( 'activitypub_custom_post_content' );
 
@@ -61,6 +62,17 @@ class Test_Activitypub_Migrate extends ActivityPub_TestCase_Cache_HTTP {
 		$type     = \get_option( 'activitypub_post_content_type' );
 
 		$this->assertEquals( "[ap_title type=\"html\"]\n\n[ap_permalink type=\"html\"]", $template );
-		$this->assertEquals( null, $type );
+		$this->assertFalse( $type );
+
+		\update_option( 'activitypub_post_content_type', 'content' );
+		\update_option( 'activitypub_custom_post_content', '[ap_content]' );
+
+		\Activitypub\Migration::migrate_to_4_1_0();
+
+		$template = \get_option( 'activitypub_custom_post_content' );
+		$type     = \get_option( 'activitypub_post_content_type' );
+
+		$this->assertEquals( "[ap_content]\n\n[ap_permalink type=\"html\"]\n\n[ap_hashtags]", $template );
+		$this->assertFalse( $type );
 	}
 }
