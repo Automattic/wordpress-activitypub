eb7f5f52d05deebadc9fd93e02ad28f3fc998b83
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-10-29 08:43:23 +0100

    use the $user_id from the Inbox-Request

--- includes/handler/class-undo.php
+++ includes/handler/class-undo.php
@@ -23,16 +23,19 @@ class Undo {
 	public static function init() {
 		\add_action(
 			'activitypub_inbox_undo',
-			array( self::class, 'handle_undo' )
+			array( self::class, 'handle_undo' ),
+			10,
+			2
 		);
 	}
 
 	/**
 	 * Handle "Unfollow" requests.
 	 *
-	 * @param array $activity The JSON "Undo" Activity.
+	 * @param array    $activity The JSON "Undo" Activity.
+	 * @param int|null $user_id  The ID of the user who initiated the "Undo" activity.
 	 */
-	public static function handle_undo( $activity ) {
+	public static function handle_undo( $activity, $user_id ) {
 		if (
 			! isset( $activity['object']['type'] ) ||
 			! isset( $activity['object']['object'] )
@@ -40,14 +43,13 @@ class Undo {
 			return;
 		}
 
-		$type    = $activity['object']['type'];
-		$state   = false;
-		$user_id = null;
+		$type  = $activity['object']['type'];
+		$state = false;
 
 		// Handle "Unfollow" requests.
 		if ( 'Follow' === $type ) {
-			$user_id = object_to_uri( $activity['object']['object'] );
-			$user    = Users::get_by_resource( $user_id );
+			$id   = object_to_uri( $activity['object']['object'] );
+			$user = Users::get_by_resource( $id );
 
 			if ( ! $user || is_wp_error( $user ) ) {
 				// If we can not find a user, we can not initiate a follow process.
