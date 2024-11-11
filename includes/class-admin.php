e4dff50e52d0ca7cc8075f9736a505d167ba9574
Jeremy Herve <jeremy@jeremy.hu>
2024-11-11 15:50:22 +0100

    Use the custom activitypub cap for this scenario
    Co-authored-by: Matthias Pfefferle <pfefferle@users.noreply.github.com>

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -72,7 +72,7 @@ class Admin {
 			$followers_list_page = \add_users_page(
 				\__( '⁂ Followers', 'activitypub' ),
 				\__( '⁂ Followers', 'activitypub' ),
-				'edit_posts',
+				'activitypub',
 				'activitypub-followers-list',
 				array(
 					self::class,
@@ -88,7 +88,7 @@ class Admin {
 			\add_users_page(
 				\__( '⁂ Extra Fields', 'activitypub' ),
 				\__( '⁂ Extra Fields', 'activitypub' ),
-				'edit_posts',
+				'activitypub',
 				\esc_url( \admin_url( '/edit.php?post_type=ap_extrafield' ) )
 			);
 		}
