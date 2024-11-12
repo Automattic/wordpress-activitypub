939ae850bc3b838209f01b52f3d657e032185a29
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-11-12 17:38:31 +0100

    never used

--- includes/collection/class-actors.php
+++ includes/collection/class-actors.php
@@ -65,12 +65,6 @@ class Actors {
 			default:
 				return User::from_wp_user( $user_id );
 		}
-
-		return new WP_Error(
-			'activitypub_user_not_found',
-			\__( 'Actor not found', 'activitypub' ),
-			array( 'status' => 404 )
-		);
 	}
 
 	/**
