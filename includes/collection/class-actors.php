1bfc5191506dde02b33444c9fe3b8c60f95be2f3
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-11-12 17:36:42 +0100

    Update includes/collection/class-actors.php
    Co-authored-by: Konstantin Obenland <obenland@automattic.com>

--- includes/collection/class-actors.php
+++ includes/collection/class-actors.php
@@ -240,7 +240,7 @@ class Actors {
 	/**
 	 * Get the Actor by resource.
 	 *
-	 * @param string $id The Actor-Resource.
+	 * @param string $id The Actor resource.
 	 *
 	 * @return User|Blog|Application|WP_Error The Actor or WP_Error if user not found.
 	 */
