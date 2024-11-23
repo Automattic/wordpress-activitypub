6c1874059976e66c3e4254f69257d56bf57a7c0b
Matt Wiebe <wiebe@automattic.com>
2024-11-23 09:07:51 -0600

    oops

--- includes/class-migration.php
+++ includes/class-migration.php
@@ -278,7 +278,7 @@ class Migration {
 		self::update_usermeta_key( 'activitypub_user_description', 'activitypub_description' );
 
 		self::update_options_key( 'activitypub_blog_user_description', 'activitypub_blog_description' );
-		self::update_options_key( 'activitypub_BLOG_ACTOR_IDentifier', 'activitypub_blog_identifier' );
+		self::update_options_key( 'activitypub_blog_user_identifier', 'activitypub_blog_identifier' );
 	}
 
 	/**
