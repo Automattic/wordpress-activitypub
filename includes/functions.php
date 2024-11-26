49cfbc6be323f492cb3b0a53d7dcb42ebb310a3e
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-11-26 10:37:06 +0100

    * Fixed: Missing attachement-type for enclosures

--- includes/functions.php
+++ includes/functions.php
@@ -1015,7 +1015,7 @@ function get_enclosures( $post_id ) {
 			return array(
 				'url'       => $attributes[0],
 				'length'    => isset( $attributes[1] ) ? trim( $attributes[1] ) : null,
-				'mediaType' => isset( $attributes[2] ) ? trim( $attributes[2] ) : null,
+				'mediaType' => isset( $attributes[2] ) ? trim( $attributes[2] ) : 'application/octet-stream',
 			);
 		},
 		$enclosures
--- readme.txt
+++ readme.txt
@@ -142,6 +142,7 @@ For reasons of data protection, it is not possible to see the followers of other
 * Improved: Better handling of `readme.txt` and `README.md`
 * Fixed: Fediverse preview showing `preferredUsername` instead of `name`
 * Fixed: Potential fatal error in Enable Mastodon Apps
+* Fixed: Missing attachement-type for enclosures
 
 = 4.2.1 =
 
