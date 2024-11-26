04898e4edf79f164e2ecf401c6ef96bdaeb34708
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-11-26 17:39:12 +0100

    simplify code

--- includes/functions.php
+++ includes/functions.php
@@ -1014,8 +1014,8 @@ function get_enclosures( $post_id ) {
 
 			return array(
 				'url'       => $attributes[0],
-				'length'    => isset( $attributes[1] ) ? trim( $attributes[1] ) : null,
-				'mediaType' => isset( $attributes[2] ) ? trim( $attributes[2] ) : 'application/octet-stream',
+				'length'    => $attributes[1] ?? null,
+				'mediaType' => $attributes[2] ?? 'application/octet-stream',
 			);
 		},
 		$enclosures
