e64b6bec8385804c919f8a552fe48a74a4f4d676
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-12-09 18:30:56 +0100

    remove history link from all ActivityPub links

---
+++ integration/class-akismet.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Akismet integration.
+ *
+ * @package Activitypub
+ */
+
+namespace Activitypub\Integration;
+
+/**
+ * Compatibility with the Akismet plugin.
+ *
+ * @see https://wordpress.org/plugins/akismet/
+ */
+class Akismet {
+	/**
+	 * Initialize the class, registering WordPress hooks.
+	 */
+	public static function init() {
+		\add_filter( 'comment_row_actions', array( self::class, 'comment_row_actions' ), 10, 2 );
+	}
+
+	/**
+	 * Remove the "history" action from the comment row actions.
+	 *
+	 * @param array           $actions The existing actions.
+	 * @param int|\WP_Comment $comment The comment object or ID.
+	 *
+	 * @return array The modified actions.
+	 */
+	public static function comment_row_actions( $actions, $comment ) {
+		if ( 'activitypub' === \get_comment_meta( $comment->comment_ID, 'protocol', true ) ) {
+			unset( $actions['history'] );
+		}
+
+		return $actions;
+	}
+}
