ef3f3a8c87f2f3be16dc65d9e9fea0e58b386e88
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-12-09 18:31:36 +0100

    remove reply links from likes and announces

--- includes/class-admin.php
+++ includes/class-admin.php
@@ -594,9 +594,19 @@ class Admin {
 	 * @return array The modified actions.
 	 */
 	public static function comment_row_actions( $actions, $comment ) {
+		global $activitypub_comment_types;
+
 		if ( was_comment_received( $comment ) ) {
 			unset( $actions['edit'] );
 			unset( $actions['quickedit'] );
+		}
+
+		$comment_type = get_comment_type( $comment );
+
+		foreach ( $activitypub_comment_types as $type ) {
+			if ( $type['type'] === $comment_type ) {
+				unset( $actions['reply'] );
+			}
 		}
 
 		return $actions;
