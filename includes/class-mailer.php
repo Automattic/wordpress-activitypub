1311315076e7a0278d2f435d7993c6cd502ac9e5
Konstantin Obenland <obenland@gmx.de>
2024-12-10 15:22:14 -0600

    Keep bottom line URLs in separate line

--- includes/class-mailer.php
+++ includes/class-mailer.php
@@ -91,18 +91,14 @@ class Mailer {
 		$comment_author_domain = \gethostbyaddr( $comment->comment_author_IP );
 
 		/* translators: 1: Comment type, 2: Post title */
-		$notify_message = \sprintf( esc_html__( 'New %1$s on your post &#8220;%2$s&#8221;', 'activitypub' ), \esc_html( $comment_type['singular'] ), \esc_html( $post->post_title ) ) . "\r\n\r\n";
+		$notify_message = \sprintf( html_entity_decode( esc_html__( 'New %1$s on your post &#8220;%2$s&#8221;.', 'activitypub' ) ), \esc_html( $comment_type['singular'] ), \esc_html( $post->post_title ) ) . "\r\n\r\n";
 		/* translators: 1: Website name, 2: Website IP address, 3: Website hostname. */
-		$notify_message .= \sprintf( esc_html__( 'From: %1$s (IP address: %2$s, %3$s)', 'activitypub' ), \esc_html( $comment->comment_author ), \esc_html( $comment->comment_author_IP ), \esc_html( $comment_author_domain ) ) . "\r\n";
+		$notify_message .= \sprintf( \esc_html__( 'From: %1$s (IP address: %2$s, %3$s)', 'activitypub' ), \esc_html( $comment->comment_author ), \esc_html( $comment->comment_author_IP ), \esc_html( $comment_author_domain ) ) . "\r\n";
 		/* translators: Reaction author URL. */
-		$notify_message .= \sprintf( esc_html__( 'URL: %s', 'activitypub' ), \esc_url( $comment->comment_author_url ) ) . "\r\n\r\n";
-
-		$notify_message .= \sprintf(
-			/* translators: 1: Comment type label, 2: comment permalink */
-			esc_html__( 'You can see all %1$s on this post here: %2$s', 'activitypub' ),
-			\esc_html( $comment_type['label'] ),
-			\get_permalink( $comment->comment_post_ID ) . '#' . \esc_attr( $comment_type['type'] )
-		) . "\r\n\r\n";
+		$notify_message .= \sprintf( \esc_html__( 'URL: %s', 'activitypub' ), \esc_url( $comment->comment_author_url ) ) . "\r\n\r\n";
+		/* translators: Comment type label */
+		$notify_message .= \sprintf( \esc_html__( 'You can see all %s on this post here:', 'activitypub' ), \esc_html( $comment_type['label'] ) ) . "\r\n";
+		$notify_message .= \get_permalink( $comment->comment_post_ID ) . '#' . \esc_attr( $comment_type['type'] ) . "\r\n\r\n";
 
 		return $notify_message;
 	}
@@ -134,11 +130,11 @@ class Mailer {
 		/* translators: 1: Blog name, 2: Follower name */
 		$subject = \sprintf( \esc_html__( '[%1$s] Follower: %2$s', 'activitypub' ), \esc_html( get_option( 'blogname' ) ), \esc_html( $actor['name'] ) );
 		/* translators: 1: Blog name, 2: Follower name */
-		$message = \sprintf( \esc_html__( 'New Follower: %2$s', 'activitypub' ), \esc_html( get_option( 'blogname' ) ), \esc_html( $actor['name'] ) ) . "\r\n\r\n";
+		$message = \sprintf( \esc_html__( 'New Follower: %2$s.', 'activitypub' ), \esc_html( get_option( 'blogname' ) ), \esc_html( $actor['name'] ) ) . "\r\n\r\n";
 		/* translators: Follower URL */
 		$message .= \sprintf( \esc_html__( 'URL: %s', 'activitypub' ), \esc_url( $actor['url'] ) ) . "\r\n\r\n";
-		/* translators: Followers list URL */
-		$message .= \sprintf( \esc_html__( 'You can see all followers here: %s', 'activitypub' ), \esc_url( \admin_url( '/users.php?page=activitypub-followers-list' ) ) ) . "\r\n\r\n";
+		$message .= \esc_html__( 'You can see all followers here:', 'activitypub' ) . "\r\n";
+		$message .= \esc_url( \admin_url( '/users.php?page=activitypub-followers-list' ) ) . "\r\n\r\n";
 
 		\wp_mail( $email, $subject, $message );
 	}
