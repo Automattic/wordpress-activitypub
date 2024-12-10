c8ee5213888bc1d6abd19e212197de845afd6561
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-12-10 19:23:19 +0100

    this is not wp_kses

--- includes/class-mailer.php
+++ includes/class-mailer.php
@@ -174,7 +174,7 @@ class Mailer {
 		/* translators: 1: Blog name, 2 Actor name */
 		$subject = \sprintf( \__( '[%1$s] Direct Message from: %2$s', 'activitypub' ), get_option( 'blogname' ), \esc_html( $actor['name'] ) );
 		/* translators: 1: Blog name, 2: Actor name */
-		$message = \sprintf( \__( 'New Direct Message: %2$s', 'activitypub' ), get_option( 'blogname' ), \wp_strip_all_tags( $activity['object']['content'], 'allowed_comment_html' ) ) . "\r\n\r\n";
+		$message = \sprintf( \__( 'New Direct Message: %2$s', 'activitypub' ), get_option( 'blogname' ), \wp_strip_all_tags( $activity['object']['content'] ) ) . "\r\n\r\n";
 		/* translators: %s: Actor name */
 		$message .= \sprintf( __( 'From: %s', 'activitypub' ), \esc_html( $actor['name'] ) ) . "\r\n";
 		/* translators: %s: Actor URL */
