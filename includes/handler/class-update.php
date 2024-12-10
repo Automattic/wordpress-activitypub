59a7fdc56fa4defc3a900a79448db931c96302ee
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-12-10 08:23:50 +0100

    revert notification

--- includes/handler/class-update.php
+++ includes/handler/class-update.php
@@ -2,10 +2,8 @@
 namespace Activitypub\Handler;
 
 use WP_Error;
-use Activitypub\Model\Notification;
 use Activitypub\Collection\Interactions;
 
-use function Activitypub\is_activity_public;
 use function Activitypub\get_remote_metadata_by_actor;
 
 /**
@@ -68,20 +66,6 @@ class Update {
 	 * @return void
 	 */
 	public static function update_interaction( $activity ) {
-		// check if Activity is public or not
-		if ( ! is_activity_public( $activity ) ) {
-			// send notification
-			$notification = new Notification(
-				'create',
-				$activity['actor'],
-				$activity,
-				$user_id
-			);
-			$notification->send();
-
-			return;
-		}
-
 		$commentdata = Interactions::update_comment( $activity );
 		$reaction    = null;
 
