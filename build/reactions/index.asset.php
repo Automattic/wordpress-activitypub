a6fb9f777b7ccb1a09f41a7dc22c2cd20c11e2d7
Matt Wiebe <wiebe@automattic.com>
2024-12-10 15:54:10 -0600

    built files

---
+++ build/reactions/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'a1d5717ae5a8c5be9af7');
