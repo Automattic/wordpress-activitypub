11be60698a610dc4dd1df63c0498db159d6c0cff
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2024-12-12 11:04:25 +0100

    Add a CLI to generate some demo reactions
    This helps with testing new features!

--- includes/class-cli.php
+++ includes/class-cli.php
@@ -9,6 +9,7 @@ namespace Activitypub;
 
 use WP_CLI;
 use WP_CLI_Command;
+use WP_CLI\Utils;
 
 /**
  * WP-CLI commands.
@@ -126,6 +127,112 @@ class Cli extends WP_CLI_Command {
 				break;
 			default:
 				WP_CLI::error( 'Unknown action.' );
+		}
+	}
+
+	/**
+	 * Generates some number of new dummy ActivityPub reactions.
+	 *
+	 * Creates a specified number of new ActivityPub reactions with dummy data.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <action>
+	 * : The action to perform.
+	 * ---
+	 * options:
+	 *   - comments
+	 * ---
+	 *
+	 * [--count=<number>]
+	 * : How many ActivityPub reactions to generate?
+	 * ---
+	 * default: 100
+	 * ---
+	 *
+	 * [--post_id=<post-id>]
+	 * : Assign ActivityPub reactions to a specific post.
+	 *
+	 * [--format=<format>]
+	 * : Render output in a particular format.
+	 * ---
+	 * default: progress
+	 * options:
+	 *   - progress
+	 *   - ids
+	 * ---
+	 *
+	 * ## EXAMPLES
+	 *
+	 *     # Generate comments for the given post.
+	 *     $ wp activitypub generate comments --format=ids --count=3 --post_id=123
+	 *     138 139 140
+	 */
+	public function generate( $args, $assoc_args ) {
+		switch ( $args[0] ) {
+			case 'comments':
+				$this->generate_comments( $args, $assoc_args );
+				break;
+			default:
+				WP_CLI::error( 'Unknown action.' );
+		}
+	}
+
+	/**
+	 * Generate demo comments and reactions.
+	 *
+	 * @param array $args The arguments.
+	 * @param array $assoc_args The associative arguments.
+	 */
+	private function generate_comments( $args, $assoc_args ) {
+		$defaults   = array(
+			'count'   => 100,
+			'post_id' => 0,
+		);
+
+		$assoc_args = array_merge( $defaults, $assoc_args );
+
+		$format = Utils\get_flag_value( $assoc_args, 'format', 'progress' );
+
+		$notify = false;
+		if ( 'progress' === $format ) {
+			$notify = Utils\make_progress_bar( 'Generating comments', $assoc_args['count'] );
+		}
+
+		$comment_count = wp_count_comments();
+		$total         = (int) $comment_count->total_comments;
+		$limit         = $total + $assoc_args['count'];
+
+		for ( $index = $total; $index < $limit; $index++ ) {
+			$comment_types   = Comment::get_comment_type_names();
+			$comment_types[] = 'comment';
+
+			$comment_type = $comment_types[ array_rand( $comment_types ) ];
+
+			$comment_id = wp_insert_comment(
+				array(
+					'comment_content' => "{$comment_type} {$index}",
+					'comment_post_ID' => $assoc_args['post_id'],
+					'comment_type'    => $comment_type,
+					'comment_meta'    => array(
+						'protocol'  => 'activitypub',
+						'avatar'    => "https://i.pravatar.cc/80?u={$index}",
+						'source_id' => "https://example.org/canonical/{$index}",
+					),
+				)
+			);
+			if ( 'progress' === $format ) {
+				$notify->tick();
+			} elseif ( 'ids' === $format ) {
+				echo $comment_id;
+				if ( $index < $limit - 1 ) {
+					echo ' ';
+				}
+			}
+		}
+
+		if ( 'progress' === $format ) {
+			$notify->finish();
 		}
 	}
 }
