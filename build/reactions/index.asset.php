6fa685754d3e44879dd6b67634a95e66decd720d
Matt Wiebe <wiebe@automattic.com>
2024-12-13 14:17:03 -0600

    built reactions

--- build/reactions/index.asset.php
+++ build/reactions/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '55ab7d62d199fbe4d320');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f4c4522c7203fe3786f4');
--- build/reactions/view.asset.php
+++ build/reactions/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '29431b14122db1380a21');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '0e2791f56cd75776751b');
