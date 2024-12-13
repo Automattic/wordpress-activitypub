5f48511f7cd26b88bf8d0ba445cd9c2cbd703f72
Matt Wiebe <wiebe@automattic.com>
2024-12-13 16:34:28 -0600

    fix tests

--- tests/includes/class-test-federated-reactions-settings.php
+++ tests/includes/class-test-federated-reactions-settings.php
@@ -22,8 +22,6 @@ class Test_Federated_Reactions_Settings extends \WP_UnitTestCase {
 	public function tear_down() {
 		parent::tear_down();
 		delete_option( 'activitypub_reactions_enabled' );
-		$GLOBALS['wp_meta_boxes'] = array();
-		$_POST                    = array();
 	}
 
 	/**
@@ -34,8 +32,6 @@ class Test_Federated_Reactions_Settings extends \WP_UnitTestCase {
 	public function test_init() {
 		$this->assertEquals( 11, has_action( 'init', array( Federated_Reactions_Settings::class, 'register_post_meta' ) ) );
 		$this->assertEquals( 10, has_action( 'admin_init', array( Federated_Reactions_Settings::class, 'register_settings' ) ) );
-		$this->assertEquals( 10, has_action( 'add_meta_boxes', array( Federated_Reactions_Settings::class, 'add_meta_box' ) ) );
-		$this->assertEquals( 10, has_action( 'save_post', array( Federated_Reactions_Settings::class, 'meta_box_save' ) ) );
 	}
 
 	/**
@@ -106,91 +102,6 @@ class Test_Federated_Reactions_Settings extends \WP_UnitTestCase {
 	}
 
 	/**
-	 * Test adding meta box.
-	 *
-	 * @covers ::add_meta_box
-	 */
-	public function test_add_meta_box() {
-		global $wp_meta_boxes;
-
-		Federated_Reactions_Settings::add_meta_box();
-
-		$this->assertArrayHasKey( 'post', $wp_meta_boxes );
-		$this->assertArrayHasKey( 'side', $wp_meta_boxes['post'] );
-		$this->assertArrayHasKey( 'default', $wp_meta_boxes['post']['side'] );
-		$this->assertArrayHasKey( 'activitypub_reactions', $wp_meta_boxes['post']['side']['default'] );
-	}
-
-	/**
-	 * Test rendering of meta box.
-	 *
-	 * @covers ::render_meta_box
-	 */
-	public function test_render_meta_box() {
-		$post = self::factory()->post->create_and_get();
-
-		// Test with default value (no meta set).
-		update_option( 'activitypub_reactions_enabled', '1' );
-		ob_start();
-		Federated_Reactions_Settings::render_meta_box( $post );
-		$output = ob_get_clean();
-
-		$this->assertStringContainsString( '<input type="checkbox"', $output );
-		$this->assertStringContainsString( 'name="activitypub_reactions_enabled"', $output );
-		$this->assertStringContainsString( 'value="1"', $output );
-		$this->assertStringContainsString( 'checked=\'checked\'', $output );
-
-		// Test with meta value set.
-		update_post_meta( $post->ID, 'activitypub_reactions_enabled', '0' );
-		ob_start();
-		Federated_Reactions_Settings::render_meta_box( $post );
-		$output = ob_get_clean();
-
-		$this->assertStringContainsString( '<input type="checkbox"', $output );
-		$this->assertStringContainsString( 'name="activitypub_reactions_enabled"', $output );
-		$this->assertStringContainsString( 'value="1"', $output );
-		$this->assertStringNotContainsString( 'checked=\'checked\'', $output );
-	}
-
-	/**
-	 * Test saving meta box data.
-	 *
-	 * @covers ::meta_box_save
-	 */
-	public function test_meta_box_save() {
-		$post    = self::factory()->post->create_and_get();
-		$user_id = self::factory()->user->create( array( 'role' => 'administrator' ) );
-		wp_set_current_user( $user_id );
-
-		// Test with missing nonce.
-		$_POST = array();
-		Federated_Reactions_Settings::meta_box_save( $post->ID );
-		$this->assertEmpty( get_post_meta( $post->ID, 'activitypub_reactions_enabled', true ) );
-
-		// Test with invalid nonce.
-		$_POST = array(
-			'activitypub_reactions_meta_box_nonce' => 'invalid',
-		);
-		Federated_Reactions_Settings::meta_box_save( $post->ID );
-		$this->assertEmpty( get_post_meta( $post->ID, 'activitypub_reactions_enabled', true ) );
-
-		// Test with valid data - checkbox checked.
-		$_POST = array(
-			'activitypub_reactions_meta_box_nonce' => wp_create_nonce( 'activitypub_reactions_meta_box' ),
-			'activitypub_reactions_enabled'        => '1',
-		);
-		Federated_Reactions_Settings::meta_box_save( $post->ID );
-		$this->assertEquals( '1', get_post_meta( $post->ID, 'activitypub_reactions_enabled', true ) );
-
-		// Test with valid data - checkbox unchecked.
-		$_POST = array(
-			'activitypub_reactions_meta_box_nonce' => wp_create_nonce( 'activitypub_reactions_meta_box' ),
-		);
-		Federated_Reactions_Settings::meta_box_save( $post->ID );
-		$this->assertEquals( '0', get_post_meta( $post->ID, 'activitypub_reactions_enabled', true ) );
-	}
-
-	/**
 	 * Test checking if reactions are enabled.
 	 *
 	 * @covers ::is_reactions_enabled
@@ -199,18 +110,17 @@ class Test_Federated_Reactions_Settings extends \WP_UnitTestCase {
 		$post = self::factory()->post->create_and_get();
 
 		// Test with default global setting.
-		update_option( 'activitypub_reactions_enabled', '1' );
 		$this->assertTrue( Federated_Reactions_Settings::is_reactions_enabled( $post->ID ) );
 
-		// Test with disabled global setting.
+		// Test with global setting disabled.
 		update_option( 'activitypub_reactions_enabled', '0' );
 		$this->assertFalse( Federated_Reactions_Settings::is_reactions_enabled( $post->ID ) );
 
-		// Test with post meta overriding global setting.
+		// Test with global setting enabled but post setting disabled.
 		update_option( 'activitypub_reactions_enabled', '1' );
 		update_post_meta( $post->ID, 'activitypub_reactions_enabled', '0' );
 		$this->assertFalse( Federated_Reactions_Settings::is_reactions_enabled( $post->ID ) );
-
+		
 		update_option( 'activitypub_reactions_enabled', '0' );
 		update_post_meta( $post->ID, 'activitypub_reactions_enabled', '1' );
 		$this->assertTrue( Federated_Reactions_Settings::is_reactions_enabled( $post->ID ) );
