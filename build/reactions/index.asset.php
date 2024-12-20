d0b27b2025646089ee85a076ef7902f81f38c2de
Konstantin Obenland <obenland@gmx.de>
2024-12-20 08:25:14 -0600

    Update with latest build

--- build/reactions/index.asset.php
+++ build/reactions/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'da6dfc9f5f27770a7d45');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dbcb7afdfaa6ae6975c0');
--- build/reactions/view.asset.php
+++ build/reactions/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => 'd288cb356be2059aab95');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '1d4b9bd742ccb2da7be6');
