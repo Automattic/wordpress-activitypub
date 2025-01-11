bfa76dd42ca616d07dd236c1cf1e79e20b25fcc0
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-01-11 16:04:56 +0100

    fix number of params

--- includes/class-activitypub.php
+++ includes/class-activitypub.php
@@ -50,10 +50,9 @@ class Activitypub {
 		}
 
 		\add_filter( 'activitypub_get_actor_extra_fields', array( Extra_Fields::class, 'default_actor_extra_fields' ), 10, 2 );
+		\add_filter( 'pre_render_activitypub_template', array( self::class, 'add_headers' ) );
 
 		\add_action( 'updated_postmeta', array( self::class, 'updated_postmeta' ), 10, 4 );
-
-		\add_filter( 'pre_render_activitypub_template', array( self::class, 'add_headers' ), 10, 3 );
 
 		// Register several post_types.
 		self::register_post_types();
