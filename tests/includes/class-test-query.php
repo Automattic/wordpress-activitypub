c15e134500b8e73af1f84e7a2a5f731711edb903
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-01-14 17:24:09 +0100

    fix phpcs

--- tests/includes/class-test-query.php
+++ tests/includes/class-test-query.php
@@ -79,14 +79,13 @@ class Test_Query extends WP_UnitTestCase {
 	 * @covers ::has_activitypub_object
 	 */
 	public function test_has_activitypub_object() {
+		// Test with no queried object.
 		Query::get_instance()->__destruct();
 		$this->go_to( site_url( '/404' ) );
-
-		// Test with no queried object
 		$this->assertFalse( Query::get_instance()->has_activitypub_object() );
 
+		// Set up post query.
 		Query::get_instance()->__destruct();
-		// Set up post query
 		$this->go_to( get_permalink( self::$post_id ) );
 
 		$this->assertTrue( Query::get_instance()->has_activitypub_object() );
@@ -98,8 +97,8 @@ class Test_Query extends WP_UnitTestCase {
 	 * @covers ::get_activitypub_object
 	 */
 	public function test_get_activitypub_object() {
+		// Test with post.
 		Query::get_instance()->__destruct();
-		// Set up post query
 		$this->go_to( get_permalink( self::$post_id ) );
 		$query = Query::get_instance();
 
@@ -114,13 +113,13 @@ class Test_Query extends WP_UnitTestCase {
 	 * @covers ::get_activitypub_object_id
 	 */
 	public function test_get_activitypub_object_id() {
+		// Test with no queried object.
 		Query::get_instance()->__destruct();
-		// Test with no queried object
 		$query = Query::get_instance();
 		$this->assertNull( $query->get_activitypub_object_id() );
 
+		// Set up post query.
 		Query::get_instance()->__destruct();
-		// Set up post query
 		$this->go_to( get_permalink( self::$post_id ) );
 		$query = Query::get_instance();
 
@@ -133,20 +132,19 @@ class Test_Query extends WP_UnitTestCase {
 	 * @covers ::get_queried_object
 	 */
 	public function test_get_queried_object() {
+		// Test with post.
 		Query::get_instance()->__destruct();
-
-		// Test with post
 		$this->go_to( get_permalink( self::$post_id ) );
-		$query = Query::get_instance();
+		$query  = Query::get_instance();
 		$object = $query->get_queried_object();
 
 		$this->assertInstanceOf( 'WP_Post', $object );
 		$this->assertEquals( self::$post_id, $object->ID );
 
+		// Test with author.
 		Query::get_instance()->__destruct();
-		// Test with author
 		$this->go_to( get_author_posts_url( self::$user_id ) );
-		$query = Query::get_instance();
+		$query  = Query::get_instance();
 		$object = $query->get_queried_object();
 
 		$this->assertInstanceOf( 'WP_User', $object );
@@ -159,23 +157,22 @@ class Test_Query extends WP_UnitTestCase {
 	 * @covers ::is_activitypub_request
 	 */
 	public function test_is_activitypub_request() {
+		// Test without ActivityPub headers.
 		Query::get_instance()->__destruct();
-		// Test without ActivityPub headers
 		$this->assertFalse( Query::get_instance()->is_activitypub_request() );
 
+		// Test with ActivityPub query var.
 		Query::get_instance()->__destruct();
-		// Test with ActivityPub query var
+		$this->go_to( get_permalink( self::$post_id ) );
 		set_query_var( 'activitypub', '1' );
 		$this->assertTrue( Query::get_instance()->is_activitypub_request() );
 		set_query_var( 'activitypub', '' );
 
+		// Test with Accept header.
 		Query::get_instance()->__destruct();
-		// Test with Accept header
 		$_SERVER['HTTP_ACCEPT'] = 'application/activity+json';
 		$this->go_to( get_permalink( self::$post_id ) );
 		$this->assertTrue( Query::get_instance()->is_activitypub_request() );
-
-
 
 		Query::get_instance()->__destruct();
 		$_SERVER['HTTP_ACCEPT'] = 'application/ld+json';
@@ -202,18 +199,18 @@ class Test_Query extends WP_UnitTestCase {
 	 */
 	public function test_maybe_get_virtual_object() {
 		$reflection = new \ReflectionClass( Query::class );
-		$method = $reflection->getMethod( 'maybe_get_virtual_object' );
+		$method     = $reflection->getMethod( 'maybe_get_virtual_object' );
 		$method->setAccessible( true );
 
 		$query = Query::get_instance();
 
-		// Test with invalid URL
+		// Test with invalid URL.
 		$_SERVER['REQUEST_URI'] = '/invalid/url';
 		$this->assertNull( $method->invoke( $query ) );
 
-		// Test with author URL
+		// Test with author URL.
 		$_SERVER['REQUEST_URI'] = '/?author=' . self::$user_id;
-		$object = $method->invoke( $query );
+		$object                 = $method->invoke( $query );
 		$this->assertNotNull( $object );
 		$this->assertEquals( get_author_posts_url( self::$user_id ), $object->get_id() );
 
