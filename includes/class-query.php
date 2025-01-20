086cb19a041c5b213973b8599e115dbaaeac610d
Konstantin Obenland <obenland@gmx.de>
2025-01-20 09:45:10 -0600

    Align asterisks in block comments

--- includes/class-query.php
+++ includes/class-query.php
@@ -235,20 +235,20 @@ class Query {
 		}
 
 		/*
-		* The other (more common) option to make an ActivityPub request
-		* is to send an Accept header.
-		*/
+		 * The other (more common) option to make an ActivityPub request
+		 * is to send an Accept header.
+		 */
 		if ( isset( $_SERVER['HTTP_ACCEPT'] ) ) {
 			$accept = \sanitize_text_field( \wp_unslash( $_SERVER['HTTP_ACCEPT'] ) );
 
 			/*
-			* $accept can be a single value, or a comma separated list of values.
-			* We want to support both scenarios,
-			* and return true when the header includes at least one of the following:
-			* - application/activity+json
-			* - application/ld+json
-			* - application/json
-			*/
+			 * $accept can be a single value, or a comma separated list of values.
+			 * We want to support both scenarios,
+			 * and return true when the header includes at least one of the following:
+			 * - application/activity+json
+			 * - application/ld+json
+			 * - application/json
+			 */
 			if ( \preg_match( '/(application\/(ld\+json|activity\+json|json))/i', $accept ) ) {
 				$this->is_activitypub_request = true;
 
