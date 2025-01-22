904848b1e95efcc13c067aa1bad2aff284d5d21a
Konstantin Obenland <obenland@gmx.de>
2025-01-22 11:25:16 -0600

    Account for inheritance in Transformer Factory
    Reverts changes to `add_to_outbox()`

--- includes/transformer/class-factory.php
+++ includes/transformer/class-factory.php
@@ -7,6 +7,7 @@
 
 namespace Activitypub\Transformer;
 
+use Activitypub\Activity\Base_Object;
 use WP_Error;
 
 use function Activitypub\is_user_disabled;
@@ -93,10 +94,12 @@ class Factory {
 					return new User( $data );
 				}
 				break;
-			case 'Base_Object':
-				return new Activity_Object( $data );
 			case 'json':
 				return new Json( $data );
+		}
+
+		if ( $data instanceof Base_Object ) {
+			return new Activity_Object( $data );
 		}
 
 		return new WP_Error( 'invalid_object', __( 'Invalid object', 'activitypub' ) );
