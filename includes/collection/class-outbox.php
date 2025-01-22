c28d03e0d5a3ed73d99658b25a63294100356fdf
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-01-22 19:33:22 +0100

    Update includes/collection/class-outbox.php
    Co-authored-by: André Menrath <99024746+Menrath@users.noreply.github.com>

--- includes/collection/class-outbox.php
+++ includes/collection/class-outbox.php
@@ -20,7 +20,7 @@ class Outbox {
 	 *
 	 * @param \Activitypub\Activity\Base_Object $activity_object    The Activity-Object  to add as JSON.
 	 * @param string                            $activity_type      The activity type.
-	 * @param int                               $user_id            The user ID.
+	 * @param int                               $user_id            The real or imaginary user ID of the actor that published the activity that will be added to the outbox.
 	 * @param string                            $content_visibility Optional. The visibility of the content. Default 'public'.
 	 *
 	 * @return false|int|\WP_Error The added item or an error.
