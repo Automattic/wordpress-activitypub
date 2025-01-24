f3e054c5e493caf4ab9bfcc0bd1671e5edeff29c
Matt Wiebe <wiebe@automattic.com>
2025-01-24 13:48:14 -0600

    oops

--- activitypub.php
+++ activitypub.php
@@ -19,7 +19,7 @@ namespace Activitypub;
 
 use WP_CLI;
 
-\define( 'ACTIVITYPUB_PLUGIN_VERSION', '4.7.4' );
+\define( 'ACTIVITYPUB_PLUGIN_VERSION', '4.7.3' );
 
 // Plugin related constants.
 \define( 'ACTIVITYPUB_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
