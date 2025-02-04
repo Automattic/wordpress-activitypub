0554686b456fdda65db3b518c37bc300dfdfa12f
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-02-04 12:05:27 +0100

    fix phpcs issues

--- tests/includes/collection/class-test-replies.php
+++ tests/includes/collection/class-test-replies.php
@@ -63,30 +63,30 @@ class Test_Replies extends \WP_UnitTestCase {
 	 * @covers ::get_context_collection
 	 */
 	public function test_get_context_collection() {
-		// Erstelle einen Test-Post
+		// Erstelle einen Test-Post.
 		$context_post_id = self::factory()->post->create(
 			array(
 				'post_author'  => 1,
 			)
 		);
 
-		// Test mit deaktiviertem Post
+		// Test mit deaktiviertem Post.
 		add_post_meta( $context_post_id, 'activitypub_content_visibility', ACTIVITYPUB_CONTENT_VISIBILITY_LOCAL );
 		$this->assertFalse( Replies::get_context_collection( $context_post_id ), 'Sollte false für deaktivierte Posts zurückgeben' );
 		delete_post_meta( $context_post_id, 'activitypub_content_visibility' );
 
-		// Test mit ungültigem Post
+		// Test mit ungültigem Post.
 		$this->assertFalse( Replies::get_context_collection( 999999 ), 'Sollte false für nicht existierende Posts zurückgeben' );
 
-		// Test ohne Kommentare
+		// Test ohne Kommentare.
 		$context = Replies::get_context_collection( $context_post_id );
 		$this->assertIsArray( $context, 'Sollte ein leeres Array für Posts ohne Kommentare zurückgeben' );
 		$this->assertEmpty( $context, 'Array sollte leer sein für Posts ohne Kommentare' );
 
-		// Erstelle Test-Kommentare
+		// Erstelle Test-Kommentare.
 		$comments = array();
 
-		// Lokaler Kommentar
+		// Lokaler Kommentar.
 		$comments[] = self::factory()->comment->create(
 			array(
 				'comment_post_ID'  => $context_post_id,
@@ -98,7 +98,7 @@ class Test_Replies extends \WP_UnitTestCase {
 			)
 		);
 
-		// ActivityPub Kommentar
+		// ActivityPub Kommentar.
 		$comments[] = self::factory()->comment->create(
 			array(
 				'comment_post_ID'  => $context_post_id,
@@ -111,7 +111,7 @@ class Test_Replies extends \WP_UnitTestCase {
 			)
 		);
 
-		// Test mit Kommentaren
+		// Test mit Kommentaren.
 		$context = Replies::get_context_collection( $context_post_id );
 
 		$this->assertIsArray( $context, 'Sollte ein Array zurückgeben' );
@@ -121,17 +121,17 @@ class Test_Replies extends \WP_UnitTestCase {
 		$this->assertArrayHasKey( 'totalItems', $context, 'Sollte totalItems enthalten' );
 		$this->assertArrayHasKey( 'items', $context, 'Sollte items enthalten' );
 
-		// Überprüfe die Anzahl der Items (Post + alle Kommentare)
+		// Überprüfe die Anzahl der Items (Post + alle Kommentare).
 		$this->assertEquals( 3, $context['totalItems'], 'Sollte Post + alle Kommentare zählen' );
 		$this->assertCount( 3, $context['items'], 'Items sollte Post + alle Kommentare enthalten' );
 
-		// Überprüfe, dass der Post-URI das erste Element ist
+		// Überprüfe, dass der Post-URI das erste Element ist.
 		$this->assertStringContainsString( (string) $context_post_id, $context['items'][0], 'Erstes Item sollte Post-URI sein' );
 
-		// Überprüfe, dass der ActivityPub Kommentar enthalten ist
+		// Überprüfe, dass der ActivityPub Kommentar enthalten ist.
 		$this->assertContains( 'https://example.com/comment/1', $context['items'], 'Sollte ActivityPub Kommentar-ID enthalten' );
 
-		// Clean up
+		// Clean up.
 		wp_delete_post( $context_post_id, true );
 		foreach ( $comments as $comment_id ) {
 			wp_delete_comment( $comment_id, true );
