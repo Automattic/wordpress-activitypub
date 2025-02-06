e22d0a55736c816c2cd1d363116eb2c9fd9e1a6a
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-02-06 16:52:03 +0100

    store object id as meta

--- includes/class-activitypub.php
+++ includes/class-activitypub.php
@@ -619,6 +619,17 @@ class Activitypub {
 
 		\register_post_meta(
 			Outbox::POST_TYPE,
+			'_activitypub_object_id',
+			array(
+				'show_in_rest'      => true,
+				'single'            => true,
+				'type'              => 'string',
+				'sanitize_callback' => 'sanatize_url',
+			)
+		);
+
+		\register_post_meta(
+			Outbox::POST_TYPE,
 			'activitypub_content_visibility',
 			array(
 				'show_in_rest'      => true,
