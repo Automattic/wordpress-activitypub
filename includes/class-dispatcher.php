9fb08d04ecaa98047ce67c4a3130dfe872fb35fd
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-02-06 10:32:47 +0100

    Switch actions
    props @obenland

--- includes/class-dispatcher.php
+++ includes/class-dispatcher.php
@@ -137,21 +137,6 @@ class Dispatcher {
 
 		if ( is_countable( $inboxes ) && count( $inboxes ) < self::$batch_size ) {
 			/**
-			 * Fires when the batch of followers is complete.
-			 *
-			 * @param array    $inboxes  The inboxes.
-			 * @param int      $actor_id The actor ID.
-			 * @param Activity $activity The activity.
-			 * @param \WP_Post $outbox_item The outbox item.
-			 * @param int      $batch_size The batch size.
-			 * @param int      $offset The offset.
-			 */
-			\do_action( 'activitypub_sent_to_inboxes_batch_complete', $inboxes, $actor_id, $activity, $outbox_item, $batch_size, $offset );
-
-			// No more followers to process for this update.
-			\wp_publish_post( $outbox_item );
-		} else {
-			/**
 			 * Fires when the followers are complete.
 			 *
 			 * @param array    $inboxes  The inboxes.
@@ -162,6 +147,21 @@ class Dispatcher {
 			 * @param int      $offset The offset.
 			 */
 			\do_action( 'activitypub_sent_to_inboxes_complete', $inboxes, $actor_id, $activity, $outbox_item, $batch_size, $offset );
+
+			// No more followers to process for this update.
+			\wp_publish_post( $outbox_item );
+		} else {
+			/**
+			 * Fires when the batch of followers is complete.
+			 *
+			 * @param array    $inboxes  The inboxes.
+			 * @param int      $actor_id The actor ID.
+			 * @param Activity $activity The activity.
+			 * @param \WP_Post $outbox_item The outbox item.
+			 * @param int      $batch_size The batch size.
+			 * @param int      $offset The offset.
+			 */
+			\do_action( 'activitypub_sent_to_inboxes_batch_complete', $inboxes, $actor_id, $activity, $outbox_item, $batch_size, $offset );
 
 			return array( $activity, $actor_id, $outbox_item, $batch_size, $offset + $batch_size );
 		}
