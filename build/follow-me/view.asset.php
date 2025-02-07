9c162140bb64e91c7368da51a2e2f6a10dd10183
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-02-07 10:18:28 +0100

    Improve the wording of the Reply-Block
    and regenerate the JS

--- build/follow-me/view.asset.php
+++ build/follow-me/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '91c0e513a192990d783c');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '23b8c0ea643db71e16b0');
