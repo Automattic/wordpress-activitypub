ce99a915ceaf2be74c3670e1d46c4ad7876652a5
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-02-10 11:07:17 +0100

    Fix status header
    Fix #1296
    
    props @Menrath

--- includes/class-activitypub.php
+++ includes/class-activitypub.php
@@ -170,9 +170,14 @@ class Activitypub {
 
 		if ( $activitypub_template ) {
 			// Check if header already sent.
-			if ( ! \headers_sent() && ACTIVITYPUB_SEND_VARY_HEADER ) {
-				// Send Vary header for Accept header.
-				\header( 'Vary: Accept' );
+			if ( ! \headers_sent() ) {
+				// Send 200 status header.
+				\status_header( 200 );
+
+				if ( ACTIVITYPUB_SEND_VARY_HEADER ) {
+					// Send Vary header for Accept header.
+					\header( 'Vary: Accept' );
+				}
 			}
 
 			return $activitypub_template;
