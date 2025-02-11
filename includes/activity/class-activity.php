af77a71d6737b1253ae89ac38fb99b1da3535162
Konstantin Obenland <obenland@gmx.de>
2025-02-11 13:56:48 -0600

    Revert comment changes

--- includes/activity/class-activity.php
+++ includes/activity/class-activity.php
@@ -67,84 +67,122 @@ class Activity extends Base_Object {
 
 	/**
 	 * Describes the direct object of the activity.
-	 * For instance, in the activity "John added a movie to his wishlist", the object of the activity is the movie added.
+	 * For instance, in the activity "John added a movie to his
+	 * wishlist", the object of the activity is the movie added.
 	 *
 	 * @see https://www.w3.org/TR/activitystreams-vocabulary/#dfn-object-term
 	 *
-	 * @var string|Base_Object|null
+	 * @var string
+	 *    | Base_Object
+	 *    | Link
+	 *    | null
 	 */
 	protected $object;
 
 	/**
-	 * Describes one or more entities that either performed or are expected to perform the activity.
+	 * Describes one or more entities that either performed or are
+	 * expected to perform the activity.
 	 * Any single activity can have multiple actors.
+	 * The actor MAY be specified using an indirect Link.
 	 *
 	 * @see https://www.w3.org/TR/activitystreams-vocabulary/#dfn-actor
 	 *
-	 * @var string|\ActivityPhp\Type\Extended\AbstractActor|array<Actor>|null
+	 * @var string
+	 *    | \ActivityPhp\Type\Extended\AbstractActor
+	 *    | array<Actor>
+	 *    | array<Link>
+	 *    | Link
 	 */
 	protected $actor;
 
 	/**
 	 * The indirect object, or target, of the activity.
-	 * The precise meaning of the target is largely dependent on the type of action being described but will often be the object of the English preposition "to".
-	 * For instance, in the activity "John added a movie to his wishlist", the target of the activity is John's wishlist.
+	 * The precise meaning of the target is largely dependent on the
+	 * type of action being described but will often be the object of
+	 * the English preposition "to".
+	 * For instance, in the activity "John added a movie to his
+	 * wishlist", the target of the activity is John's wishlist.
 	 * An activity can have more than one target.
 	 *
 	 * @see https://www.w3.org/TR/activitystreams-vocabulary/#dfn-target
 	 *
-	 * @var string|ObjectType|array<ObjectType>|null
+	 * @var string
+	 *    | ObjectType
+	 *    | array<ObjectType>
+	 *    | Link
+	 *    | array<Link>
 	 */
 	protected $target;
 
 	/**
 	 * Describes the result of the activity.
-	 * For instance, if a particular action results in the creation of a new resource, the result property can be used to describe that new resource.
+	 * For instance, if a particular action results in the creation of
+	 * a new resource, the result property can be used to describe
+	 * that new resource.
 	 *
 	 * @see https://www.w3.org/TR/activitystreams-vocabulary/#dfn-result
 	 *
-	 * @var string|ObjectType|null
+	 * @var string
+	 *    | ObjectType
+	 *    | Link
+	 *    | null
 	 */
 	protected $result;
 
 	/**
-	 * Identifies a Collection containing objects considered to be responses to this object.
-	 * WordPress has a strong core system of approving replies. We only include approved replies here.
+	 * Identifies a Collection containing objects considered to be responses
+	 * to this object.
+	 * WordPress has a strong core system of approving replies. We only include
+	 * approved replies here.
 	 *
 	 * @see https://www.w3.org/TR/activitystreams-vocabulary/#dfn-replies
 	 *
-	 * @var array|ObjectType|null
+	 * @var array
+	 *    | ObjectType
+	 *    | Link
+	 *    | null
 	 */
 	protected $replies;
 
 	/**
-	 * An indirect object of the activity from which the activity is directed.
-	 * The precise meaning of the origin is the object of the English preposition "from".
-	 * For instance, in the activity "John moved an item to List B from List A", the origin of the activity is "List A".
+	 * An indirect object of the activity from which the
+	 * activity is directed.
+	 * The precise meaning of the origin is the object of the English
+	 * preposition "from".
+	 * For instance, in the activity "John moved an item to List B
+	 * from List A", the origin of the activity is "List A".
 	 *
 	 * @see https://www.w3.org/TR/activitystreams-vocabulary/#dfn-origin
 	 *
-	 * @var string|ObjectType|null
+	 * @var string
+	 *    | ObjectType
+	 *    | Link
+	 *    | null
 	 */
 	protected $origin;
 
 	/**
-	 * One or more objects used (or to be used) in the completion of an Activity.
+	 * One or more objects used (or to be used) in the completion of an
+	 * Activity.
 	 *
 	 * @see https://www.w3.org/TR/activitystreams-vocabulary/#dfn-instrument
 	 *
-	 * @var string|ObjectType|null
+	 * @var string
+	 *    | ObjectType
+	 *    | Link
+	 *    | null
 	 */
 	protected $instrument;
 
 	/**
 	 * Set the object and copy Object properties to the Activity.
 	 *
-	 * Any to, bto, cc, bcc, and audience properties specified on the object MUST be copied over to the new Create activity by the server.
+	 * Any to, bto, cc, bcc, and audience properties specified on the object
+	 * MUST be copied over to the new Create activity by the server.
 	 *
 	 * @see https://www.w3.org/TR/activitypub/#object-without-create
 	 *
-	 * @param array|string|Base_Object|null $data Activity object.
+	 * @param array|string|Base_Object|Link|null $data Activity object.
 	 *
 	 * @return void
 	 */
