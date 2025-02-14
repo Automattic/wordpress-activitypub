402c3440a5b01a39342f44b06b55dae1a0929137
Matt Wiebe <wiebe@automattic.com>
2025-02-14 13:03:14 -0600

    built files

--- build/reply/index.asset.php
+++ build/reply/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'dbf6ba0f6fd6b04971f6');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7358d3a5554c292a89c9');
