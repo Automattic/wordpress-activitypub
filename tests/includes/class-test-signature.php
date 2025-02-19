c92ddaab101ca3c2403a4e0d08416e203ddc7f91
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-02-19 12:19:29 +0100

    Fix tests

--- tests/includes/class-test-signature.php
+++ tests/includes/class-test-signature.php
@@ -16,20 +16,6 @@ use Activitypub\Collection\Actors;
  * @coversDefaultClass \Activitypub\Signature
  */
 class Test_Signature extends \WP_UnitTestCase {
-
-	public function set_up() {
-		parent::set_up();
-		\delete_option( 'activitypub_keypair_for_0' );
-		\delete_option( 'activitypub_keypair_for_-1' );
-		\delete_option( 'activitypub_keypair_for_admin' );
-		\delete_option( 'activitypub_blog_user_public_key' );
-		\delete_option( 'activitypub_blog_user_private_key' );
-		\delete_option( 'activitypub_application_user_public_key' );
-		\delete_option( 'activitypub_application_user_private_key' );
-		\delete_user_meta( 1, 'magic_sig_public_key' );
-		\delete_user_meta( 1, 'magic_sig_private_key' );
-	}
-
 	public function tear_down() {
 		parent::tear_down();
 		\delete_option( 'activitypub_keypair_for_0' );
@@ -72,6 +58,8 @@ class Test_Signature extends \WP_UnitTestCase {
 		// Check user.
 		$user = Actors::get_by_id( 1 );
 
+		\delete_option( 'activitypub_keypair_for_admin' );
+
 		$public_key  = 'public key ' . $user->get__id();
 		$private_key = 'private key ' . $user->get__id();
 
@@ -87,11 +75,13 @@ class Test_Signature extends \WP_UnitTestCase {
 		// Check application user.
 		$user = Actors::get_by_id( Actors::APPLICATION_USER_ID );
 
+		\delete_option( 'activitypub_keypair_for_-1' );
+
 		$public_key  = 'public key ' . $user->get__id();
 		$private_key = 'private key ' . $user->get__id();
 
-		add_option( 'activitypub_application_user_public_key', $public_key );
-		add_option( 'activitypub_application_user_private_key', $private_key );
+		\add_option( 'activitypub_application_user_public_key', $public_key );
+		\add_option( 'activitypub_application_user_private_key', $private_key );
 
 		$key_pair = Signature::get_keypair_for( $user->get__id() );
 
@@ -107,8 +97,10 @@ class Test_Signature extends \WP_UnitTestCase {
 		$public_key  = 'public key ' . $user->get__id();
 		$private_key = 'private key ' . $user->get__id();
 
-		add_option( 'activitypub_blog_user_public_key', $public_key );
-		add_option( 'activitypub_blog_user_private_key', $private_key );
+		\delete_option( 'activitypub_keypair_for_0' );
+
+		\add_option( 'activitypub_blog_user_public_key', $public_key );
+		\add_option( 'activitypub_blog_user_private_key', $private_key );
 
 		$key_pair = Signature::get_keypair_for( $user->get__id() );
 
