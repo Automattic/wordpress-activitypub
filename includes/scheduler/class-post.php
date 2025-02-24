8af759d2c5505522da640ed8a610c415fdf3a424
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-02-24 13:21:06 +0100

    Improve outbox scheduling of (custom) posts

--- includes/scheduler/class-post.php
+++ includes/scheduler/class-post.php
@@ -22,7 +22,7 @@ class Post {
 	 */
 	public static function init() {
 		// Post transitions.
-		\add_action( 'transition_post_status', array( self::class, 'schedule_post_activity' ), 33, 3 );
+		\add_action( 'wp_after_insert_post', array( self::class, 'handle_post_update' ), 33, 4 );
 
 		// Attachment transitions.
 		\add_action( 'add_attachment', array( self::class, 'transition_attachment_status' ) );
@@ -33,41 +33,19 @@ class Post {
 		$post_types = \get_post_types_by_support( 'activitypub' );
 
 		foreach ( $post_types as $post_type ) {
-			\add_filter( "rest_pre_insert_{$post_type}", array( self::class, 'rest_insert' ), 10, 2 );
+			//\add_filter( "rest_pre_insert_{$post_type}", array( self::class, 'rest_insert' ), 10, 2 );
 		}
 	}
 
 	/**
-	 * Schedules Activities for attachment transitions.
+	 * Handle post updates and determine the appropriate Activity type.
 	 *
-	 * @param int $post_id Attachment ID.
+	 * @param int      $post_id     Post ID.
+	 * @param \WP_Post $post        Post object.
+	 * @param bool     $update      Whether this is an existing post being updated.
+	 * @param \WP_Post $post_before Post object before the update.
 	 */
-	public static function transition_attachment_status( $post_id ) {
-		if ( ! \post_type_supports( 'attachment', 'activitypub' ) ) {
-			return;
-		}
-
-		switch ( current_action() ) {
-			case 'add_attachment':
-				self::schedule_post_activity( 'publish', '', get_post( $post_id ) );
-				break;
-			case 'edit_attachment':
-				self::schedule_post_activity( 'publish', 'publish', get_post( $post_id ) );
-				break;
-			case 'delete_attachment':
-				self::schedule_post_activity( 'trash', '', get_post( $post_id ) );
-				break;
-		}
-	}
-
-	/**
-	 * Schedule Activities.
-	 *
-	 * @param string   $new_status New post status.
-	 * @param string   $old_status Old post status.
-	 * @param \WP_Post $post       Post object.
-	 */
-	public static function schedule_post_activity( $new_status, $old_status, $post ) {
+	public static function handle_post_update( $post_id, $post, $update, $post_before ) {
 		if ( defined( 'WP_IMPORTING' ) && WP_IMPORTING ) {
 			return;
 		}
@@ -81,9 +59,12 @@ class Post {
 			return;
 		}
 
+		$new_status = get_post_status( $post );
+		$old_status = $post_before ? get_post_status( $post_before ) : null;
+
 		switch ( $new_status ) {
 			case 'publish':
-				$type = ( 'publish' === $old_status ) ? 'Update' : 'Create';
+				$type = $update ? 'Update' : 'Create';
 				break;
 
 			case 'draft':
@@ -105,6 +86,34 @@ class Post {
 
 		// Add the post to the outbox.
 		add_to_outbox( $post, $type, $post->post_author );
+	}
+
+	/**
+	 * Schedules Activities for attachment transitions.
+	 *
+	 * @param int $post_id Attachment ID.
+	 */
+	public static function transition_attachment_status( $post_id ) {
+		if ( ! \post_type_supports( 'attachment', 'activitypub' ) ) {
+			return;
+		}
+
+		$post = \get_post( $post_id );
+
+		switch ( current_action() ) {
+			case 'add_attachment':
+				// Add the post to the outbox.
+				add_to_outbox( $post, 'Create', $post->post_author );
+				break;
+			case 'edit_attachment':
+				// Update the post to the outbox.
+				add_to_outbox( $post, 'Update', $post->post_author );
+				break;
+			case 'delete_attachment':
+				// Delete the post from the outbox.
+				add_to_outbox( $post, 'Delete', $post->post_author );
+				break;
+		}
 	}
 
 	/**
