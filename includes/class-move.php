0c39f78bfe3e9488d6d24e1596a3b321ac4990c2
Konstantin Obenland <obenland@gmx.de>
2025-02-24 15:03:47 -0600

    Move: General method to move accounts
    First pass

--- includes/class-move.php
+++ includes/class-move.php
@@ -7,6 +7,8 @@
 
 namespace Activitypub;
 
+use Activitypub\Collection\Actors;
+
 /**
  * ActivityPub (Account) Move Class
  *
@@ -48,5 +50,41 @@ class Move {
 		}
 
 		return $actor;
+	}
+
+	/**
+	 * Move an ActivityPub account from one location to another.
+	 *
+	 * @param string $from The current account URL.
+	 * @param string $to   The new account URL.
+	 * @return int|bool The ID of the outbox item or false on failure.
+	 */
+	public static function account( $from, $to ) {
+		$user = Actors::get_by_resource( $from );
+
+		if ( ! $user ) {
+			return false;
+		}
+
+		// Update the movedTo property.
+		\update_user_option( $user->get__id(), 'activitypub_move_to', $to );
+
+		// Add the old account URL to alsoKnownAs.
+		$also_known_as   = (array) \get_user_option( 'activitypub_also_known_as', $user->get__id() );
+		$also_known_as[] = $from;
+		$also_known_as   = array_unique( $also_known_as );
+		\update_user_option( $user->get__id(), 'activitypub_also_known_as', $also_known_as );
+
+		// Create Move activity.
+		$activity = array(
+			'@context' => get_context(),
+			'type'     => 'Move',
+			'actor'    => $from,
+			'object'   => $from,
+			'target'   => $to,
+		);
+
+		// Add to outbox.
+		return add_to_outbox( $activity, 'Move', $user->get__id() );
 	}
 }
