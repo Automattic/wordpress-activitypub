81c9cab6df0cab413ace0be437e19fefd1aad6c6
Konstantin Obenland <obenland@gmx.de>
2025-02-25 13:11:15 -0600

    Add upgrade routine

--- includes/class-migration.php
+++ includes/class-migration.php
@@ -185,6 +185,9 @@ class Migration {
 		if ( \version_compare( $version_from_db, '5.3.0', '<' ) ) {
 			add_action( 'init', 'flush_rewrite_rules', 20 );
 		}
+		if ( \version_compare( $version_from_db, 'unreleased', '<' ) ) {
+			\wp_schedule_single_event( \time(), 'activitypub_upgrade', array( 'update_comment_author_emails' ) );
+		}
 
 		/*
 		 * Add new update routines above this comment. ^
@@ -620,6 +623,57 @@ class Migration {
 
 		foreach ( $comments as $comment ) {
 			self::add_to_outbox( $comment, 'Create', $comment->user_id );
+		}
+
+		if ( count( $comments ) === $batch_size ) {
+			return array(
+				'batch_size' => $batch_size,
+				'offset'     => $offset + $batch_size,
+			);
+		}
+
+		return null;
+	}
+
+	/**
+	 * Update comment author emails with webfinger addresses for ActivityPub comments.
+	 *
+	 * @param int $batch_size Optional. Number of comments to process per batch. Default 50.
+	 * @param int $offset     Optional. Number of comments to skip. Default 0.
+	 * @return array|null Array with batch size and offset if there are more comments to process, null otherwise.
+	 */
+	public static function update_comment_author_emails( $batch_size = 50, $offset = 0 ) {
+		$comments = \get_comments(
+			array(
+				'number'     => $batch_size,
+				'offset'     => $offset,
+				// phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_query
+				'meta_query' => array(
+					array(
+						'key'   => 'protocol',
+						'value' => 'activitypub',
+					),
+				),
+			)
+		);
+
+		foreach ( $comments as $comment ) {
+			$comment_author_url = $comment->comment_author_url;
+			if ( empty( $comment_author_url ) ) {
+				continue;
+			}
+
+			$webfinger = Webfinger::uri_to_acct( $comment_author_url );
+			if ( \is_wp_error( $webfinger ) ) {
+				continue;
+			}
+
+			\wp_update_comment(
+				array(
+					'comment_ID'           => $comment->comment_ID,
+					'comment_author_email' => \str_replace( 'acct:', '', $webfinger ),
+				)
+			);
 		}
 
 		if ( count( $comments ) === $batch_size ) {
