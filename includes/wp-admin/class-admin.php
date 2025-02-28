130d472334b061953dfe8931236b40ca060e33f8
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-02-28 13:39:57 +0100

    fix PHPCS

--- includes/wp-admin/class-admin.php
+++ includes/wp-admin/class-admin.php
@@ -173,7 +173,8 @@ class Admin {
 			\delete_user_option( $user_id, 'activitypub_header_image' );
 		}
 
-		$also_known_as = $_POST['activitypub_blog_user_also_known_as'];
+		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+		$also_known_as = \sanitize_text_field( \wp_unslash( isset( $_POST['activitypub_blog_user_also_known_as'] ) ? $_POST['activitypub_blog_user_also_known_as'] : '' ) );
 		if ( $also_known_as ) {
 			$also_known_as = explode( PHP_EOL, $also_known_as );
 			$also_known_as = array_map( 'trim', $also_known_as );
