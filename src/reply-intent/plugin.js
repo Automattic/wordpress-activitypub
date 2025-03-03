8885f18a64afb79f3b7e689f08dfbb3bf40172b3
Matt Wiebe <wiebe@automattic.com>
2025-03-03 17:43:10 -0600

    update bookmarklet flow to prefer embeds

--- src/reply-intent/plugin.js
+++ src/reply-intent/plugin.js
@@ -4,29 +4,30 @@ import { dispatch } from '@wordpress/data';
 import { store as blockEditorStore } from '@wordpress/block-editor';
 import { useEffect, useState } from '@wordpress/element';
 
+// We use a kind of global state to ensure only a single embed is rendered across component re-renders.
+let didHandleEmbed = false;
+
 const HandleReplyIntent = () => {
-	const [ didHandle, setDidHandle ] = useState( false );
 	useEffect( () => {
-		if ( didHandle ) {
+		if ( didHandleEmbed ) {
 			return;
 		}
 		// Get the GET['in_reply_to'] value from the URL
 		const urlParams = new URLSearchParams( window.location.search );
 		const inReplyTo = urlParams.get( 'in_reply_to' );
-		if ( inReplyTo ) {
+		if ( inReplyTo && ! didHandleEmbed ) {
 			// prepend an activitypub/reply block to the editor
 			// it appears to need a slight delay
 			setTimeout( () => {
-				const block = createBlock( 'activitypub/reply', { url: inReplyTo } );
+				const block = createBlock( 'activitypub/reply', { url: inReplyTo, embedPost: true } );
 				const store = dispatch( blockEditorStore );
 				store.insertBlock( block );
 				// add a new block after it so the user can just type
 				store.insertAfterBlock( block.clientId );
 			}, 200 );
 		}
-
-		setDidHandle( true );
-	}, [ didHandle ] );
+		didHandleEmbed = true;
+	} );
 
 	return null;
 };
