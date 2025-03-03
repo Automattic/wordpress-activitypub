53666012785cb4d1247aadfa5b568495b7f20df0
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-03-03 08:34:43 +0100

    use sanitize class

--- includes/wp-admin/class-settings.php
+++ includes/wp-admin/class-settings.php
@@ -178,17 +178,7 @@ class Settings {
 				'type'              => 'array',
 				'description'       => \__( 'Relays', 'activitypub' ),
 				'default'           => array(),
-				'sanitize_callback' => function ( $value ) {
-					if ( ! is_array( $value ) ) {
-						$value = explode( PHP_EOL, $value );
-					}
-
-					$value = array_map( 'trim', $value );
-					$value = array_filter( $value );
-					$value = array_map( 'esc_url', $value );
-
-					return $value;
-				},
+				'sanitize_callback' => array( Sanitize::class, 'url_list' ),
 			)
 		);
 
