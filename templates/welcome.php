aa111905786ca98fefc473d835a1a7455ff46615
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-03-11 18:38:53 +0100

    remove `settings_fields()`
    props @obenland

--- templates/welcome.php
+++ templates/welcome.php
@@ -8,6 +8,5 @@
 ?>
 
 <div class="activitypub-settings activitypub-welcome-page hide-if-no-js">
-	<?php settings_fields( 'activitypub_welcome' ); ?>
 	<?php do_settings_sections( 'activitypub_welcome' ); ?>
 </div>
