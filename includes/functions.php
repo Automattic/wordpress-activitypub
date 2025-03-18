b6c8ec69a3f7b400dfd22e0d9251d99935a2d023
Konstantin Obenland <obenland@gmx.de>
2025-03-18 16:31:05 +0100

    Use activity types

--- includes/functions.php
+++ includes/functions.php
@@ -1530,38 +1530,7 @@ function is_activity( $data ) {
 	 *
 	 * @param array $types The activity types.
 	 */
-	$types = apply_filters(
-		'activitypub_activity_types',
-		array(
-			'Accept',
-			'Add',
-			'Announce',
-			'Arrive',
-			'Block',
-			'Create',
-			'Delete',
-			'Dislike',
-			'Follow',
-			'Flag',
-			'Ignore',
-			'Invite',
-			'Join',
-			'Leave',
-			'Like',
-			'Listen',
-			'Move',
-			'Offer',
-			'Read',
-			'Reject',
-			'Remove',
-			'TentativeAccept',
-			'TentativeReject',
-			'Travel',
-			'Undo',
-			'Update',
-			'View',
-		)
-	);
+	$types = apply_filters( 'activitypub_activity_types', Activitypub::$activity_types );
 
 	if ( is_string( $data ) ) {
 		return in_array( $data, $types, true );
