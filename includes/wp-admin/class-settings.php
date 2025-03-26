3ff08bf9b77cc03febab04391504964986dd49be
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-03-26 09:14:48 +0100

    fix typo

--- includes/wp-admin/class-settings.php
+++ includes/wp-admin/class-settings.php
@@ -491,7 +491,7 @@ class Settings {
 		}
 
 		$screen_settings = '<fieldset>
-		<legend class="screen-layout">' . __( 'SettingsPages', 'activitypub' ) . '</legend>
+		<legend class="screen-layout">' . __( 'Settings Pages', 'activitypub' ) . '</legend>
 		<p>
 			' . __( 'Some settings pages can be shown or hidden by using the checkboxes.', 'activitypub' ) . '
 		</p>
