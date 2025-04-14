21730ce2bd4d508f55d697ff342b25ddc5b4ba9a
Konstantin Obenland <obenland@gmx.de>
2025-04-14 13:43:41 -0500

    Revert js changes

--- build/reply-intent/plugin.asset.php
+++ build/reply-intent/plugin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-core-data', 'wp-data', 'wp-element', 'wp-plugins'), 'version' => '2d02c6406bed6cffb5e9');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-plugins'), 'version' => 'f65a7269b5abb57d3e73');
--- src/reply-intent/plugin.js
+++ src/reply-intent/plugin.js
@@ -1,8 +1,7 @@
 import { registerPlugin } from '@wordpress/plugins';
 import { createBlock } from '@wordpress/blocks';
-import { dispatch, select } from '@wordpress/data';
+import { dispatch } from '@wordpress/data';
 import { store as blockEditorStore } from '@wordpress/block-editor';
-import { store as coreStore } from '@wordpress/core-data';
 import { useEffect, useState } from '@wordpress/element';
 
 // We use a kind of global state to ensure only a single embed is rendered across component re-renders.
@@ -16,8 +15,6 @@ const HandleReplyIntent = () => {
 		// Get the GET['in_reply_to'] value from the URL
 		const urlParams = new URLSearchParams( window.location.search );
 		const inReplyTo = urlParams.get( 'in_reply_to' );
-		const visibility = urlParams.get( 'visibility' );
-
 		if ( inReplyTo && ! didHandleEmbed ) {
 			// prepend an activitypub/reply block to the editor
 			// it appears to need a slight delay
@@ -27,21 +24,6 @@ const HandleReplyIntent = () => {
 				store.insertBlock( block );
 				// add a new block after it so the user can just type
 				store.insertAfterBlock( block.clientId );
-
-				// Set the activitypub_content_visibility post meta if visibility parameter is present
-				if ( visibility ) {
-					const postType = select( 'core/editor' ).getCurrentPostType();
-					const postId = select( 'core/editor' ).getCurrentPostId();
-
-					if ( postType && postId ) {
-						const metaStore = dispatch( coreStore );
-						metaStore.editEntityRecord( 'postType', postType, postId, {
-							meta: {
-								activitypub_content_visibility: visibility
-							}
-						} );
-					}
-				}
 			}, 200 );
 		}
 		didHandleEmbed = true;
