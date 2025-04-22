e97956667b965bb898cc2fba23c15cba6d4eaa5f
Konstantin Obenland <obenland@gmx.de>
2025-04-22 08:58:44 -0500

    Update labels

--- build/reactions/index.asset.php
+++ build/reactions/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c6b90b80fa4d667df7c4');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '32631215c76c36b38e5e');
--- src/reactions/edit.js
+++ src/reactions/edit.js
@@ -82,13 +82,19 @@ const generateDummyReaction = ( index ) => {
  */
 const generateDummyReactions = () => ( {
 	likes: {
-		/* translators: %d: Number of likes */
-		label: sprintf( _x( '%d likes', 'number of likes', 'activitypub' ), 9 ),
+		label: sprintf(
+			/* translators: %d: Number of likes */
+			_x( '%d likes', 'number of likes', 'activitypub' ),
+			9
+		),
 		items: Array.from( { length: 9 }, ( _, i ) => generateDummyReaction( i ) ),
 	},
 	reposts: {
-		/* translators: %d: Number of reposts */
-		label:  sprintf( _x( '%d reposts', 'number of reposts', 'activitypub' ), 6 ),
+		label:  sprintf(
+			/* translators: %d: Number of reposts */
+			_x( '%d reposts', 'number of reposts', 'activitypub' ),
+			6
+		),
 		items: Array.from( { length: 6 }, ( _, i ) => generateDummyReaction( i + 9 ) ),
 	},
 } );
