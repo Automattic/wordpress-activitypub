011341c2c666f19ce5e0d7984a9cac04a3e77d15
Konstantin Obenland <obenland@gmx.de>
2025-04-22 08:56:42 -0500

    Update build

--- build/follow-me/view.asset.php
+++ build/follow-me/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '73fd5ab9bc049f94f37e');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-compose', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '635ed3e6db3230ae865f');
