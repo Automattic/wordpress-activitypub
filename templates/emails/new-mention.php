2e57bd571aab3466f119d8c64537e685a2a8bf72
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-05-02 18:29:48 +0200

    remove slashes

--- templates/emails/new-mention.php
+++ templates/emails/new-mention.php
@@ -11,7 +11,7 @@ use Activitypub\Embed;
 use function Activitypub\site_supports_blocks;
 
 /* @var array $args Template arguments. */
-$args = \wp_parse_args( $args ?? array() );
+$args = wp_parse_args( $args ?? array() );
 
 // Load header.
 require __DIR__ . '/parts/header.php';
@@ -20,9 +20,9 @@ require __DIR__ . '/parts/header.php';
 <h1>
 	<?php
 	if ( Actors::BLOG_USER_ID === $args['user_id'] ) :
-		\esc_html_e( 'Your blog was mentioned!', 'activitypub' );
+		esc_html_e( 'Your blog was mentioned!', 'activitypub' );
 	else :
-		\esc_html_e( 'You were mentioned!', 'activitypub' );
+		esc_html_e( 'You were mentioned!', 'activitypub' );
 	endif;
 	?>
 </h1>
@@ -31,13 +31,13 @@ require __DIR__ . '/parts/header.php';
 	<?php
 	if ( Actors::BLOG_USER_ID === $args['user_id'] ) :
 		/* translators: %s: The name of the person who mentioned the blog. */
-		$message = \__( 'Looks like someone&#8217;s talking about your blog! It was just mentioned by %s in a post on the Fediverse. Here&#8217;s what they said:', 'activitypub' );
+		$message = __( 'Looks like someone&#8217;s talking about your blog! It was just mentioned by %s in a post on the Fediverse. Here&#8217;s what they said:', 'activitypub' );
 	else :
 		/* translators: %s: The name of the person who mentioned the user. */
-		$message = \__( 'Looks like someone&#8217;s talking about you! You were just mentioned by %s in a post on the Fediverse. Here&#8217;s what they said:', 'activitypub' );
+		$message = __( 'Looks like someone&#8217;s talking about you! You were just mentioned by %s in a post on the Fediverse. Here&#8217;s what they said:', 'activitypub' );
 	endif;
 
-	\printf( \esc_html( $message ), '<a href="' . \esc_url( $args['actor']['url'] ) . '">' . \esc_html( $args['actor']['webfinger'] ) . '</a>' );
+	printf( qesc_html( $message ), '<a href="' . esc_url( $args['actor']['url'] ) . '">' . esc_html( $args['actor']['webfinger'] ) . '</a>' );
 	?>
 </p>
 
@@ -48,10 +48,10 @@ require __DIR__ . '/parts/header.php';
 	?>
 </div>
 
-<?php if ( site_supports_blocks() && ! \is_plugin_active( 'classic-editor/classic-editor.php' ) ) : ?>
+<?php if ( site_supports_blocks() && ! is_plugin_active( 'classic-editor/classic-editor.php' ) ) : ?>
 <p>
-	<a class="button" href="<?php echo \esc_url( \admin_url( 'post-new.php?in_reply_to=' . $args['activity']['object']['id'] ) ); ?>">
-		<?php \esc_html_e( 'Reply to the post', 'activitypub' ); ?>
+	<a class="button" href="<?php echo esc_url( admin_url( 'post-new.php?in_reply_to=' . $args['activity']['object']['id'] ) ); ?>">
+		<?php esc_html_e( 'Reply to the post', 'activitypub' ); ?>
 	</a>
 </p>
 <?php endif; ?>
