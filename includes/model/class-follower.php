4ae36ef5c9648fc42c28aa9533b0a3e105619901
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-05-07 10:10:15 +0200

    Fix: Reset Errors when Follower update is successfull

--- includes/model/class-follower.php
+++ includes/model/class-follower.php
@@ -52,6 +52,25 @@ class Follower extends Actor {
 	}
 
 	/**
+	 * Clear the errors for the current Follower.
+	 *
+	 * @return bool True on success, false on failure.
+	 */
+	public function clear_errors() {
+		if ( ! $this->_id ) {
+			\_doing_it_wrong(
+				__METHOD__,
+				__( 'Follower ID is not set.', 'activitypub' ),
+				'unreleased'
+			);
+
+			return false;
+		}
+
+		return Followers::clear_errors( $this->_id );
+	}
+
+	/**
 	 * Get the Summary.
 	 *
 	 * @return string The Summary.
