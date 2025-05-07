bbcbeebcf7067c54848119a66263afa326439664
Konstantin Obenland <obenland@gmx.de>
2025-05-07 04:58:36 -0500

    Checklist: Update help tab checks (#1666)
    Co-authored-by: Matthias Pfefferle <pfefferle@users.noreply.github.com>

--- includes/wp-admin/class-settings.php
+++ includes/wp-admin/class-settings.php
@@ -358,6 +358,15 @@ class Settings {
 				\wp_enqueue_media();
 				\wp_enqueue_script( 'activitypub-header-image' );
 				break;
+			case 'settings':
+				if ( isset( $_GET['help-tab'] ) && 'getting-started' === $_GET['help-tab'] ) { // phpcs:ignore WordPress.Security.NonceVerification
+					\update_option( 'activitypub_checklist_fediverse_intro_visited', true );
+				} elseif ( isset( $_GET['help-tab'] ) && 'editor-blocks' === $_GET['help-tab'] ) { // phpcs:ignore WordPress.Security.NonceVerification
+					\update_option( 'activitypub_checklist_blocks_visited', true );
+				} else {
+					\update_option( 'activitypub_checklist_settings_visited', true );
+				}
+				break;
 		}
 
 		if ( ! isset( $settings_tabs[ $tab ] ) ) {
