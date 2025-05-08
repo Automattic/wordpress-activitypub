4e95b69294d72a7683aaf83d49e94d592699243c
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-05-08 15:01:57 +0200

    Update includes/collection/class-actors.php
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>

--- includes/collection/class-actors.php
+++ includes/collection/class-actors.php
@@ -90,7 +90,7 @@ class Actors {
 		// Check for blog user.
 		if (
 			Blog::get_default_username() === $username ||
-			get_option( 'activitypub_blog_identifier' ) === $username
+			\get_option( 'activitypub_blog_identifier' ) === $username
 		) {
 			if ( is_user_type_disabled( 'blog' ) ) {
 				return new WP_Error(
