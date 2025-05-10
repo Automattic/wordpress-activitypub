f26b7dff40c769832b3e37d1417f6c1cf46e3015
Konstantin Obenland <obenland@gmx.de>
2025-05-10 15:43:30 -0500

    Follow Me: Make it interactive!

--- build/follow-me/index.asset.php
+++ build/follow-me/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '02db6907860d67947aae');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'faa65c23487cda14a94f');
