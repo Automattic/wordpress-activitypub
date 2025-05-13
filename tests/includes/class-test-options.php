49164633906809ae73db16c30b8ee2c8dc376535
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-05-13 16:20:15 +0200

    add proof

--- tests/includes/class-test-options.php
+++ tests/includes/class-test-options.php
@@ -20,10 +20,12 @@ class Test_Options extends \WP_UnitTestCase {
 		\add_option( 'activitypub_test_option_1', 'value1' );
 		\add_option( 'activitypub_test_option_2', 'value2' );
 		\add_option( 'activitypub_test_option_3', 'value3' );
+		\add_option( 'no_activitypub_test_option', 'value4' );
 
 		$this->assertEquals( 'value1', \get_option( 'activitypub_test_option_1' ) );
 		$this->assertEquals( 'value2', \get_option( 'activitypub_test_option_2' ) );
 		$this->assertEquals( 'value3', \get_option( 'activitypub_test_option_3' ) );
+		$this->assertEquals( 'value4', \get_option( 'no_activitypub_test_option' ) );
 
 		Options::delete();
 
@@ -32,5 +34,6 @@ class Test_Options extends \WP_UnitTestCase {
 		$this->assertFalse( \get_option( 'activitypub_test_option_1', false ) );
 		$this->assertFalse( \get_option( 'activitypub_test_option_2', false ) );
 		$this->assertFalse( \get_option( 'activitypub_test_option_3', false ) );
+		$this->assertEquals( 'value4', \get_option( 'no_activitypub_test_option' ) );
 	}
 }
