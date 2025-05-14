1b2d9578726fdda2e6b4ae070cd2e0265885b7ef
Konstantin Obenland <obenland@gmx.de>
2025-05-14 11:40:56 -0500

    Don't render appender and lock

--- build/reactions/index.asset.php
+++ build/reactions/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '13c017000d37a3025875');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '3a159dbba14b00d4163c');
