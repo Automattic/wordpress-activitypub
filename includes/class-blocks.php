ce9ec7a3a615baa4c58a4e52bd5c575342da0e96
Konstantin Obenland <obenland@gmx.de>
2025-05-14 11:19:57 -0500

    Update class-blocks.php

--- includes/class-blocks.php
+++ includes/class-blocks.php
@@ -187,10 +187,10 @@ class Blocks {
 
 		$args = array( 'data-attrs' => wp_json_encode( $attrs ) );
 		if ( empty( $content ) ) {
-			if ( ! isset( $attrs['title'] ) ) {
-				$content = '<div class="activitypub-reactions"><h6>' . \__( 'Fediverse Reactions', 'activitypub' ) . '</h6><div class="activitypub-reactions-block" ' . get_block_wrapper_attributes( $args ) . '></div></div>';
+			if ( isset( $attrs['title'] ) ) {
+				$content = '<div class="activitypub-reactions"><div class="activitypub-reactions-block" ' . \get_block_wrapper_attributes( $args ) . '></div></div>';
 			} else {
-				$args['class'] = 'activitypub-reactions-block';
+				$content = '<div class="activitypub-reactions"><h6>' . \__( 'Fediverse Reactions', 'activitypub' ) . '</h6><div class="activitypub-reactions-block" ' . \get_block_wrapper_attributes( $args ) . '></div></div>';
 			}
 		}
 
