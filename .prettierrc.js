ee06b69cc2da70f90beae7ebd40dfd4e6b797b6b
Konstantin Obenland <obenland@gmx.de>
2025-05-15 21:23:00 -0500

    Keep tabs on json files
    Also removes prettier files from distribution

--- .prettierrc.js
+++ .prettierrc.js
@@ -6,22 +6,10 @@ module.exports = {
 	printWidth: 120,
 	overrides: [
 		{
-			files: '*.json',
-			options: {
-				useTabs: false,
-			},
-		},
-		{
 			files: '*.yml',
 			options: {
 				useTabs: false,
 				tabWidth: 2,
-			},
-		},
-		{
-			files: '*.md',
-			options: {
-				trimTrailingWhitespace: false, // Not a valid Prettier option, handled by editorconfig only
 			},
 		},
 	],
