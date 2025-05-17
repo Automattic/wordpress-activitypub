2cd97222b154ef0fc4d0c84aa43921c579261520
Konstantin Obenland <obenland@gmx.de>
2025-05-17 10:54:33 -0500

    Save latest state

--- build/reactions/view.asset.php
+++ build/reactions/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '03525f0763422de12315', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '0ef5d5e100ad5d4d6e14', 'type' => 'module');
