acf11a3de2613ee7b1fb7f297c92a415075701d2
Konstantin Obenland <obenland@gmx.de>
2025-05-17 11:36:22 -0500

    New build

--- build/reactions/index.asset.php
+++ build/reactions/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '49eb4f69734089479cfd');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c79095cdd222a064fffb');
