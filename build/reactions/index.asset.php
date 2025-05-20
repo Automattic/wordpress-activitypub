0684de6a32b74c44ee66c3fec0376da7ab9ecc1e
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-05-20 15:17:40 +0200

    rebuild binaries

--- build/reactions/index.asset.php
+++ build/reactions/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '861b76cedf6b7abcf57e');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '71a9b97929d16c6932ad');
--- build/reactions/view.asset.php
+++ build/reactions/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '623ed543024a3381b6d6');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-dom-ready', 'wp-element', 'wp-i18n'), 'version' => '8709d255c0c51cead818');
