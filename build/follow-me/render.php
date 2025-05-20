7b977cccff40b75f96cc279cd281394827db39ba
Konstantin Obenland <obenland@gmx.de>
2025-05-20 13:28:11 -0500

    Fix close button in 2021

--- build/follow-me/render.php
+++ build/follow-me/render.php
@@ -137,7 +137,7 @@ $content = Blocks::add_directions(
 					data-wp-on--click="actions.closeModal"
 					aria-label="<?php echo esc_attr__( 'Close dialog', 'activitypub' ); ?>"
 				>
-					<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" role="img" aria-hidden="true" focusable="false">
+					<svg fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="24" height="24" role="img" aria-hidden="true" focusable="false">
 						<path d="M13 11.8l6.1-6.3-1-1-6.1 6.2-6.1-6.2-1 1 6.1 6.3-6.5 6.7 1 1 6.5-6.6 6.5 6.6 1-1z"></path>
 					</svg>
 				</button>
