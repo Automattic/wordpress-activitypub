c10b3b867b59d40495bf93d04b428206994af8bc
Konstantin Obenland <obenland@gmx.de>
2025-05-21 13:02:19 -0500

    Toggle Modal and update styles

--- build/reactions/index.asset.php
+++ build/reactions/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '156e86b34517d21f0265');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '56761d90c3a890dddfd0');
