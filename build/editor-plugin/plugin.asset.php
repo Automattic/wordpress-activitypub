21ec63016dd66d79639b07bd2ea2f33f2b78edb7
Konstantin Obenland <obenland@gmx.de>
2025-05-29 10:25:47 -0500

    Editor: Fix console warnings

--- build/editor-plugin/plugin.asset.php
+++ build/editor-plugin/plugin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => 'c4ec9c3a1f0d32bd9118');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => '35eb7359fc61b0d65be6');
