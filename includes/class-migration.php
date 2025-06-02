5fd693cac3eed81e74e08262242a97b5b7810bc5
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-06-02 11:35:45 +0200

    improve phpdoc

--- includes/class-migration.php
+++ includes/class-migration.php
@@ -971,6 +971,7 @@ class Migration {
 	 *
 	 * @param int $batch_size Optional. Number of meta values to process per batch. Default 100.
 	 * @param int $offset     Optional. Number of meta values to skip. Default 0.
+	 *
 	 * @return array|null Array with batch size and offset if there are more meta values to process, null otherwise.
 	 */
 	public static function update_actor_json_storage( $batch_size = 100, $offset = 0 ) {
