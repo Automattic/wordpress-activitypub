c3e87a758286a9bd6a6be59322bcdda77b47ecac
Konstantin Obenland <obenland@gmx.de>
2025-06-03 16:01:41 -0500

    Reactions: Apply learnings from other blocks

--- build/reactions/index.asset.php
+++ build/reactions/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3b68725ce4f6e2d7ba9a');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '924ffd281ef19da276e3');
