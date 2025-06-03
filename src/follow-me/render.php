3c736e391d3532a28fc455cfa2cea02e91b5edec
Konstantin Obenland <obenland@gmx.de>
2025-06-03 16:35:08 -0500

    Remove Following rendering as well

--- src/follow-me/render.php
+++ src/follow-me/render.php
@@ -140,7 +140,7 @@ $stats = array(
 						<div>
 							<?php
 							printf(
-							/* translators: %s: Number of posts */
+								/* translators: %s: Number of posts */
 								esc_html( _n( '%s post', '%s posts', (int) $stats['posts'], 'activitypub' ) ),
 								'<strong>' . esc_html( number_format_i18n( $stats['posts'] ) ) . '</strong>'
 							);
@@ -151,20 +151,9 @@ $stats = array(
 						<div>
 							<?php
 							printf(
-							/* translators: %s: Number of followers */
+								/* translators: %s: Number of followers */
 								esc_html( _n( '%s follower', '%s followers', (int) $stats['followers'], 'activitypub' ) ),
 								'<strong>' . esc_html( number_format_i18n( $stats['followers'] ) ) . '</strong>'
-							);
-							?>
-						</div>
-					<?php endif; ?>
-					<?php if ( null !== $stats['following'] ) : ?>
-						<div>
-							<?php
-							printf(
-							/* translators: %s: Number of following */
-								esc_html( _n( '%s following', '%s following', (int) $stats['following'], 'activitypub' ) ),
-								'<strong>' . esc_html( number_format_i18n( $stats['following'] ) ) . '</strong>'
 							);
 							?>
 						</div>
