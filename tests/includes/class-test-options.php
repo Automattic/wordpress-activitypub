d54842b74aa23b8410074a591173404a2ee67592
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-06-04 18:06:16 +0200

    add tests

--- tests/includes/class-test-options.php
+++ tests/includes/class-test-options.php
@@ -36,4 +36,20 @@ class Test_Options extends \WP_UnitTestCase {
 		$this->assertFalse( \get_option( 'activitypub_test_option_3', false ) );
 		$this->assertEquals( 'value4', \get_option( 'no_activitypub_test_option' ) );
 	}
+
+	/**
+	 * Test that default_max_image_attachments() returns the default value.
+	 */
+	public function test_default_max_image_attachments() {
+		$this->assertEquals( ACTIVITYPUB_MAX_IMAGE_ATTACHMENTS, \get_option( 'activitypub_max_image_attachments', ACTIVITYPUB_MAX_IMAGE_ATTACHMENTS ) );
+
+		\update_option( 'activitypub_max_image_attachments', '10' );
+		$this->assertEquals( '4', \get_option( 'activitypub_max_image_attachments', ACTIVITYPUB_MAX_IMAGE_ATTACHMENTS ) );
+
+		\add_filter( 'activitypub_site_supports_blocks', '__return_false' );
+
+		$this->assertEquals( '10', \get_option( 'activitypub_max_image_attachments', ACTIVITYPUB_MAX_IMAGE_ATTACHMENTS ) );
+
+		\remove_filter( 'activitypub_site_supports_blocks', '__return_false' );
+	}
 }
