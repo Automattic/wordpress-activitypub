6da39fe5d14f9854a288163f21c9fe14b1b4a8c8
Konstantin Obenland <obenland@gmx.de>
2025-06-04 19:40:32 -0500

    Add support for Shadow control

--- build/follow-me/index.asset.php
+++ build/follow-me/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9c8a1c2b19b22ab155a2');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'e4b8de35c5aa8ae7bbe7');
