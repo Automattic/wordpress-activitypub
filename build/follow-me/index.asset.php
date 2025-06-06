9daa5f520f54eaa22733a2089d414728d8230734
Konstantin Obenland <obenland@gmx.de>
2025-06-06 12:42:48 -0500

    Update build files

--- build/follow-me/index.asset.php
+++ build/follow-me/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7ed4fa5961834f1ed1f4');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '91226a235770ce3aa6c4');
