f9becd708eb18ade37e372052c76d6cc29761c39
Konstantin Obenland <obenland@gmx.de>
2025-06-06 11:58:27 -0500

    Reactions: Add Dotcom improvements

--- build/reactions/index.asset.php
+++ build/reactions/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b96e375c677baed5250b');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5f7aa4dbef0027c467cd');
