407ff009a2f397aea7b5d228e90bdba293ffc0ef
Konstantin Obenland <obenland@gmx.de>
2025-06-13 15:41:10 -0500

    Fix (most) tests

--- includes/class-signature.php
+++ includes/class-signature.php
@@ -356,8 +356,8 @@ class Signature {
 	public static function get_signature_algorithm( $signature_block ) {
 		if ( $signature_block['algorithm'] ) {
 			switch ( $signature_block['algorithm'] ) {
-				case 'rsa-sha-512':
-					return 'sha512'; // hs2019 https://datatracker.ietf.org/doc/html/draft-cavage-http-signatures-12.
+				case 'hs2019':
+					return 'sha512'; // https://datatracker.ietf.org/doc/html/draft-cavage-http-signatures-12.
 				default:
 					return 'sha256';
 			}
