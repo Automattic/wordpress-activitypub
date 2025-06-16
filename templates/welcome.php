43b259b53c73702af6a5d4b9b340ec77388758ce
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-06-16 15:33:50 +0200

    enqueue in template instead

--- templates/welcome.php
+++ templates/welcome.php
@@ -5,6 +5,12 @@
  * @package Activitypub
  */
 
+wp_enqueue_style(
+	'activitypub-welcome',
+	plugins_url( 'assets/css/activitypub-welcome.css', ACTIVITYPUB_PLUGIN_FILE ),
+	array(),
+	ACTIVITYPUB_PLUGIN_VERSION
+);
 ?>
 
 <div class="activitypub-settings activitypub-welcome-page hide-if-no-js">
