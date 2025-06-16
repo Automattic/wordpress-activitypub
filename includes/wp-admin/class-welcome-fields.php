5edd5b9d690bb46a0f51ebe8702883756b613afb
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-06-16 10:53:55 +0200

    Add check for template

--- includes/wp-admin/class-welcome-fields.php
+++ includes/wp-admin/class-welcome-fields.php
@@ -36,11 +36,18 @@ class Welcome_Fields {
 	 * Enqueue styles.
 	 */
 	public static function enqueue_styles() {
-		\wp_enqueue_style(
-			'activitypub-welcome',
-			\plugins_url( 'assets/css/activitypub-welcome.css', ACTIVITYPUB_PLUGIN_FILE ),
-			array(),
-			ACTIVITYPUB_PLUGIN_VERSION
+		add_action(
+			'wp_after_load_template',
+			function ( $template ) {
+				if ( str_ends_with( $template, 'welcome.php' ) ) {
+					\wp_enqueue_style(
+						'activitypub-welcome',
+						\plugins_url( 'assets/css/activitypub-welcome.css', ACTIVITYPUB_PLUGIN_FILE ),
+						array(),
+						ACTIVITYPUB_PLUGIN_VERSION
+					);
+				}
+			}
 		);
 	}
 
