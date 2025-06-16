f35b910927ad6115ba6e69afd974e3447f4f9cd7
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-06-16 11:40:45 +0200

    Fix `activitypub_max_image_attachments` check

--- includes/transformer/class-post.php
+++ includes/transformer/class-post.php
@@ -283,8 +283,11 @@ class Post extends Base {
 			return array();
 		}
 
-		// phpcs:ignore Universal.Operators.DisallowShortTernary
-		$max_media = \get_post_meta( $this->item->ID, 'activitypub_max_image_attachments', true ) ?: \get_option( 'activitypub_max_image_attachments', ACTIVITYPUB_MAX_IMAGE_ATTACHMENTS );
+		$max_media = \get_post_meta( $this->item->ID, 'activitypub_max_image_attachments', true );
+
+		if ( ! is_numeric( $max_media ) ) {
+			$max_media = \get_option( 'activitypub_max_image_attachments', ACTIVITYPUB_MAX_IMAGE_ATTACHMENTS );
+		}
 
 		/**
 		 * Filters the maximum number of media attachments allowed in a post.
