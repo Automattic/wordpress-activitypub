03cfa10f7e6f1292cbd9c6edef3c1c22118ba975
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-06-19 17:46:37 +0200

    remove duplicate cache clear

--- includes/handler/class-move.php
+++ includes/handler/class-move.php
@@ -75,8 +75,6 @@ class Move {
 				array( 'ID' => sanitize_key( $origin_object->ID ) )
 			);
 
-			\wp_cache_delete( $origin_object->ID, 'posts' );
-
 			Actors::upsert( $target_json );
 
 			// Clear the cache.
