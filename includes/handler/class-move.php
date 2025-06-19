47402bf5df0fef8e333eb8a4a9ffd33ffb967357
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-06-19 18:10:52 +0200

    cache should be before update

--- includes/handler/class-move.php
+++ includes/handler/class-move.php
@@ -75,10 +75,10 @@ class Move {
 				array( 'ID' => sanitize_key( $origin_object->ID ) )
 			);
 
-			Actors::upsert( $target_json );
-
 			// Clear the cache.
 			\wp_cache_delete( $origin_object->ID, 'posts' );
+
+			Actors::upsert( $target_json );
 			return;
 		}
 
