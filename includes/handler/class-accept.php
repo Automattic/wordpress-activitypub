5f861b38b6f345b3ed9f62340f558b11fbad4ddc
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-06-24 11:06:08 +0200

    validate request

--- includes/handler/class-accept.php
+++ includes/handler/class-accept.php
@@ -9,6 +9,8 @@ namespace Activitypub\Handler;
 
 use Activitypub\Collection\Following;
 
+use function Activitypub\object_to_uri;
+
 /**
  * Handle Accept requests.
  */
@@ -23,15 +25,57 @@ class Accept {
 			10,
 			2
 		);
+
+		\add_filter(
+			'activitypub_validate_object',
+			array( self::class, 'validate_object' ),
+			10,
+			3
+		);
 	}
 
 	/**
 	 * Handles "Accept" requests.
 	 *
-	 * @param array $accept The activity-object.
+	 * @param array $accept  The activity-object.
 	 * @param int   $user_id The id of the local blog-user.
 	 */
 	public static function handle_accept( $accept, $user_id ) {
-		Following::accept( $accept['object']['id'], $user_id );
+		Following::accept( object_to_uri( $accept['object'] ), $user_id );
+	}
+
+	/**
+	 * Validate the object.
+	 *
+	 * @param bool             $valid   The validation state.
+	 * @param string           $param   The object parameter.
+	 * @param \WP_REST_Request $request The request object.
+	 *
+	 * @return bool The validation state: true if valid, false if not.
+	 */
+	public static function validate_object( $valid, $param, $request ) {
+		$json_params = $request->get_json_params();
+
+		if ( empty( $json_params['type'] ) ) {
+			return false;
+		}
+
+		if (
+			'Accept' !== $json_params['type'] ||
+			\is_wp_error( $request )
+		) {
+			return $valid;
+		}
+
+		$required = array(
+			'actor',
+			'object',
+		);
+
+		if ( \array_intersect( $required, \array_keys( $json_params ) ) !== $required ) {
+			return false;
+		}
+
+		return $valid;
 	}
 }
