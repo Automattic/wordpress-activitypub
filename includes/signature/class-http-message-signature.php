689367d8783ec45c643a313058475ee7aa56dd82
Konstantin Obenland <obenland@gmx.de>
2025-07-07 13:01:09 -0500

    Signature: Examine public key when no algorithm passed
    Moved supported algorithm definitions to a class property and updated verify_algorithm() to use this property instead of a local map. Also improved handling of empty algorithm strings by inferring the algorithm from the public key details.

--- includes/signature/class-http-message-signature.php
+++ includes/signature/class-http-message-signature.php
@@ -23,6 +23,55 @@ use Activitypub\Collection\Actors;
 class Http_Message_Signature implements Signature_Standard {
 
 	/**
+	 * Supported algorithms.
+	 *
+	 * @var int[][]
+	 */
+	private $algorithms = array(
+		// RSA PKCS#1 v1.5.
+		'rsa-v1_5-sha256'   => array(
+			'type' => OPENSSL_KEYTYPE_RSA,
+			'algo' => OPENSSL_ALGO_SHA256,
+		),
+		'rsa-v1_5-sha384'   => array(
+			'type' => OPENSSL_KEYTYPE_RSA,
+			'algo' => OPENSSL_ALGO_SHA384,
+		),
+		'rsa-v1_5-sha512'   => array(
+			'type' => OPENSSL_KEYTYPE_RSA,
+			'algo' => OPENSSL_ALGO_SHA512,
+		),
+
+		// RSA PSS (note: not supported in openssl_verify() until PHP 8.1).
+		'rsa-pss-sha256'    => array(
+			'type' => OPENSSL_KEYTYPE_RSA,
+			'algo' => OPENSSL_ALGO_SHA256,
+		),
+		'rsa-pss-sha384'    => array(
+			'type' => OPENSSL_KEYTYPE_RSA,
+			'algo' => OPENSSL_ALGO_SHA384,
+		),
+		'rsa-pss-sha512'    => array(
+			'type' => OPENSSL_KEYTYPE_RSA,
+			'algo' => OPENSSL_ALGO_SHA512,
+		),
+
+		// ECDSA.
+		'ecdsa-p256-sha256' => array(
+			'type' => OPENSSL_KEYTYPE_EC,
+			'algo' => OPENSSL_ALGO_SHA256,
+		),
+		'ecdsa-p384-sha384' => array(
+			'type' => OPENSSL_KEYTYPE_EC,
+			'algo' => OPENSSL_ALGO_SHA384,
+		),
+		'ecdsa-p521-sha512' => array(
+			'type' => OPENSSL_KEYTYPE_EC,
+			'algo' => OPENSSL_ALGO_SHA512,
+		),
+	);
+
+	/**
 	 * Generate RFC-9421 compliant Signature-Input and Signature headers for an outgoing HTTP request.
 	 *
 	 * @param array  $args The request arguments.
@@ -243,69 +292,52 @@ class Http_Message_Signature implements Signature_Standard {
 	 * @return int|\WP_Error OpenSSL algorithm constant or WP_Error.
 	 */
 	private function verify_algorithm( $alg_string, $public_key ) {
-		$alg_string = \strtolower( $alg_string );
-		if ( \strpos( $alg_string, 'rsa-pss-' ) === 0 && \version_compare( PHP_VERSION, '8.1.0', '<' ) ) {
-			return new \WP_Error( 'unsupported_pss', 'RSA-PSS algorithms are not supported.' );
-		}
-
 		$details = \openssl_pkey_get_details( $public_key );
 		if ( ! isset( $details['type'] ) ) {
 			return new \WP_Error( 'invalid_key_details', 'Unable to read public key details.' );
 		}
 
-		$map = array(
-			// RSA PKCS#1 v1.5.
-			'rsa-v1_5-sha256'   => array(
-				'type' => OPENSSL_KEYTYPE_RSA,
-				'algo' => OPENSSL_ALGO_SHA256,
-			),
-			'rsa-v1_5-sha384'   => array(
-				'type' => OPENSSL_KEYTYPE_RSA,
-				'algo' => OPENSSL_ALGO_SHA384,
-			),
-			'rsa-v1_5-sha512'   => array(
-				'type' => OPENSSL_KEYTYPE_RSA,
-				'algo' => OPENSSL_ALGO_SHA512,
-			),
+		// If alg_string is empty, determine algorithm based on public key.
+		if ( empty( $alg_string ) ) {
+			switch ( $details['type'] ) {
+				case \OPENSSL_KEYTYPE_RSA:
+					$bits = $details['bits'] ?? 2048;
 
-			// RSA PSS (note: not supported in openssl_verify() until PHP 8.1).
-			'rsa-pss-sha256'    => array(
-				'type' => OPENSSL_KEYTYPE_RSA,
-				'algo' => OPENSSL_ALGO_SHA256,
-			),
-			'rsa-pss-sha384'    => array(
-				'type' => OPENSSL_KEYTYPE_RSA,
-				'algo' => OPENSSL_ALGO_SHA384,
-			),
-			'rsa-pss-sha512'    => array(
-				'type' => OPENSSL_KEYTYPE_RSA,
-				'algo' => OPENSSL_ALGO_SHA512,
-			),
+					if ( $bits >= 4 * KB_IN_BYTES ) {
+						return \OPENSSL_ALGO_SHA512;
+					} elseif ( $bits >= 3 * KB_IN_BYTES ) {
+						return \OPENSSL_ALGO_SHA384;
+					} else {
+						return \OPENSSL_ALGO_SHA256;
+					}
 
-			// ECDSA.
-			'ecdsa-p256-sha256' => array(
-				'type' => OPENSSL_KEYTYPE_EC,
-				'algo' => OPENSSL_ALGO_SHA256,
-			),
-			'ecdsa-p384-sha384' => array(
-				'type' => OPENSSL_KEYTYPE_EC,
-				'algo' => OPENSSL_ALGO_SHA384,
-			),
-			'ecdsa-p521-sha512' => array(
-				'type' => OPENSSL_KEYTYPE_EC,
-				'algo' => OPENSSL_ALGO_SHA512,
-			),
-		);
+				case \OPENSSL_KEYTYPE_EC:
+					switch ( $details['ec']['curve_name'] ?? '' ) {
+						case 'prime256v1':
+						case 'secp256r1':
+							return \OPENSSL_ALGO_SHA256;
+						case 'secp384r1':
+							return \OPENSSL_ALGO_SHA384;
+						case 'secp521r1':
+							return \OPENSSL_ALGO_SHA512;
+					}
+			}
+		}
 
-		if ( ! isset( $map[ $alg_string ] ) ) {
+		$alg_string = \strtolower( $alg_string );
+		if ( \strpos( $alg_string, 'rsa-pss-' ) === 0 && \version_compare( PHP_VERSION, '8.1.0', '<' ) ) {
+			return new \WP_Error( 'unsupported_pss', 'RSA-PSS algorithms are not supported.' );
+		}
+
+		if ( ! isset( $this->algorithms[ $alg_string ] ) ) {
 			return new \WP_Error( 'unsupported_alg', 'Unsupported or unknown alg parameter: ' . $alg_string );
 		}
 
-		if ( $map[ $alg_string ]['type'] !== $details['type'] ) {
+		if ( $this->algorithms[ $alg_string ]['type'] !== $details['type'] ) {
 			return new \WP_Error( 'alg_key_mismatch', 'Algorithm does not match public key type.' );
 		}
 
-		return $map[ $alg_string ]['algo'];
+		return $this->algorithms[ $alg_string ]['algo'];
 	}
 
 	/**
