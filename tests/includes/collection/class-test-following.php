6cb26d713560e0a903a8ec3eb929b13294e63401
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-07-08 08:56:06 +0200

    also test blog and application user

--- tests/includes/collection/class-test-following.php
+++ tests/includes/collection/class-test-following.php
@@ -349,10 +349,14 @@ class Test_Following extends \WP_UnitTestCase {
 		$outbox_item_1 = \get_post( follow( 'https://example.com/actor/1', $user_ids[0] ) );
 		$outbox_item_2 = \get_post( follow( 'https://example.com/actor/1', $user_ids[1] ) );
 		$outbox_item_3 = \get_post( follow( 'https://example.com/actor/1', $user_ids[2] ) );
+		$outbox_item_4 = \get_post( follow( 'https://example.com/actor/1', 0 ) );
+		$outbox_item_5 = \get_post( follow( 'https://example.com/actor/1', -1 ) );
 
 		wp_publish_post( $outbox_item_1 );
 		wp_publish_post( $outbox_item_2 );
 		wp_publish_post( $outbox_item_3 );
+		wp_publish_post( $outbox_item_4 );
+		wp_publish_post( $outbox_item_5 );
 
 		$accept_1 = array(
 			'object' => array(
@@ -372,18 +376,47 @@ class Test_Following extends \WP_UnitTestCase {
 				'actor' => 'https://example.com/actor/1',
 			),
 		);
+		$accept_4 = array(
+			'object' => array(
+				'id'    => $outbox_item_4->guid,
+				'actor' => 'https://example.com/actor/1',
+			),
+		);
+		$accept_5 = array(
+			'object' => array(
+				'id'    => $outbox_item_5->guid,
+				'actor' => 'https://example.com/actor/1',
+			),
+		);
 
 		Accept::handle_accept( $accept_1, $user_ids[0] );
 		Accept::handle_accept( $accept_2, $user_ids[1] );
 		Accept::handle_accept( $accept_3, $user_ids[2] );
+		Accept::handle_accept( $accept_4, 0 );
+		Accept::handle_accept( $accept_5, -1 );
 
 		// User 1 follows https://example.com/actor/1.
 		$following = Following::get_following_with_count( $user_ids[0] );
 		$this->assertCount( 1, $following['following'] );
 		$this->assertSame( 1, $following['total'] );
 
+		$following = Following::get_following_with_count( -1 );
+		$this->assertCount( 1, $following['following'] );
+		$this->assertSame( 1, $following['total'] );
+
 		// User 3 unfollows https://example.com/actor/1.
 		Following::unfollow( Actors::get_remote_by_uri( 'https://example.com/actor/1' ), $user_ids[2] );
+
+		// User 3 unfollows https://example.com/actor/1.
+		Following::unfollow( Actors::get_remote_by_uri( 'https://example.com/actor/1' ), 0 );
+
+		$following = Following::get_following_with_count( 0 );
+		$this->assertCount( 0, $following['following'] );
+		$this->assertSame( 0, $following['total'] );
+
+		$following = Following::get_following_with_count( -1 );
+		$this->assertCount( 1, $following['following'] );
+		$this->assertSame( 1, $following['total'] );
 
 		// User 1 still follows https://example.com/actor/1.
 		$posts = get_posts(
