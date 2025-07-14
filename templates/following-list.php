24759e1c0e2f191208ea8f966f96a986b8cacf97
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-07-14 18:30:59 +0200

    Do not mention "Author"

--- templates/following-list.php
+++ templates/following-list.php
@@ -16,7 +16,7 @@ $_status = \sanitize_text_field( \wp_unslash( $_REQUEST['status'] ?? 'accepted' 
 $table->prepare_items();
 ?>
 <div class="wrap">
-	<h1 class="wp-heading-inline"><?php esc_html_e( 'Author Followings', 'activitypub' ); ?></h1>
+	<h1 class="wp-heading-inline"><?php esc_html_e( 'Followings', 'activitypub' ); ?></h1>
 
 	<?php
 	if ( strlen( $_search ) ) :
