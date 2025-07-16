8dcb247f386317a3914cf07c8aa066e23b475bdd
Konstantin Obenland <obenland@gmx.de>
2025-07-16 13:25:16 -0500

    Account for multiple users

--- includes/wp-admin/table/class-followers.php
+++ includes/wp-admin/table/class-followers.php
@@ -136,6 +136,8 @@ class Followers extends \WP_List_Table {
 				break;
 
 			case 'block':
+				$redirect_to = \remove_query_arg( array( 'follower', 'followers' ), $redirect_to );
+
 				// Handle single follower block.
 				if ( isset( $_GET['follower'], $_GET['_wpnonce'] ) ) {
 					$follower = \esc_url_raw( \wp_unslash( $_GET['follower'] ) );
