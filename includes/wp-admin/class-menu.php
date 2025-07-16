5dbf5b7c20cc12ed4d39633ddfc1e6515a12a027
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-07-16 18:32:29 +0200

    remove menu

--- includes/wp-admin/class-menu.php
+++ includes/wp-admin/class-menu.php
@@ -61,14 +61,6 @@ class Menu {
 
 				\add_action( 'load-' . $following_list_page, array( Admin::class, 'add_following_list_table' ) );
 				\add_action( 'load-' . $following_list_page, array( Screen_Options::class, 'add_following_list_options' ) );
-
-				\add_users_page(
-					\__( 'Follow ⁂', 'activitypub' ),
-					\__( 'Follow ⁂', 'activitypub' ),
-					'activitypub',
-					'activitypub-follow',
-					array( Admin::class, 'follow_page' )
-				);
 			}
 
 			\add_users_page(
