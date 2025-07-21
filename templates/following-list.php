be066a2152772f69a88d1bd60ea8f982cf15ee9c
Konstantin Obenland <obenland@gmx.de>
2025-07-21 10:23:30 -0500

    Fix comma placement

--- templates/following-list.php
+++ templates/following-list.php
@@ -78,7 +78,7 @@ $following_list_table->prepare_items();
 					</form>
 					<div class="edit-term-notes">
 						<strong><?php esc_html_e( 'About Followings', 'activitypub' ); ?></strong>
-						<p class="description"><?php esc_html_e( 'When you follow another author, a follow request is sent on your behalf. If you see &#8220;Pending,&#8212;&#8221; it means your follow request hasn&#8217;t been accepted yet&#8212;so you aren&#8217;t following that author until they approve your request. This is a normal part of the ActivityPub protocol and helps ensure that authors have control over who follows them.', 'activitypub' ); ?></p>
+						<p class="description"><?php esc_html_e( 'When you follow another author, a follow request is sent on your behalf. If you see &#8220;Pending&#8221;, it means your follow request hasn&#8217;t been accepted yet&#8212;so you aren&#8217;t following that author until they approve your request. This is a normal part of the ActivityPub protocol and helps ensure that authors have control over who follows them.', 'activitypub' ); ?></p>
 					</div>
 				</div>
 			</div>
