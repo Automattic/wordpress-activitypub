a48d1cd57df02ba10d382f985137b1417a481826
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-07-22 11:37:55 +0200

    simplify calls

--- includes/table/class-followers.php
+++ includes/table/class-followers.php
@@ -205,8 +205,7 @@ class Followers extends \WP_List_Table {
 				continue;
 			}
 
-			$url       = object_to_uri( $actor->get_url() ?? $actor->get_id() );
-			$webfinger = self::get_webfinger( $actor );
+			$url = object_to_uri( $actor->get_url() ?? $actor->get_id() );
 
 			$this->items[] = array(
 				'id'         => $follower->ID,
@@ -214,7 +213,7 @@ class Followers extends \WP_List_Table {
 				'post_title' => $actor->get_name() ?? $actor->get_preferred_username(),
 				'username'   => $actor->get_preferred_username(),
 				'url'        => $url,
-				'webfinger'  => $webfinger,
+				'webfinger'  => self::get_webfinger( $actor ),
 				'identifier' => $actor->get_id(),
 				'modified'   => $follower->post_modified_gmt,
 			);
