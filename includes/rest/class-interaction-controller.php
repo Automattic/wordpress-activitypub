f3d5c29f55f0b632ccd71618ed4674507f214b28
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-07-24 16:03:09 +0200

    urlencode

--- includes/rest/class-interaction-controller.php
+++ includes/rest/class-interaction-controller.php
@@ -106,6 +106,9 @@ class Interaction_Controller extends \WP_REST_Controller {
 			$uri = \esc_url( $object['id'] );
 		}
 
+		// Prepare URL parameter.
+		$url_param = \rawurlencode( $uri );
+
 		switch ( $object['type'] ) {
 			case 'Group':
 			case 'Person':
@@ -114,9 +117,9 @@ class Interaction_Controller extends \WP_REST_Controller {
 			case 'Organization':
 				if ( boolval( get_option( 'activitypub_following_ui', '0' ) ) ) {
 					if ( user_can_activitypub( \get_current_user_id() ) ) {
-						$redirect_url = \admin_url( 'users.php?page=activitypub-following-list&resource=' . $uri );
+						$redirect_url = \admin_url( 'users.php?page=activitypub-following-list&resource=' . $url_param );
 					} elseif ( user_can_activitypub( Actors::BLOG_USER_ID ) ) {
-						$redirect_url = \admin_url( 'options-general.php?page=activitypub&tab=following&resource=' . $uri );
+						$redirect_url = \admin_url( 'options-general.php?page=activitypub&tab=following&resource=' . $url_param );
 					}
 				}
 
@@ -130,7 +133,7 @@ class Interaction_Controller extends \WP_REST_Controller {
 				$redirect_url = \apply_filters( 'activitypub_interactions_follow_url', $redirect_url, $uri, $object );
 				break;
 			default:
-				$redirect_url = \admin_url( 'post-new.php?in_reply_to=' . $uri );
+				$redirect_url = \admin_url( 'post-new.php?in_reply_to=' . $url_param );
 				/**
 				 * Filters the URL used for replying to an ActivityPub object.
 				 *
