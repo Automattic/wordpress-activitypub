2dc3a0408cd028c4d57ed84fbe7d1b113f7bb462
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-07-24 14:58:21 +0200

    rebuild JS and linting

--- build/reply/index.asset.php
+++ build/reply/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'e4cc0f6ffd681f4d678d');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '8bc11d3b2b7c893c3032');
