dfd24de1513c72f32efaaf0ea4796718ed306549
Konstantin Obenland <obenland@gmx.de>
2025-07-25 14:31:48 -0500

    Fix user selection for non-admin users in ActivityPub blocks
    Non-admin users were unable to select users in Follow Me and Followers blocks
    due to lack of list_users capability. This fix adds fallback logic to allow
    users with activitypub capability to select themselves when the full user
    list query fails.
    
    Changes:
    - Modified useUserOptions hook to fallback to current user when getUsers() fails
    - Added HEAD request to ActivityPub actor endpoint to verify user capability
    - Includes built JavaScript files
    
    Fixes #1790

--- build/reactions/index.asset.php
+++ build/reactions/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '26b867e95d84dc34fc93');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '7f460a8fc211166884fa');
