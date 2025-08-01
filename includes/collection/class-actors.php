07db57fd07f45434ceb9da5a4bf22aab10c04a09
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-08-01 20:33:58 +0200

    Update return type annotations in Actors class
    Adjusted the PHPDoc return types for get_by_id and get_by_username methods to accurately reflect possible return values, including Actor and \WP_Error types.

--- includes/collection/class-actors.php
+++ includes/collection/class-actors.php
@@ -54,7 +54,7 @@ class Actors {
 	 *
 	 * @param int $user_id The user ID.
 	 *
-	 * @return User|Blog|Application|\WP_Error Actor object or WP_Error if not found.
+	 * @return Actor|User|Blog|Application|\WP_Error Actor object or WP_Error if not found or not permitted.
 	 */
 	public static function get_by_id( $user_id ) {
 		if ( is_numeric( $user_id ) ) {
@@ -84,7 +84,7 @@ class Actors {
 	 *
 	 * @param string $username Name of the actor.
 	 *
-	 * @return User|Blog|Application|WP_Error Actor object or WP_Error if not found.
+	 * @return User|Blog|Application|\WP_Error Actor object or WP_Error if not found.
 	 */
 	public static function get_by_username( $username ) {
 		/**
