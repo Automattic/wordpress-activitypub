02b64598218b5b1d3f2a0976986630b4b7f4fc92
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-08-01 12:49:17 +0200

    do not rely on get_collection

--- includes/class-migration.php
+++ includes/class-migration.php
@@ -955,11 +955,17 @@ class Migration {
 		\add_option( 'activitypub_blog_user_mailer_new_follower', $new_follower );
 		\add_option( 'activitypub_blog_user_mailer_new_mention', '1' );
 
+		$users = \get_users(
+			array(
+				'capability__in' => array( 'activitypub' ),
+			)
+		);
+
 		// Add the actor notification options.
-		foreach ( Actors::get_collection() as $actor ) {
-			\update_user_option( $actor->get__id(), 'activitypub_mailer_new_dm', $new_dm );
-			\update_user_option( $actor->get__id(), 'activitypub_mailer_new_follower', $new_follower );
-			\update_user_option( $actor->get__id(), 'activitypub_mailer_new_mention', '1' );
+		foreach ( $users as $user ) {
+			\update_user_option( $user->ID, 'activitypub_mailer_new_dm', $new_dm );
+			\update_user_option( $user->ID, 'activitypub_mailer_new_follower', $new_follower );
+			\update_user_option( $user->ID, 'activitypub_mailer_new_mention', '1' );
 		}
 
 		// Delete the old notification options.
