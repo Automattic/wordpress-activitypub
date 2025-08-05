d0177938676631ce1c4a574858f411fd2c9ecbab
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-08-05 11:50:13 +0200

    Improve variable alignment in Starter_Kit class
    Aligned variable assignments for better readability in the Starter_Kit class within the import module. No functional changes were made.

--- includes/wp-admin/import/class-starter-kit.php
+++ includes/wp-admin/import/class-starter-kit.php
@@ -231,10 +231,10 @@ class Starter_Kit {
 		}
 
 		// Create a temporary file to store the JSON content.
-		$upload_dir = \wp_upload_dir();
-		$base_filename = 'starter-kit.json';
+		$upload_dir      = \wp_upload_dir();
+		$base_filename   = 'starter-kit.json';
 		$unique_filename = \wp_unique_filename( $upload_dir['path'], $base_filename );
-		$temp_file  = \trailingslashit( $upload_dir['path'] ) . $unique_filename;
+		$temp_file       = \trailingslashit( $upload_dir['path'] ) . $unique_filename;
 		\WP_Filesystem();
 		global $wp_filesystem;
 
