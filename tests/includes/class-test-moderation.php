02cf12537381a49e6cfb179bd856361e0aa2d817
Konstantin Obenland <obenland@gmx.de>
2025-08-20 14:20:49 -0500

    Create a warning

--- tests/includes/class-test-moderation.php
+++ tests/includes/class-test-moderation.php
@@ -373,7 +373,7 @@ class Test_Moderation extends \WP_UnitTestCase {
 	/**
 	 * Test edge cases with malformed activity data.
 	 *
-	 * @covers ::activity_is_blocked
+	 * @covers ::activity_is_blockeded
 	 * @throws \Exception Thrown when an error occurs.
 	 */
 	public function test_activity_blocking_edge_cases() {
