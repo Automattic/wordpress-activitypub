5b53e1d26c5ddf8a5c10158c3e5d7e28eeea40fb
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-08-25 22:48:13 +0200

    Restrict inbox retrieval to Delete activities
    Updated get_inboxes_for_activity to only retrieve all inboxes for 'Delete' activity types, instead of both 'Update' and 'Delete'. This change ensures that only delete actions trigger the broader inbox notification.

--- includes/collection/class-followers.php
+++ includes/collection/class-followers.php
@@ -314,7 +314,7 @@ class Followers {
 	public static function get_inboxes_for_activity( $json, $actor_id, $batch_size = 50, $offset = 0 ) {
 		$activity = \json_decode( $json, true );
 		// Only if this is an Update or Delete. Create handles its own "Announce" in dual user mode.
-		if ( \in_array( $activity['type'] ?? null, array( 'Update', 'Delete' ), true ) ) {
+		if ( 'Delete' === ( $activity['type'] ?? null ) ) {
 			$inboxes = Actors::get_inboxes();
 		} else {
 			$inboxes = self::get_inboxes( $actor_id );
