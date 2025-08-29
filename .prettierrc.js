0c1dd2c77507019fdb066607bd148c6dcd489c20
Konstantin Obenland <obenland@gmx.de>
2025-08-29 15:33:41 -0500

    Remove prettier PHP

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,22 +4,12 @@ module.exports = {
 	...wpPrettierConfig,
 
 	printWidth: 120,
-	plugins: [ '@prettier/plugin-php' ],
 	overrides: [
 		{
 			files: '*.yml',
 			options: {
 				useTabs: false,
 				tabWidth: 2,
-			},
-		},
-		{
-			files: '*.php',
-			options: {
-				phpVersion: '7.4',
-				braceStyle: '1tbs',
-				tabWidth: 4,
-				useTabs: true,
 			},
 		},
 	],
