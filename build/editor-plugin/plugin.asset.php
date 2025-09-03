2a3eee64656b3d55431b57ba83f18d82f21a065d
Konstantin Obenland <obenland@gmx.de>
2025-09-03 16:03:47 -0500

    Improve attachment selection and drag-and-drop UI
    Enhances the editor plugin's attachment selector to combine both post_parent and content-referenced attachments, deduplicate them, and provide a more robust drag-and-drop reordering experience. The UI is updated for better usability, and the logic for determining media types and selection is improved. Removes the maximum attachment limit and RangeControl, and replaces it with a more flexible interface.

--- build/editor-plugin/plugin.asset.php
+++ build/editor-plugin/plugin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => 'd9d30bb7168d9e1faaf8');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => 'c395ce9d7d54e11a0214');
