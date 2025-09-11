f002a3721331887621f5f2d8909ddc0ac3f55a63
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-09-11 10:03:32 +0200

    Add hooks above and below settings forms
    Introduced do_action hooks before and after the settings forms in blog-settings.php and settings.php to allow extensibility for additional content or functionality.

--- templates/settings.php
+++ templates/settings.php
@@ -10,9 +10,11 @@
 <hr class="wp-header-end">
 
 <div class="activitypub-settings activitypub-settings-page hide-if-no-js">
+	<?php \do_action( 'above_activitypub_settings' ); ?>
 	<form method="post" action="options.php">
 		<?php \settings_fields( 'activitypub' ); ?>
 		<?php \do_settings_sections( 'activitypub_settings' ); ?>
 		<?php \submit_button(); ?>
 	</form>
+	<?php \do_action( 'below_activitypub_settings' ); ?>
 </div>
