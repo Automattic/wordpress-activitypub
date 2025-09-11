22143c09bf40beac7a43e5dcb6f6dc6c73e42637
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-09-11 09:37:53 +0200

    Refactor Mastodon oEmbed provider registration
    Moved Mastodon oEmbed provider registration to a new method 'register_fediverse_providers' and updated the init hook to use it. Removed the redundant 'register_oembed_providers' method for better clarity and maintainability.

--- includes/class-embed.php
+++ includes/class-embed.php
@@ -16,10 +16,22 @@ class Embed {
 	 * Initialize the embed handler
 	 */
 	public static function init() {
+		\add_action( 'init', array( self::class, 'register_fediverse_providers' ), 11 );
 		\add_filter( 'pre_oembed_result', array( self::class, 'maybe_use_activitypub_embed' ), 10, 3 );
 		\add_filter( 'oembed_dataparse', array( self::class, 'handle_filtered_oembed_result' ), 11, 3 );
 		\add_filter( 'oembed_request_post_id', array( self::class, 'register_fallback_hook' ) );
-		\add_action( 'init', array( self::class, 'register_oembed_providers' ), 11 );
+	}
+
+	/**
+	 * Register some Mastodon oEmbed providers.
+	 */
+	public static function register_fediverse_providers() {
+		\wp_oembed_add_provider( '#https?://mastodon\.social/(@.+)/([0-9]+)#i', 'https://mastodon.social/api/oembed', true );
+		\wp_oembed_add_provider( '#https?://mastodon\.online/(@.+)/([0-9]+)#i', 'https://mastodon.online/api/oembed', true );
+		\wp_oembed_add_provider( '#https?://mastodon\.cloud/(@.+)/([0-9]+)#i', 'https://mastodon.cloud/api/oembed', true );
+		\wp_oembed_add_provider( '#https?://mstdn\.social/(@.+)/([0-9]+)#i', 'https://mstdn.social/api/oembed', true );
+		\wp_oembed_add_provider( '#https?://mastodon\.world/(@.+)/([0-9]+)#i', 'https://mastodon.world/api/oembed', true );
+		\wp_oembed_add_provider( '#https?://mas\.to/(@.+)/([0-9]+)#i', 'https://mas.to/api/oembed', true );
 	}
 
 	/**
@@ -281,17 +293,5 @@ class Embed {
 		}
 
 		return $response;
-	}
-
-	/**
-	 * Register some Mastodon oEmbed providers.
-	 */
-	public static function register_oembed_providers() {
-		\wp_oembed_add_provider( '#https?://mastodon\.social/(@.+)/([0-9]+)#i', 'https://mastodon.social/api/oembed', true );
-		\wp_oembed_add_provider( '#https?://mastodon\.online/(@.+)/([0-9]+)#i', 'https://mastodon.online/api/oembed', true );
-		\wp_oembed_add_provider( '#https?://mastodon\.cloud/(@.+)/([0-9]+)#i', 'https://mastodon.cloud/api/oembed', true );
-		\wp_oembed_add_provider( '#https?://mstdn\.social/(@.+)/([0-9]+)#i', 'https://mstdn.social/api/oembed', true );
-		\wp_oembed_add_provider( '#https?://mastodon\.world/(@.+)/([0-9]+)#i', 'https://mastodon.world/api/oembed', true );
-		\wp_oembed_add_provider( '#https?://mas\.to/(@.+)/([0-9]+)#i', 'https://mas.to/api/oembed', true );
 	}
 }
