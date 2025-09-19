5f5d7f86ef889e12461a3a0040fff4b532cc4886
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-09-19 08:23:52 +0200

    Format help text for SelectControl in editor plugin
    Refactored the help prop for the 'Who can quote this post?' SelectControl to use a multi-line format for improved readability and maintainability.

--- build/editor-plugin/plugin.asset.php
+++ build/editor-plugin/plugin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => '4fad8f2d381cbf53a288');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => 'faa910495b60327c9360');
