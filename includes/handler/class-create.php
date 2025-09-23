f9b7ec0686b304bf846347c43bd0dea929473565
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-09-23 15:54:29 +0200

    Update includes/handler/class-create.php
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>

--- includes/handler/class-create.php
+++ includes/handler/class-create.php
@@ -71,10 +71,10 @@ class Create {
 		/**
 		 * Fires after an ActivityPub Create activity has been handled.
 		 *
-		 * @param array      $activity The ActivityPub activity data.
-		 * @param int.       $user_id  The local user ID, or null if not applicable.
-		 * @param mixed      $state    The state/result of the operation (e.g., comment ID, WP_Error, or status).
-		 * @param mixed|null $result   The WP_Comment object of the created comment, or null if creation failed.
+		 * @param array               $activity The ActivityPub activity data.
+		 * @param int                 $user_id  The local user ID.
+		 * @param int|false|\WP_Error $state    The state/result of the operation (e.g., comment ID, WP_Error, or status).
+		 * @param \WP_Comment|null    $result   The WP_Comment object of the created comment, or null if creation failed.
 		 */
 		\do_action( 'activitypub_handled_create', $activity, $user_id, $state, $result );
 	}
