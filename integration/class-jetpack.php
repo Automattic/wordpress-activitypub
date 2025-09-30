07311327e78655e0c29be54f5ca3a7ad4a6570b4
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-09-30 13:14:11 +0200

    Add external link icon to Jetpack reader feed link
    Updated the Jetpack integration to add the 'activitypub-external' class to the reader feed link, and added CSS to display a dashicon after external links. This visually indicates external links in the admin interface.

--- integration/class-jetpack.php
+++ integration/class-jetpack.php
@@ -10,6 +10,7 @@ namespace Activitypub\Integration;
 use Activitypub\Collection\Followers;
 use Activitypub\Collection\Following;
 use Activitypub\Comment;
+use Automattic\Jetpack\Connection\Manager;
 
 /**
  * Jetpack integration class.
@@ -30,7 +31,7 @@ class Jetpack {
 
 		if (
 			( \defined( 'IS_WPCOM' ) && IS_WPCOM ) ||
-			( \class_exists( '\Jetpack' ) && \Jetpack::is_connection_ready() )
+			( \class_exists( 'Automattic\Jetpack\Connection\Manager' ) && ( new Manager() )->is_user_connected() )
 		) {
 			\add_filter( 'activitypub_following_row_actions', array( self::class, 'add_reader_link' ), 10, 2 );
 			\add_filter( 'pre_option_activitypub_following_ui', array( self::class, 'pre_option_activitypub_following_ui' ) );
@@ -104,7 +105,7 @@ class Jetpack {
 		return array_merge(
 			array(
 				'reader' => sprintf(
-					'<a href="%s" target="_blank">%s</a>',
+					'<a href="%s" target="_blank" class="activitypub-external">%s</a>',
 					esc_url( $url ),
 					esc_html__( 'View Feed', 'activitypub' )
 				),
