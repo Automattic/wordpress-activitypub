bcdf5431966e0c99229b6203ef23e91238f5f8f5
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-09-30 12:09:09 +0200

    Add support for stamp query var and improve ID functions
    Introduces handling for the 'stamp' query variable in ActivityPub queries, enabling the creation of QuoteAuthorization objects from post meta. Updates get_user_id() to respect actor mode and fallback logic, and get_post_id() to generate URIs based on post ID for newer posts, improving compatibility and flexibility.

--- includes/functions.php
+++ includes/functions.php
@@ -1300,15 +1300,27 @@ function get_content_warning( $post_id ) {
 /**
  * Get the ActivityPub ID of a User by the WordPress User ID.
  *
+ * If
+ *
  * @param int $id The WordPress User ID.
  *
  * @return string The ActivityPub ID (a URL) of the User.
  */
 function get_user_id( $id ) {
-	$user = Actors::get_by_id( $id );
+	$mode = \get_option( 'activitypub_actor_mode', 'default' );
+
+	if ( ACTIVITYPUB_BLOG_MODE === $mode ) {
+		$user = Actors::get_by_id( 0 );
+	} else {
+		$user = Actors::get_by_id( $id );
+	}
 
 	if ( \is_wp_error( $user ) ) {
-		return false;
+		if ( ! is_user_type_disabled( 'blog' ) ) {
+			$user = Actors::get_by_id( 0 );
+		} else {
+			return false;
+		}
 	}
 
 	return $user->get_id();
@@ -1322,14 +1334,15 @@ function get_user_id( $id ) {
  * @return string The ActivityPub ID (a URL) of the Post.
  */
 function get_post_id( $id ) {
-	$post = get_post( $id );
+	$last_legacy_id = (int) \get_option( 'activitypub_last_post_with_permalink_as_id', 0 );
+	$post_id        = (int) $id;
 
-	if ( ! $post ) {
-		return false;
+	if ( $post_id > $last_legacy_id ) {
+		// Generate URI based on post ID.
+		return \add_query_arg( 'p', $post_id, \trailingslashit( \home_url() ) );
 	}
 
-	$transformer = new Post( $post );
-	return $transformer->get_id();
+	return \get_permalink( $post_id );
 }
 
 /**
