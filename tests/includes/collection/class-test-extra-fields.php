5afcc6060e9d31854ad764b002b1769a2f92dcf6
Konstantin Obenland <obenland@gmx.de>
2025-10-01 13:19:08 -0500

    Fix HTML entity encoding in extra field names (#2261)

--- tests/includes/collection/class-test-extra-fields.php
+++ tests/includes/collection/class-test-extra-fields.php
@@ -38,4 +38,29 @@ class Test_Extra_Fields extends \WP_UnitTestCase {
 
 		$this->assertEquals( 1, $value_count['me'] );
 	}
+
+	/**
+	 * Test that HTML entities are decoded in field names and values.
+	 *
+	 * @covers ::fields_to_attachments
+	 */
+	public function test_html_entities_decoded() {
+		$post = self::factory()->post->create_and_get(
+			array(
+				'post_type'    => Extra_Fields::BLOG_POST_TYPE,
+				'post_content' => 'Test content with &quot;quotes&quot; and &amp; ampersands',
+				'post_title'   => 'Void&#8217;s Profile',
+			)
+		);
+
+		$attachments = Extra_Fields::fields_to_attachments( array( $post ) );
+
+		// Check PropertyValue has decoded entities in both name and value.
+		$this->assertEquals( 'PropertyValue', $attachments[0]['type'] );
+		// WordPress converts the HTML entity &#8217; to the UTF-8 right single quotation mark character.
+		$expected_name = "Void\u{2019}s Profile";
+		$this->assertEquals( $expected_name, $attachments[0]['name'] );
+		$this->assertStringContainsString( '"quotes"', $attachments[0]['value'] );
+		$this->assertStringContainsString( '& ampersands', $attachments[0]['value'] );
+	}
 }
