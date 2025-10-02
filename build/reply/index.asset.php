cf4b11cbaedc472d1f09e38f7dbfc7269d10baea
Konstantin Obenland <obenland@gmx.de>
2025-10-02 08:13:14 -0500

    Update build files after rebase

--- build/reply/index.asset.php
+++ build/reply/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '8750a8adfeddb1215a5b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '7232ea5e863a340bd748');
