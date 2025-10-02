0e337d709f85e0b126402d76e9c0a1cd00a7bb33
Konstantin Obenland <obenland@gmx.de>
2025-10-02 08:06:58 -0500

    Add bidirectional transforms between reply and embed blocks (#2244)

--- build/reply/index.asset.php
+++ build/reply/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'b24c13a1511313a54eb6');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '54772633de8a75101c7a');
