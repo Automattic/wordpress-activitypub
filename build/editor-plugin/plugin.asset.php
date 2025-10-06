93a9cef4a0463c630ede8d871c0da4dc0263b83b
Jiwoon-Kim <71701140+Jiwoon-Kim@users.noreply.github.com>
2025-10-06 22:11:18 +0900

    Fix: issue #2286 – typo, improved quoting explanation, add missing i18n in advanced settings (#2290)

--- build/editor-plugin/plugin.asset.php
+++ build/editor-plugin/plugin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => '3331b1701416b9746825');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => '12df5cf70e99e55c6091');
