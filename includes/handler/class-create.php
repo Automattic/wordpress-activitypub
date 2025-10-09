bd372ff1fb9d58bc9bff70dd1cfd0e1476536364
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-10-09 12:47:26 +0200

    Restrict comment creation to public activity replies
    Adds a check to ensure that only public activity replies trigger comment creation. Private activities or non-replies are now ignored in handle_create, improving content visibility handling.

--- includes/handler/class-create.php
+++ includes/handler/class-create.php
@@ -34,6 +34,14 @@ class Create {
 	 * @param \Activitypub\Activity\Activity $activity_object Optional. The activity object. Default null.
 	 */
 	public static function handle_create( $activity, $user_id, $activity_object = null ) {
+		// Check if Activity is public or not.
+		if (
+			ACTIVITYPUB_CONTENT_VISIBILITY_PRIVATE === get_activity_visibility( $activity ) ||
+			! is_activity_reply( $activity )
+		) {
+			return;
+		}
+
 		$check_dupe = object_id_to_comment( $activity['object']['id'] );
 
 		// If comment exists, call update action.
@@ -54,15 +62,7 @@ class Create {
 		}
 
 		$success = false;
-		$result  = false;
-
-		// Check if Activity is public or not.
-		if (
-			in_array( get_activity_visibility( $activity ), array( ACTIVITYPUB_CONTENT_VISIBILITY_PUBLIC, ACTIVITYPUB_CONTENT_VISIBILITY_QUIET_PUBLIC ), true ) ||
-			is_activity_reply( $activity )
-		) {
-			$result = Interactions::add_comment( $activity );
-		}
+		$result  = Interactions::add_comment( $activity );
 
 		if ( $result && ! \is_wp_error( $result ) ) {
 			$success = true;
