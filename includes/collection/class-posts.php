ce7d197439dd1182b3abf57ca018aa66502eaaef
Konstantin Obenland <obenland@gmx.de>
2025-10-13 16:02:55 -0500

    Refactor attachment processing calls
    Simplified the Attachments::process method calls by removing unnecessary multi-line formatting and consolidating arguments onto a single line for improved readability.

--- includes/collection/class-posts.php
+++ includes/collection/class-posts.php
@@ -53,11 +53,7 @@ class Posts {
 
 		// Process attachments if present.
 		if ( ! empty( $activity_object['attachment'] ) ) {
-			Attachments::process(
-				$activity_object['attachment'],
-				$post_id,
-				$actor->ID
-			);
+			Attachments::process( $activity_object['attachment'], $post_id, $actor->ID );
 		}
 
 		return \get_post( $post_id );
@@ -137,11 +133,7 @@ class Posts {
 			}
 
 			// Add new attachments.
-			Attachments::process(
-				$activity['object']['attachment'],
-				$post_id,
-				$actor_id
-			);
+			Attachments::process( $activity['object']['attachment'], $post_id, $actor_id );
 		}
 
 		return \get_post( $post_id );
