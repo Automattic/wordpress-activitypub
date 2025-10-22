4daf495a64f07649edf8d8b403987f75841c0d64
Konstantin Obenland <obenland@gmx.de>
2025-10-22 15:23:08 -0500

    Update return types in Inbox and Posts classes
    Adjusted the PHPDoc return type annotations for methods in Inbox and Posts classes to more accurately reflect possible return values, including namespaced types and additional types returned by WordPress functions.

--- includes/collection/class-posts.php
+++ includes/collection/class-posts.php
@@ -132,7 +132,7 @@ class Posts {
 	 *
 	 * @param int $id The object ID.
 	 *
-	 * @return bool|int|null The deleted post ID, false on failure, or null if no post to delete.
+	 * @return array|false|\WP_Post|null The deleted post ID, false on failure, or null if no post to delete.
 	 */
 	public static function delete( $id ) {
 		return \wp_delete_post( $id, true );
@@ -143,7 +143,7 @@ class Posts {
 	 *
 	 * @param string $guid The object GUID.
 	 *
-	 * @return bool|int|null The deleted post ID, false on failure, or null if no post to delete.
+	 * @return array|false|\WP_Error|\WP_Post|null The deleted post ID, false on failure, or null if no post to delete.
 	 */
 	public static function delete_by_guid( $guid ) {
 		$post = self::get_by_guid( $guid );
