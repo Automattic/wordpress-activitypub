a472a99e26cbd98abb8b025dcb0ba99282bcadce
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-10-29 15:07:29 +0100

    Set default avatar URL when icon is missing
    If the actor data does not contain an icon, set and return a default avatar URL, and update the post meta with this value. This ensures that a valid avatar is always available.

--- includes/collection/class-remote-actors.php
+++ includes/collection/class-remote-actors.php
@@ -655,7 +655,10 @@ class Remote_Actors {
 
 		$actor_data = \json_decode( $post->post_content, true );
 		if ( empty( $actor_data['icon'] ) ) {
-			return '';
+			$default_avatar_url = ACTIVITYPUB_PLUGIN_URL . 'assets/img/mp.jpg';
+			\update_post_meta( $id, '_activitypub_avatar_url', \esc_url_raw( $default_avatar_url ) );
+
+			return $default_avatar_url;
 		}
 
 		$avatar_url = object_to_uri( $actor_data['icon'] );
