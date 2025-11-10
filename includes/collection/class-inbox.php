ef524d12e947a96dbad55c43cc5887febe99c2d7
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-11-10 16:48:00 +0100

    Allow short ternary (Elvis) operator in codebase
    Removes all inline phpcs ignore comments for the Universal.Operators.DisallowShortTernary rule and updates phpcs.xml to exclude this rule, permitting use of the short ternary (Elvis) operator throughout the codebase.

--- includes/collection/class-inbox.php
+++ includes/collection/class-inbox.php
@@ -143,11 +143,9 @@ class Inbox {
 			return $post_id ? \get_the_title( $post_id ) : '';
 		}
 
-		// phpcs:ignore Universal.Operators.DisallowShortTernary.Found
 		$title = $activity_object->get_name() ?: $activity_object->get_content();
 
 		if ( ! $title && $activity_object->get_object() instanceof Base_Object ) {
-			// phpcs:ignore Universal.Operators.DisallowShortTernary.Found
 			$title = $activity_object->get_object()->get_name() ?: $activity_object->get_object()->get_content();
 		}
 
