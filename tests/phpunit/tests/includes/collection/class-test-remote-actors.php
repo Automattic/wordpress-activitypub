207dc0cca4d94bda9e9b46704a0d14a369f0f84a
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-12-03 15:31:23 +0100

    Update tests/phpunit/tests/includes/collection/class-test-remote-actors.php
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>

--- tests/phpunit/tests/includes/collection/class-test-remote-actors.php
+++ tests/phpunit/tests/includes/collection/class-test-remote-actors.php
@@ -1339,7 +1339,6 @@ tjUBdXrPxz998Ns/cu9jjg06d+XV3TcSU+AOldmGLJuB/AWV/+F9c9DlczqmnXqd
 		$this->assertEquals( $expected, $actor_obj->get_webfinger() );
 
 		\remove_filter( 'pre_http_request', $webfinger_callback );
-		\wp_delete_post( $post_id, true );
 	}
 
 	/**
