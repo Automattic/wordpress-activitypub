ffe501d50387610307a73dc2e7c31d0e60e0f56d
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-12-08 18:36:41 +0100

    Fix transformer instance caching in test
    Creates a new Post transformer instance in the test to avoid using a cached attachment result, ensuring accurate test behavior.

--- tests/phpunit/tests/includes/transformer/class-test-post.php
+++ tests/phpunit/tests/includes/transformer/class-test-post.php
@@ -426,7 +426,9 @@ class Test_Post extends \WP_UnitTestCase {
 
 		\delete_post_meta( $post_id, 'activitypub_max_image_attachments' );
 
-		$result = $method->invoke( $transformer );
+		// Create a new transformer instance to avoid cached attachment result.
+		$transformer = new Post( $post );
+		$result      = $method->invoke( $transformer );
 		$this->assertTrue( (bool) \did_filter( 'activitypub_attachment_ids' ) );
 	}
 
