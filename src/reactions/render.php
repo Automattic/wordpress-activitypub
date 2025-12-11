de5ebb4c0e4f48d50da47a1e43230a59ab3edeae
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-12-11 17:03:47 +0100

    Update src/reactions/render.php
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>

--- src/reactions/render.php
+++ src/reactions/render.php
@@ -105,6 +105,7 @@ if ( $is_summary ) {
 	if ( empty( $counts ) ) {
 		ob_end_clean();
 		echo '<!-- Reactions block: No reactions found. -->';
+		echo '<!-- Reactions block: No reactions found. -->';
 		return;
 	}
 
