0ba13f9a92100def785f2debdd18fa1353019d05
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-12-11 17:50:03 +0100

    Respect WordPress "show avatars" setting for remote actors (#2611)

--- build/followers/index.asset.php
+++ build/followers/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'd864454e562ad019ef94');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'ca48b1a965467d35ffc6');
