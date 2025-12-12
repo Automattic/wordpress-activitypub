d542fcfeb6f8d8e1f49ecd4ff9af35c62bb009b4
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-12-12 16:27:36 +0100

    Remove 'Create Posts' field from advanced settings
    The 'activitypub_create_posts' settings field registration has been removed from the advanced settings fields. This streamlines the settings page and may reflect a change in feature availability or configuration approach.

--- includes/wp-admin/class-advanced-settings-fields.php
+++ includes/wp-admin/class-advanced-settings-fields.php
@@ -79,15 +79,6 @@ class Advanced_Settings_Fields {
 			array( 'label_for' => 'activitypub_following_ui' )
 		);
 
-		\add_settings_field(
-			'activitypub_create_posts',
-			\__( 'Create Posts', 'activitypub' ),
-			array( self::class, 'render_create_posts_field' ),
-			'activitypub_advanced_settings',
-			'activitypub_advanced_settings',
-			array( 'label_for' => 'activitypub_create_posts' )
-		);
-
 		if ( \version_compare( \get_bloginfo( 'version' ), '6.9-alpha', '>=' ) ) {
 			\add_settings_field(
 				'activitypub_reader_ui',
