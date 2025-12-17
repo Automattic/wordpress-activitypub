918c3625bdb838c9a4926f935ddd05460605a00c
Konstantin Obenland <obenland@gmx.de>
2025-12-17 16:38:41 -0600

    Remove sans-serif fallback from dashicons icon font
    Address PR feedback: icon fonts should not have generic fallbacks since
    they would display random characters instead of icons if the font fails
    to load.

--- build/reply/index.asset.php
+++ build/reply/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '61cf7fe5cfd753f456d8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '09bccbff3fa1c6cdff37');
