88cc04186f727b8ea6177761141cbf5ef448cb7a
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-01-15 10:30:42 +0100

    Fix Unicode URL encoding for ActivityPub actor URLs (#2757)
    Co-authored-by: Konstantin Obenland <obenland@gmx.de>

--- includes/model/class-blog.php
+++ includes/model/class-blog.php
@@ -11,6 +11,7 @@ use Activitypub\Activity\Actor;
 use Activitypub\Collection\Actors;
 use Activitypub\Collection\Extra_Fields;
 
+use function Activitypub\encode_url_path;
 use function Activitypub\esc_hashtag;
 use function Activitypub\get_attribution_domains;
 use function Activitypub\get_rest_url_by_path;
@@ -93,7 +94,7 @@ class Blog extends Actor {
 		$permalink = \get_option( 'activitypub_use_permalink_as_id_for_blog', false );
 
 		if ( $permalink ) {
-			return \esc_url( \trailingslashit( get_home_url() ) . '@' . $this->get_preferred_username() );
+			return encode_url_path( \home_url( '/@' . $this->get_preferred_username() ) );
 		}
 
 		return \add_query_arg( 'author', $this->_id, \home_url( '/' ) );
