c7e1dd65911911a6d75c35e55bc864cef51f54f5
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-01-21 18:26:32 +0100

    Add fediverse symbol to admin bar item.

--- includes/wp-admin/class-menu.php
+++ includes/wp-admin/class-menu.php
@@ -117,6 +117,13 @@ class Menu {
 			return;
 		}
 
+		$icon_styles = '<style>
+			#wpadminbar #wp-admin-bar-activitypub-social-web > .ab-item:before {
+				content: "\2042";
+				font: normal 20px/1 sans-serif;
+			}
+		</style>';
+
 		$wp_admin_bar->add_node(
 			array(
 				'id'    => 'activitypub-social-web',
@@ -124,6 +131,7 @@ class Menu {
 				'href'  => \admin_url( 'admin.php?page=activitypub-social-web' ),
 				'meta'  => array(
 					'title' => \__( 'View your Social Web feed', 'activitypub' ),
+					'html'  => $icon_styles,
 				),
 			)
 		);
