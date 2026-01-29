9a275dcd8ad80354db96c01d9e793f55668d750a
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-01-29 18:59:16 +0100

    Move Jest mocks from root to tests/js directory
    - Move __mocks__/@wordpress/interactivity.js to tests/js/__mocks__/
    - Update jest.config.js moduleNameMapper path reference
    - Add /__mocks__/ to testPathIgnorePatterns to prevent Jest from
      treating mock files as test suites

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,17 @@ const defaultConfig = require( '@wordpress/scripts/config/jest-unit.config.js' )
 module.exports = {
 	...defaultConfig,
 	testMatch: [ '**/tests/js/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)' ],
-	testPathIgnorePatterns: [ '/node_modules/', '/tests/e2e/', '/tests/phpunit/', '/vendor/', '/build/' ],
+	testPathIgnorePatterns: [
+		'/node_modules/',
+		'/tests/e2e/',
+		'/tests/phpunit/',
+		'/vendor/',
+		'/build/',
+		'/__mocks__/',
+	],
 	setupFilesAfterEnv: [ '<rootDir>/jest.setup.js' ],
 	moduleNameMapper: {
 		...defaultConfig.moduleNameMapper,
-		'^@wordpress/interactivity$': '<rootDir>/__mocks__/@wordpress/interactivity.js',
+		'^@wordpress/interactivity$': '<rootDir>/tests/js/__mocks__/@wordpress/interactivity.js',
 	},
 };
