d2bed3c70469f86faa69ed2f5f8ec0589b77eb63
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-02-12 12:59:43 +0100

    Add @package tag to FediBlog snippet.

--- snippets/fediblog-tag/fediblog-tag.php
+++ snippets/fediblog-tag/fediblog-tag.php
@@ -12,6 +12,8 @@
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       activitypub-fediblog
  * Requires Plugins:  activitypub
+ *
+ * @package Activitypub
  */
 
 namespace Activitypub\Snippets;
