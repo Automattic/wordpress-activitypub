965e79bd66439f37137f6ba2715f8d0c4dbe099b
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-02-24 09:33:11 +0100

    Rebuild assets after dependency bumps (#2970)

--- build/editor-plugin/plugin.asset.php
+++ build/editor-plugin/plugin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => 'dd96f41c33893e8c0d53');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives', 'wp-url'), 'version' => '8fdbb2d9969a81aa6b39');
