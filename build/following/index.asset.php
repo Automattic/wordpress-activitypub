4be6fb1f46c2a55ba0aab294b643ef80d8efc232
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-02-26 12:21:43 +0100

    Standardize block.json metadata for non-block editor scripts
    Namespace block names with `activitypub/` prefix and replace
    placeholder titles with descriptive labels for editor-plugin
    and reply-intent.

--- build/following/index.asset.php
+++ build/following/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => '61369677606b177b8b3e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-url'), 'version' => 'a7c019f20a76308d3cbb');
