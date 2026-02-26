3fff8d6d616103f38e5793e55388708eb4b2d471
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-02-26 14:47:07 +0100

    Standardize block.json metadata across all blocks (#2980)

--- build/reactions/index.asset.php
+++ build/reactions/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c095816f86c62883fba6');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '81b1ae31b58f5a0dd4fa');
