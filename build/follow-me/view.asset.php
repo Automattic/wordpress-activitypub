759323dc89051ff8d85e4de86d52fa52c03237ea
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-03-03 15:00:55 +0100

    Fix reactions block responsive layout and label styling (#2992)

--- build/follow-me/view.asset.php
+++ build/follow-me/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '974b52f8f70f28c62f70', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '6287fd959363cf251b43', 'type' => 'module');
--- build/reactions/view.asset.php
+++ build/reactions/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'ef18f7ed6de863c558b7', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '90dd4cf5b4bec20ca908', 'type' => 'module');
