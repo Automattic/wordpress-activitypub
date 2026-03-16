a9093d1fd9a123666fdf53b30f0c2835be0b5bc9
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-03-16 15:14:30 +0100

    Use Query Loop icon for Posts and Replies block

--- build/posts-and-replies/index.asset.php
+++ build/posts-and-replies/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '22bb646d831c6b37eeb1');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-primitives'), 'version' => '945da2c5a323624f9c37');
--- src/posts-and-replies/index.js
+++ src/posts-and-replies/index.js
@@ -1,10 +1,12 @@
 import { registerBlockType } from '@wordpress/blocks';
 import { InnerBlocks } from '@wordpress/block-editor';
+import { loop as icon } from '@wordpress/icons';
 import edit from './edit';
 import metadata from './block.json';
 import './style.scss';
 
 registerBlockType( metadata, {
+	icon,
 	edit,
 	save: () => <InnerBlocks.Content />,
 } );
