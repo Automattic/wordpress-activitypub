079b8a3ada2afe2056784e4d75e46d18252f69fc
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-03-19 14:34:18 +0100

    Remove type overloading from podcast integrations
    Both Seriously Simple Podcasting and Podlove Podcast Publisher were
    hardcoding the ActivityPub object type to 'Note', ignoring the user's
    configured object type setting. This removes the get_type() overrides
    so podcast episodes respect the site's object type preference, just
    like regular posts.

--- tests/phpunit/tests/integration/class-test-podlove-podcast-publisher.php
+++ tests/phpunit/tests/integration/class-test-podlove-podcast-publisher.php
@@ -16,11 +16,9 @@ namespace Activitypub\Tests\Integration;
 class Test_Podlove_Podcast_Publisher extends \WP_UnitTestCase {
 
 	/**
-	 * Test that the transformer returns Note type.
-	 *
-	 * @covers ::get_type
+	 * Test that the transformer respects the configured object type setting.
 	 */
-	public function test_get_type_returns_note() {
+	public function test_get_type_respects_setting() {
 		$post = \wp_insert_post(
 			array(
 				'post_author'  => 1,
@@ -34,9 +32,16 @@ class Test_Podlove_Podcast_Publisher extends \WP_UnitTestCase {
 
 		$transformer = new \Activitypub\Integration\Podlove_Podcast_Publisher( $post );
 
+		// Default setting uses post format logic: titled post without format = Article.
+		\update_option( 'activitypub_object_type', 'wordpress-post-format' );
+		$this->assertEquals( 'Article', $transformer->get_type() );
+
+		// Explicit Note setting.
+		\update_option( 'activitypub_object_type', 'note' );
 		$this->assertEquals( 'Note', $transformer->get_type() );
 
 		// Clean up.
+		\delete_option( 'activitypub_object_type' );
 		\wp_delete_post( $post->ID, true );
 	}
 
