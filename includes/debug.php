a209ce598b6edbc385c550c63381b0ad78f19ea6
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-03-22 10:36:22 +0100

    Move localhost URL allowance to local environment only (#3076)

--- includes/debug.php
+++ includes/debug.php
@@ -12,20 +12,6 @@ use Activitypub\Collection\Outbox;
 use Activitypub\Collection\Remote_Posts;
 
 /**
- * Allow localhost URLs if WP_DEBUG is true.
- *
- * @param array $parsed_args An array of HTTP request arguments.
- *
- * @return array Array or string of HTTP request arguments.
- */
-function allow_localhost( $parsed_args ) {
-	$parsed_args['reject_unsafe_urls'] = false;
-
-	return $parsed_args;
-}
-\add_filter( 'http_request_args', '\Activitypub\allow_localhost' );
-
-/**
  * Debug the outbox post type.
  *
  * @param array  $args      The arguments for the post type.
