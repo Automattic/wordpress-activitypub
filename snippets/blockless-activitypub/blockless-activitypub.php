17a6c0e8791514cbf336d2f016de7599b14f8d92
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-09-14 08:12:15 +0200

    Add Update URI header to snippets (#3765)

--- snippets/blockless-activitypub/blockless-activitypub.php
+++ snippets/blockless-activitypub/blockless-activitypub.php
@@ -11,6 +11,7 @@
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Requires Plugins:  activitypub
+ * Update URI:        https://github.com/Automattic/wordpress-activitypub
  *
  * @package Activitypub
  */
