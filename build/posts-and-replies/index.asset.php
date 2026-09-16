184acb8ee376e5e0f6fcd3ba338d90d81eefa5ac
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2026-09-16 09:13:03 +0200

    Rebuild assets after the package bumps (#3784)

--- build/posts-and-replies/index.asset.php
+++ build/posts-and-replies/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'a13f3cfab5aec0929971');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '308fa3c0c1c11a1e33fe');
